cd0e718220225df1d978664ad33f09437d2fe611
Dave Doolin <david.doolin@gmail.com>
2012-02-12 14:54:16 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stormsavvy::Application.config.secret_token = '3987930edc64fedc4cc8a510be09cdce08b28fe59376164f37789233ece07f300989e8235a62240dd3657cb8692e64827ad3fafd92f137324c55e76525fff6ba'
