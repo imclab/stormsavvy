b43bc28c26b26cafdef38672970332730103940a
Dave Doolin <david.doolin@gmail.com>
2012-03-03 13:45:47 -0800

    added check for cem2030

--- spec/models/rain_event_workflow_spec.rb
+++ spec/models/rain_event_workflow_spec.rb
@@ -16,5 +16,26 @@ describe RainEventWorkflow do
     @rew.reap = false
     @rew.check_reap.should =~ /Prepare REAP/
   end
- 
+
+  it "should check on CEM2045 status 48 hours before rain" do
+    @rew.hours_before_rain = 48
+    @rew.chance_of_rain = 55
+    @rew.reap = true
+    @rew.check_reap.should_not =~ /Prepare REAP/
+  end
+
+  it "should check on CEM2030 status 24 hours before rain" do
+    @rew.hours_before_rain = 48
+    @rew.chance_of_rain = 55
+    @rew.reap = false
+    @rew.check_cem2030.should =~ /Prepare CEM2030/    
+  end
+
+  it "should check on CEM2030 status 24 hours before rain" do
+    @rew.hours_before_rain = 48
+    @rew.chance_of_rain = 55
+    @rew.cem2030 = true
+    @rew.check_cem2030.should_not =~ /Prepare CEM2030/
+  end
+
 end
