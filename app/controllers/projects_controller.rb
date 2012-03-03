d2c55fcf09c8bd57b21b2ec4078b8f67283bc2d4
Adam Barber <adam@adambarber.tv>
2012-03-03 15:55:03 -0800

    Nested sites under projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,6 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = current_user.projects.build(params[:project])
-    @sites = @project.sites.build
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
