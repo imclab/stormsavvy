45489fa2b1762d686607e062bc68d805714bf5ae
Walter Yu <kharma@gmail.com>
2012-03-03 20:28:10 -0800

    what changed?

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Project do
+
+  before(:each) do
+    @project = Project.create(
+	  :name => "Hwy 1",
+	  :description => "AC Overlay", 
+	  :startdate => "2011-02-20 12:01:00",
+	  :startdate => "2012-02-20 12:01:00" 
+	  )
+ end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
