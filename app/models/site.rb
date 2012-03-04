6988bce60cfeaa7bdeb97a7c1bc7546afe54b847
Paul Maki <paul.maki@gmail.com>
2012-03-04 10:15:11 -0800

    Adding site specs

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,10 +1,11 @@
 class Site < ActiveRecord::Base
 
   attr_accessible :name, :description, :costcode, :size, :address_1, :address_2, :state, :zipcode, :city, :exposed_area
-
   belongs_to :project
   geocoded_by :address, :latitude => :lat, :longitude => :long
   after_validation :geocode
+
+  validates_presence_of :name 
 
   def self.latlong(zipcode)
     l = self.where(:zipcode => zipcode).first
@@ -14,7 +15,7 @@ class Site < ActiveRecord::Base
   def address
     "#{self.address_1} #{self.address_2} #{self.city} #{self.state} #{self.zipcode}".strip
   end
-
+  
   def self.get_24_pop
 
     hydra = Typhoeus::Hydra.new
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe Site do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @site = Factory.create(:site)
+  end
+
+  it "should have name" do
+    @site.name.should == "Oakland Adams Point"
+  end
+
+  it "should have description" do
+    @site.description.should == "North of Lake Merritt"
+  end
+
+  it "should have costcode" do
+    @site.costcode.should == "450AZC"
+  end
+
+  it "should have size" do
+    @site.size.should == "20 acres"
+  end
+
+  it "should have exposed_area" do
+    @site.exposed_area.should == "10 acres"
+  end
+
+  it "should have zipcode" do
+    @site.zipcode.should == 94610
+  end
+
+  it "should geocode oakland" do
+    s = Site.create!(:city => "Oakland")
+    s.lat.round.should == 37
+    s.long.round.should == -122
+  end
+
+  it "should have a project attribute" do
+    @site.should respond_to(:project)
+  end
+
 end
