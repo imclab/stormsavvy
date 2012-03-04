11e8898040200c219762cc2d8fd871dfaa206117
Dave Doolin <david.doolin@gmail.com>
2012-03-03 16:17:46 -0800

    temperature now returned via zip code

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,9 @@ class Location < ActiveRecord::Base
   validates_presence_of :zipcode
   validates_presence_of :lat
   validates_presence_of :long
+
+  def self.latlong(zipcode)
+    l = Location.where(:zipcode => zipcode).first
+    [] << l.lat << l.long
+  end
 end
