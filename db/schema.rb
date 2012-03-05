4ce39cea6b29cac61946082c59c682526aad89a6
Dave Doolin <david.doolin@gmail.com>
2012-03-04 17:18:40 -0800

    scheme for report status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120304230203) do
+ActiveRecord::Schema.define(:version => 20120305005002) do
 
   create_table "dashboards", :force => true do |t|
     t.integer  "user_id"
