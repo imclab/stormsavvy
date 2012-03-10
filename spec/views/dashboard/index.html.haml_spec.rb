d5d966f5fcb73e712e4be322795c1f4ffc9742f1
Dave Doolin <david.doolin@gmail.com>
2012-03-09 17:08:27 -0800

    First pass at view speccing dashboard.

--- spec/views/dashboard/index.html.haml_spec.rb
+++ spec/views/dashboard/index.html.haml_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe "dashboard/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "dashboard/index" do
+
+  before(:each) do
+    @user = Factory(:user)
+    @project = Factory(:project)
+  end
+  
+  xit "should show Active Projects to signed in user" do
+    #sign_in @user
+    render
+    rendered.should =~ /Active Projects/
+  end
+
 end
