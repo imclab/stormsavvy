0f0587442426bca5b3bd48dc7714fdab729990c6
Dave Doolin <david.doolin@gmail.com>
2012-03-09 21:07:06 -0800

    Displaying chance of rain using new API.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,34 +1,11 @@
 module DashboardHelper
 
-include Typhoeus
-include Nokogiri
+require 'weather/noaa_forecast'
 
-  def seven_day_temp(zipcode)
-
-    #l = Location.where(:zipcode => zipcode).first
-    #xml = "http://www.wrh.noaa.gov/forecast/xml/xml.php?duration=168&interval=6&lat=#{l.lat}&lon=#{l.long}"
-    l = Location.new
-    ll = l.latlong(zipcode.to_i)
-    xml = "http://www.wrh.noaa.gov/forecast/xml/xml.php?duration=168&interval=6&lat=#{ll[0]}&lon=#{ll[1]}"
-#=begin    
-    
-    #table = "http://www.wrh.noaa.gov/forecast/wxtables/index.php?lat=#{l.lat}&lon=#{l.long}&clrindex=0&table=custom&duration=7&interval=6"
-    request = Typhoeus::Request.new(xml,
-              :body          => "this is a request body",
-              :method        => :post,
-              :headers       => {:Accept => "text/html"},
-              :timeout       => 2000, # milliseconds
-              :cache_timeout => 60, # seconds
-              :params        => {:field1 => "a field"})
-    hydra = Typhoeus::Hydra.new
-    hydra.queue(request)
-    hydra.run
-    hydra.queue(request)
-    hydra.run
-    response = request.response
-    doc   = Nokogiri::XML(response.body)
-    #binding.pry
-    return doc.xpath("//temperature").map { |n| n.content.to_i }
-#=end
+  def chance_of_rain
+    nf = NOAAForecast.new(94530)
+    forecast = nf.seven_day_temp(94530)
+    return forecast[0][0..4].max
   end
+  
 end
