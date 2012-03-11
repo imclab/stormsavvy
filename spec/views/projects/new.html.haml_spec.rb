95c7f760d976314cb0e7bae02bb55f6a890aa88f
Dave Doolin <david.doolin@gmail.com>
2012-03-11 15:51:50 -0700

    Cleaned up view spec.

--- spec/views/projects/new.html.haml_spec.rb
+++ spec/views/projects/new.html.haml_spec.rb
@@ -10,12 +10,9 @@ describe "projects/new" do
     ).as_new_record)
   end
 
-  xit "renders new project form" do
+  it "renders new project form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => projects_path, :method => "post" do
-      assert_select "input#project_user_id", :name => "project[user_id]"
       assert_select "input#project_name", :name => "project[name]"
       assert_select "textarea#project_description", :name => "project[description]"
       assert_select "input#project_active", :name => "project[active]"
