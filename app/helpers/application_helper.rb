39e45f24fbe22d3c959cacdd3f0d1b1980304175
Adam Barber <adam@adambarber.tv>
2012-03-10 16:44:08 -0800

    refactored views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def template_class
     {:class => "#{params[:controller].gsub(/\//,'-')} #{params[:action]}"}
   end
+
+  def is_public_home?
+    if params[:controller] == 'pages' && params[:action] == "index" && !current_user
+      true
+    end
+  end
 end
