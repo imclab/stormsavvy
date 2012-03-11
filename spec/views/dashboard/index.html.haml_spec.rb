bd68e47853bc5c37d4b8de82ea3717a08ac4ef8a
Dave Doolin <david.doolin@gmail.com>
2012-03-11 15:32:34 -0700

    Dashboard index view rspecced.

--- spec/views/dashboard/index.html.haml_spec.rb
+++ spec/views/dashboard/index.html.haml_spec.rb
@@ -2,15 +2,34 @@ require 'spec_helper'
 
 describe "dashboard/index" do
 
+  def site_attributes
+    { :name => 'Foo bar', :zipcode => '94530' }
+  end
+
   before(:each) do
     @user = Factory(:user)
-    @project = Factory(:project)
-  end
-  
-  xit "should show Active Projects to signed in user" do
+    @p1 = Factory(:project)
+    @p2 = Factory(:project)
+    @projects = [@p1, @p2]
+    @needs_attention_reports = Report.needs_attention
+    #@s1 = @p1.sites.create(site_attributes)
+    #@s2 = @p2.sites.create(site_attributes)
     sign_in @user
-    render
-    rendered.should =~ /Active Projects/
+  end
+
+  describe "index page components" do
+
+    it "should show Active Projects to signed in user" do
+      render
+      rendered.should have_selector('div#modules')
+      rendered.should =~ /Active Projects/
+    end
+
+    it "should show the Dashboard sidebar to signed in user" do
+      render
+      rendered.should have_selector('div#dashboard-sidebar')
+    end
+
   end
 
 end
