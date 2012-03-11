ffbcfb0a0ac6c84ea9a0ff7453a37733f057f88b
Dave Doolin <david.doolin@gmail.com>
2012-03-10 20:00:00 -0800

    rain state now reported

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,12 +1,27 @@
 module DashboardHelper
 
 require 'weather/noaa_forecast'
+require 'weather/forecast_examiner'
 
-  def chance_of_rain(zip)
-    zip = 90210 unless zip.present?
-    nf = NOAAForecast.new(zip)
-    nf.seven_day_temp(zip)
-    nf.noaa_forecast[0][0..4].max
+  attr_reader :rain_state, :max_rain
+  
+  def chance_of_rain(zipcode)
+    zipcode = 90210 unless zipcode.present?
+    nf = NOAAForecast.new(zipcode)
+    nf.seven_day_temp(zipcode)
+    precipitation_state(nf.noaa_forecast)
+    @max_rain = nf.noaa_forecast[0][0..4].max
+
+  end
+
+  def precipitation_state(forecast)
+    if forecast == [[],[]]
+      @rain_state = :null
+      return
+    end
+    fe = ForecastExaminer.new(forecast)
+    fe.find_rain_chance()
+    @rain_state = fe.rain
   end
 
 end
