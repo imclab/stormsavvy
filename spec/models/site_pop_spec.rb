5e73158b413e464079b51d065e89da51eee4f6c7
Dave Doolin <david.doolin@gmail.com>
2012-03-12 10:16:45 -0700

    Dummy spec for sitepop.

--- spec/models/site_pop_spec.rb
+++ spec/models/site_pop_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe SitePop do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "creates its ownself" do
+    sp = SitePop.create!
+    sp.class.should == SitePop
+  end
 end
