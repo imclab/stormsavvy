aa110620a350eaeecef5603c8526a1bdcc0dda91
Dave Doolin <david.doolin@gmail.com>
2012-03-11 18:33:40 -0700

    RSpec dashboard controller now working.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 
 describe DashboardController do
 
-  describe "GET 'index'" do
-    xit "returns http success" do
-      get 'index'
+  before(:each) do
+    @user = Factory(:user)
+    sign_in @user
+  end
+
+  describe "GET 'index' for signed in user" do
+    it "returns http success" do
+      get :index
       response.should be_success
+    end
+  end
+
+  describe "GET 'index' for non-signed in user" do
+    it "returns http success" do
+      sign_out @user
+      get :index
+      response.should redirect_to index_path
     end
   end
 
