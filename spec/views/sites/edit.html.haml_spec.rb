0790b30597b9d61ac87de2cf6dd309238ba1ccbe
Dave Doolin <david.doolin@gmail.com>
2012-03-11 21:17:43 -0700

    No love for this site/edit view spec, retested.

--- spec/views/sites/edit.html.haml_spec.rb
+++ spec/views/sites/edit.html.haml_spec.rb
@@ -7,22 +7,18 @@ describe "sites/edit" do
       :description => "MyString",
       :costcode => "MyString",
       :size => "MyString",
-      :exposed_area => "MyString",
-      :location => "MyString"
+      :exposed_area => "MyString"
     ))
   end
 
   xit "renders the edit site form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => sites_path(@site), :method => "post" do
+    assert_select "form", :action => site_path(@site.project_id, @site), :method => "post" do
       assert_select "input#site_name", :name => "site[name]"
       assert_select "input#site_description", :name => "site[description]"
       assert_select "input#site_costcode", :name => "site[costcode]"
       assert_select "input#site_size", :name => "site[size]"
       assert_select "input#site_exposed_area", :name => "site[exposed_area]"
-      assert_select "input#site_location", :name => "site[location]"
     end
   end
 end
