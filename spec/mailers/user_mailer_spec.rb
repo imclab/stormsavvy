370106443a4bcf2bb12ca5a87fbceea53e488d87
Dave Doolin <david.doolin@gmail.com>
2012-03-12 10:10:32 -0700

    Dummy spec for UserMailer.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,10 @@
 require "spec_helper"
 
+# http://robaldred.co.uk/2009/12/testing-actionmailer-models-with-rspec/
+# http://stackoverflow.com/questions/5843284/clearing-out-activemailerbase-deliveries-after-rspec-test
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "creates a valid UserMailer" do
+  end
+
 end
