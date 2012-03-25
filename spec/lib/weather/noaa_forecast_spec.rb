862f091370cb3257de64ca3b685a735899221708
Dave Doolin <david.doolin@gmail.com>
2012-03-25 15:27:57 -0700

    cleaned up noaa lib spec

--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -14,25 +14,11 @@ describe NOAAForecast do
     nf.class.should == NOAAForecast
   end
 
-  xit "should not instantiate class with invalid zipcode" do
-    nf = NOAAForecast.new('gdf33')
-    # Write up the error message as a result of the following:
-    #nf.class.should = NOAAForecast
-    nf.class.should_not == NOAAForecast
-  end
-
   it "NOAA weather forecast for one week should have 29 elements" do
     nf = NOAAForecast.new(94530,168,6)
     nf.seven_day_weather
     #p nf.noaa_forecast[0].size
     nf.noaa_forecast[0].size.should == @fullcount
-  end
-
-  it "temporary hard coded result" do
-    nf = NOAAForecast.new(94530,168,6)
-    nf.seven_day_weather
-    #p nf.noaa_forecast
-    nf.noaa_forecast[0][0..5].max.should == 77
   end
 
   it "NOAA weather forecast blows it's gourd if given nil zipcode" do
