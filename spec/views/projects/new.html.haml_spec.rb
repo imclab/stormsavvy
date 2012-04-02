4b4d0708852213b353d3f01e7161565d5c1fd772
Walter Yu <kharma@gmail.com>
2012-04-01 19:03:06 -0700

    add flash message

--- spec/views/projects/new.html.haml_spec.rb
+++ spec/views/projects/new.html.haml_spec.rb
@@ -18,4 +18,17 @@ describe "projects/new" do
       assert_select "input#project_active", :name => "project[active]"
     end
   end
+
+  # Flash message calling twitterized_type method in application helper.
+  xit "shows flash message" do
+    assign(:project, stub_model(Project,
+      :user_id => 2,
+      :name => "FlashTest",
+      :description => "SeeMessage",
+      :active => false
+    ).as_new_record)
+    rendered.should have_selector "data-alert",
+      :class => twitterized_type(:notice),
+      :count => 1
+  end
 end
