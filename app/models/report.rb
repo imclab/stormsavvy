fdacbd099aae1bf976ff499f9ac8aea2c034a447
Walter Yu <kharma@gmail.com>
2012-04-14 13:53:28 -0700

    add page 1 CEM 2030 fields

--- app/models/report.rb
+++ app/models/report.rb
@@ -28,39 +28,56 @@ class Report < ActiveRecord::Base
   :submitted_by_contractor,
   :submitted_by_date,
 
-  :wpc_manager
-  :wpc_phone
-  :wpc_emergency_phone
+  :wpc_manager,
+  :wpc_phone,
+  :wpc_emergency_phone,
 
-  :inspector_name
-  :inspection_date
+  :inspector_name,
+  :inspection_date,
 
-  :weather_condition # clear / partly cloudy / cloudy
+  :weather_condition, 
+  # clear / partly cloudy / cloudy
 
-  :precipitation_condition >> misty/ light_rain / rain /
-heavy_rain / hail / snow
+  :precipitation_condition, 
+  # misty/ light_rain / rain / heavy_rain / hail / snow
 
-:wind_condition >> none / less_5mph / more_5mph
+  :wind_condition,
+  # none / less_5mph / more_5mph
 
-:construction_phase >> highway_construction /
-plant_establishment / suspension_of_work
+  :construction_phase,
+  # highway_construction / plant_establishment / 
+  # suspension_of_work
 
-:site_information >> total_area / total_DSA /
-current_DSA / inactive_DSA
+  :site_information,
+  # total_area / total_DSA / current_DSA / inactive_DSA
 
-:inspection type >> weekly / quarterly / pre_storm /
-during_storm / post-storm
+  :inspection_type,
+  # weekly / quarterly / pre_storm / during_storm / 
+  # post-storm
 
-:time_elapsed >> x_days
-:precipitation_received >> x_inches
-:time_storm_expected >> x_time, x_date
-:expected_precipitation_amount >> x_inches
-:time_elapsed >> x_hours_minutes
-:gauge_reading >> x_inches
-:time_elapsed_began >> x_hours_minutes
-:gauge_reading_began >> x_inches
+  :time_elapsed, 
+  # x_days
 
+  :precipitation_received, 
+  # x_inches
 
+  :time_storm_expected, 
+  # x_time, x_date
+
+  :expected_precipitation_amount,
+  # x_inches
+
+  :time_elapsed, 
+  # x_hours_minutes
+
+  :gauge_reading, 
+  # x_inches
+
+  :time_elapsed_began, 
+  # x_hours_minutes
+
+  :gauge_reading_began 
+  # x_inches
 
   before_update :change_report_status
 
