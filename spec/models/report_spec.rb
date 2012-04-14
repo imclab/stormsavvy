06c504fbd01854d29e77a6f71493de12c3027bbf
Walter Yu <kharma@gmail.com>
2012-04-14 13:54:19 -0700

    add scope into report model spec

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -7,11 +7,28 @@ describe Report do
     r.should be_valid
   end
 
+  xit "should respond to sites" do
+    r = Report.new
+    r.should respond_to(:sites)
+  end
+
   it "should change status on update" do
     r = Report.create
     r.status.should be_nil
     r.contractor_name = "Ghilotti Bros"
     r.save
     r.status.should == "complete"
+  end
+
+  context 'scopes' do
+    it "should ony return reports needing attention" do
+      Report.needs_attention.where_values_hash.should == 
+        {:status => "needs_attention"}
+    end
+
+    it "should ony return completed reports" do
+      Report.completed.where_values_hash.should == 
+        {:status => "completed"}
+    end
   end
 end
