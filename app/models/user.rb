e35a7357bf2d6f61bba906b5f10fc6d575060895
Walter Yu <kharma@gmail.com>
2012-04-14 16:01:49 -0700

    reorder attr_accessible attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :lastname,
-  :contractor_name, :contractor_address_1, :contractor_address_2, :contractor_city, :contactor_state, :contactor_zipcode
+  attr_accessible :email, 
+  :password, 
+  :password_confirmation, 
+  :remember_me, 
+  :firstname, 
+  :lastname,
+  :contractor_name, 
+  :contractor_address_1, 
+  :contractor_address_2, 
+  :contractor_city, 
+  :contactor_state, 
+  :contactor_zipcode
 
   has_many :sites, :dependent => :destroy
 
