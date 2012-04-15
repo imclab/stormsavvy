5ce34faa95c93b142ffb8402931a301fb3c1bb32
Walter Yu <kharma@gmail.com>
2012-04-15 01:02:43 -0700

    change fields in report with migration file

--- db/migrate/20120415074631_change_fields_for_report.rb
+++ db/migrate/20120415074631_change_fields_for_report.rb
@@ -1,7 +1,34 @@
 class ChangeFieldsForReport < ActiveRecord::Migration
-  def up
-  end
+  def change
 
-  def down
+	add_column :reports, :submitted_by_contractor, 	:date
+	add_column :reports, :submitted_by_date,	   	:date
+
+	add_column :reports, :wpc_manager,			 	:string	 
+	add_column :reports, :wpc_phone,				:string
+	add_column :reports, :wpc_emergency_phone,	 	:string
+
+	add_column :reports, :inspector_name,			:string
+	add_column :reports, :inspection_date,		 	:date
+
+	add_column :reports, :weather_condition, 		:string
+	add_column :reports, :precipitation_condition,  :string
+	add_column :reports, :wind_condition,			:string
+
+	add_column :reports, :construction_phase,		:string
+	add_column :reports, :total_area, 			 	:decimal
+	add_column :reports, :total_DSA, 				:decimal
+	add_column :reports, :current_DSA, 			 	:decimal
+	add_column :reports, :inactive_DSA,			 	:decimal
+
+	add_column :reports, :inspection_type,		 	:string
+	add_column :reports, :time_elapsed_last_storm, 	:decimal
+	add_column :reports, :precipitation_received,  	:decimal
+	add_column :reports, :time_storm_expected,  	:decimal
+	add_column :reports, :expected_precipitation_amount, :decimal
+	add_column :reports, :time_elapsed_during_storm, :decimal
+	add_column :reports, :gauge_reading_during_storm, :decimal
+	add_column :reports, :time_elapsed_post_storm, :decimal
+	add_column :reports, :gauge_reading_post_storm, :decimal
   end
 end
