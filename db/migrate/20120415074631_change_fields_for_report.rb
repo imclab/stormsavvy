af265c95fbb5e2dbbeec7c934a5b1859f336fcde
Walter Yu <kharma@gmail.com>
2012-04-15 11:29:10 -0700

    final edit for now for report migration

--- db/migrate/20120415074631_change_fields_for_report.rb
+++ db/migrate/20120415074631_change_fields_for_report.rb
@@ -8,20 +8,13 @@ class ChangeFieldsForReport < ActiveRecord::Migration
 	add_column :reports, :wpc_phone,				:string
 	add_column :reports, :wpc_emergency_phone,	 	:string
 
-	add_column :reports, :inspector_name,			:string
 	add_column :reports, :inspection_date,		 	:date
 
-	add_column :reports, :weather_condition, 		:string
-	add_column :reports, :precipitation_condition,  :string
-	add_column :reports, :wind_condition,			:string
-
-	add_column :reports, :construction_phase,		:string
 	add_column :reports, :total_area, 			 	:decimal
 	add_column :reports, :total_DSA, 				:decimal
 	add_column :reports, :current_DSA, 			 	:decimal
 	add_column :reports, :inactive_DSA,			 	:decimal
 
-	add_column :reports, :inspection_type,		 		 :string
 	add_column :reports, :time_elapsed_last_storm, 		 :decimal
 	add_column :reports, :precipitation_received,  		 :decimal
 	add_column :reports, :time_storm_expected,  		 :decimal
