8644f15c5a193d7e4d926ac9aca7f3f069a9c58a
Walter Yu <kharma@gmail.com>
2012-04-15 18:11:30 -0700

    committing broken spec...

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -20,6 +20,36 @@ describe Report do
     r.status.should == "completed"
   end
 
+  context :report do 
+
+    before(:each) do 
+      @report = Factory(:report)
+      @report2 = Report.new
+    end
+
+    it "should be able to add 1 site" do
+      @project.site.reports.count.should == 1
+    end
+
+    it "should not able to add invalid reports" do
+      @project.site.reports << Report.new
+      @project.site.reports.count.should == 1
+    end
+
+    # it "should be able to add multiple sites" do
+    #   @project.sites << @site2
+    #   @site2.save
+    #   @project.sites.count.should be > 1
+    # end
+    
+    # it "should be able to delete an added site" do
+    #   precount = @project.sites.count
+    #   @project.sites << @site2
+    #   @project.sites.delete
+    #   @project.sites.count.should == precount
+    # end
+  end
+
   context 'scopes' do
     it "should ony return reports needing attention" do
       Report.needs_attention.where_values_hash.should == 
