26da7736b6f14a88fe8186e2bc457cfdb44efc04
Walter Yu <kharma@gmail.com>
2012-04-22 18:49:51 -0700

    figuring out how to test rendered pdf

--- spec/views/reports/show.pdf.prawn_spec.rb
+++ spec/views/reports/show.pdf.prawn_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'prawn'
+require 'pdf/inspector'
 
 describe "prawn report" do
 
@@ -8,11 +8,11 @@ describe "prawn report" do
   end
 
 	describe "when creating pdf" do
-		pdf = Prawn::Document.new
-    pdf.text "CEM2030"
-    pdf.render_file "CEM2030.pdf"
-    # send_data pdf.render, type: "application/pdf", disposition: "inline"
+		# pdf = Prawn::Document.new
+  	# pdf.text "CEM2030"
+  	# pdf.render_file "CEM2030.pdf"
 
+    # send_data pdf.render, type: "application/pdf", disposition: "inline"
 		# @pdf.pages.each do |page|
 		#   puts page.fonts
 		#   puts page.text
@@ -20,8 +20,8 @@ describe "prawn report" do
 		#  end
 
 		# Previous call from pdf-reader gem
-	  page_counter = PDF::Inspector.analyze(@pdf)
-	  page_counter.pages.size.should == 1
-	  @pdf.page_count.should == 1
+	  # page_counter = PDF::Inspector.analyze(pdf)
+	  # page_counter.pages.size.should == 1
+	  # pdf.page_count.should == 1
 	end
 end
