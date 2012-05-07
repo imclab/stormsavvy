754141c61c444fef7335b434350c53ea1cfbf08f
Dave Doolin <david.doolin@gmail.com>
2012-05-06 20:27:08 -0700

    removed useless specs

--- spec/mailers/alert_mailer_spec.rb
+++ spec/mailers/alert_mailer_spec.rb
@@ -14,16 +14,5 @@ describe AlertMailer do
       mail.body.encoded.should match("Hi")
     end
 
-    xit "should be set to be delivered to the email passed in" do
-      @email.should deliver_to("youremail@addresshere.com")
-    end
-
-    xit "should contain the user's message in the mail body" do
-      @email.should have_body_text(/Hi/)
-    end
-
-    xit "should have the correct subject" do
-      @email.should have_subject(/Pop/)
-    end
   end
 end
