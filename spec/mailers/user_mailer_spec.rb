b4ca7a736cbd0fbca06ee38fec5f06ad75426fdc
Dave Doolin <david.doolin@gmail.com>
2012-05-06 19:21:36 -0700

    Implemented spec for mailing out weather alert emails.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -30,71 +30,36 @@ describe UserMailer do
     ActionMailer::Base.deliveries.should_not be_empty
   end
 
-  describe "alert" do
+  describe "mailout" do
 
-    # before(:each) do
-    #   @order = mock_model(Order)
-    #   @order.customer = mock_model(Customer)
-    # end
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      project = user.projects.create!(:name => 'foo', :description => 'bar', :startdate => 3.days.ago, :finishdate => 1.day.ago)
+      site = project.sites.create!(:name => "Oakland Adams Point", :zipcode => 94610)
+    end
 
-    xit "should render successfully" do
-      lambda { OrderMailer.create_receipt(@order) }.should_not raise_error
+    it "should render successfully" do
+      lambda { UserMailer.mailout  }.should_not raise_error
     end
 
     describe "rendered without error" do
 
       before(:each) do
-        @mailer = OrderMailer.create_receipt(@order)
+        @mailer = UserMailer.mailout
       end
 
-      xit "should have an order number" do
-        @mailer.body.should have_tag('.order_number', :text => @order.id)
+      it "it should have an alert" do
+        @mailer.body.should have_css('.chance-of-rain', :text => 'chance of rain')
       end
 
-      xit "should have order details" do
-        @mailer.body.should have_tag(".order_details")
+      it "should have a list of projects" do
+        @mailer.body.should have_selector("ul.projects")
       end
 
-      xit "should have a billing address" do
-        @mailer.body.should have_tag(".billing.address")
-      end
-
-      xit "should have a delivery address" do
-        @mailer.body.should have_tag(".delivery.address")
-      end
-
-      xit "should have customer contact details" do
-        @mailer.body.should have_tag("#contact_details") do
-          with_tag('.landline', :text => @order.customer.landline)
-          with_tag('.mobile', :text => @order.customer.mobile)
-          with_tag('.email', :text => @order.customer.email)
-        end
-      end
-
-      xit "should have a list of items" do
-        @mailer.body.should have_tag(".line_items")
-      end
-
-      xit "should have totals" do
-        @mailer.body.should have_tag(".totals") do
-          with_tag('.sub_total', :text => format_price(@order.sub_total))
-          with_tag('.vat', :text => format_price(@order.vat))
-          with_tag('.postage', :text => format_price(@order.postage))
-          with_tag('.total', :text => format_price(@order.total))
-        end
-      end
-
-      xit "should deliver successfully" do
-        lambda { OrderMailer.deliver(@mailer) }.should_not raise_error
-      end
-
-      describe "and delivered" do
-
-        xit "should be added to the delivery queue" do
-          lambda { OrderMailer.deliver(@mailer) }.should change(ActionMailer::Base.deliveries,:size).by(1)
-        end
-
+      it "should have a list of sites" do
+        @mailer.body.should have_selector("ul.sites")
       end
     end
   end
+
 end
