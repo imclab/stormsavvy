85c8bd4ae2c18f9fae1b2406efe0e859e429e465
Walter Yu <kharma@gmail.com>
2012-05-06 17:52:54 -0700

    debugging noaa forecast mailer

--- spec/mailers/alert_mailer_spec.rb
+++ spec/mailers/alert_mailer_spec.rb
@@ -13,5 +13,17 @@ describe AlertMailer do
     it "renders the body" do
       mail.body.encoded.should match("Hi")
     end
+
+    xit "should be set to be delivered to the email passed in" do
+      @email.should deliver_to("youremail@addresshere.com")
+    end
+
+    xit "should contain the user's message in the mail body" do
+      @email.should have_body_text(/Hi/)
+    end
+
+    xit "should have the correct subject" do
+      @email.should have_subject(/Pop/)
+    end
   end
 end
