599fa38f7c97a1ed56d6d6f5ba680742ae027246
Dave Doolin <david.doolin@gmail.com>
2012-05-06 19:20:22 -0700

    Removed working spec which didn't do anything.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,9 +4,6 @@ require "spec_helper"
 # http://stackoverflow.com/questions/5843284/clearing-out-activemailerbase-deliveries-after-rspec-test
 describe UserMailer do
 
-  it "creates a valid UserMailer" do
-  end
-
   before { ActionMailer::Base.deliveries = [] }
 
   it "should send Pop Alert emails" do
