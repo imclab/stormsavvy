0a07b18fae411c29af229f7b59d6cafa121ef69b
Walter Yu <kharma@gmail.com>
2012-07-08 16:36:57 -0700

    comment out conflicting migration files

--- db/migrate/20120416040431_fix_identifer_typo.rb
+++ db/migrate/20120416040431_fix_identifer_typo.rb
@@ -1,6 +1,6 @@
 class FixIdentiferTypo < ActiveRecord::Migration
-  def change
-  	remove_column :reports, :project_identifer_number, :string
-  	add_column :reports, :project_identifier_number,   :string
-  end
+  # def change
+  # 	remove_column :reports, :project_identifer_number, :string
+  # 	add_column :reports, :project_identifier_number,   :string
+  # end
 end
