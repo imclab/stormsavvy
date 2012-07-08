376d720f458644c5a41a0e7cba6f54482c906540
Walter Yu <kharma@gmail.com>
2012-07-08 16:31:45 -0700

    resolve heroku rake db:migrate syntax error

--- db/migrate/20120416025512_fix_report_field_typos.rb
+++ db/migrate/20120416025512_fix_report_field_typos.rb
@@ -1,18 +1,18 @@
 class FixReportFieldTypos < ActiveRecord::Migration
-  def change
-    # Fixing typos in AddFieldsToReport migration file
-    # remove_column :reports, :contactor_state,    :string
-    # remove_column :reports, :contactor_zipcode,  :integer
-    # add_column    :reports, :contractor_state,   :string
-    # add_column    :reports, :contractor_zipcode, :integer
+  # def change
+  #   Fixing typos in AddFieldsToReport migration file
+  #   remove_column :reports, :contactor_state,    :string
+  #   remove_column :reports, :contactor_zipcode,  :integer
+  #   add_column    :reports, :contractor_state,   :string
+  #   add_column    :reports, :contractor_zipcode, :integer
 
-    # remove_column :reports, :water_polution_control_manager_emergancy_phone_number, :string
-    # add_column    :reports, :water_polution_control_manager_emergency_phone_number, :string
+  #   remove_column :reports, :water_polution_control_manager_emergancy_phone_number, :string
+  #   add_column    :reports, :water_polution_control_manager_emergency_phone_number, :string
 
-    # remove_column :reports, :storm_information_expected_percipitaion_amount, :decimal
-    # add_column    :reports, :storm_information_expected_precipitaion_amount, :decimal
+  #   remove_column :reports, :storm_information_expected_percipitaion_amount, :decimal
+  #   add_column    :reports, :storm_information_expected_precipitaion_amount, :decimal
 
-    # remove_column :reports, :storm_information_percipitation_since_last_storm, :decimal
-    # remove_column :reports, :storm_information_precipitation_since_last_storm, :decimal
-  end
+  #   remove_column :reports, :storm_information_percipitation_since_last_storm, :decimal
+  #   remove_column :reports, :storm_information_precipitation_since_last_storm, :decimal
+  # end
 end
