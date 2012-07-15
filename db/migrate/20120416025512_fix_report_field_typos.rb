be22e1a67618aff8a46cf699a74abe7ecea0811c
Walter Yu <kharma@gmail.com>
2012-07-14 19:33:00 -0700

    debugging integer types, unchanged - use text_box prawn method

--- db/migrate/20120416025512_fix_report_field_typos.rb
+++ db/migrate/20120416025512_fix_report_field_typos.rb
@@ -1,10 +1,10 @@
 class FixReportFieldTypos < ActiveRecord::Migration
-  # def change
-  #   Fixing typos in AddFieldsToReport migration file
-  #   remove_column :reports, :contactor_state,    :string
-  #   remove_column :reports, :contactor_zipcode,  :integer
-  #   add_column    :reports, :contractor_state,   :string
-  #   add_column    :reports, :contractor_zipcode, :integer
+  def change
+    # Fixing typos in AddFieldsToReport migration file
+    # remove_column :reports, :contactor_state,    :string
+    # remove_column :reports, :contactor_zipcode,  :integer
+    # add_column    :reports, :contractor_state,   :string
+    # add_column    :reports, :contractor_zipcode, :integer
 
   #   remove_column :reports, :water_polution_control_manager_emergancy_phone_number, :string
   #   add_column    :reports, :water_polution_control_manager_emergency_phone_number, :string
@@ -14,5 +14,5 @@ class FixReportFieldTypos < ActiveRecord::Migration
 
   #   remove_column :reports, :storm_information_percipitation_since_last_storm, :decimal
   #   remove_column :reports, :storm_information_precipitation_since_last_storm, :decimal
-  # end
+  end
 end
