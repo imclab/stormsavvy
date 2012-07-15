1291ee6fb9bdd63fa3821579426b565e655ca540
Walter Yu <kharma@gmail.com>
2012-07-15 14:47:10 -0700

    comment in report fields for  heroku database migrations

--- db/migrate/20120416032616_change_typos_for_report.rb
+++ db/migrate/20120416032616_change_typos_for_report.rb
@@ -1,21 +1,21 @@
 class ChangeTyposForReport < ActiveRecord::Migration
-  # def change
-  #   remove_column :reports, :water_polution_control_manager_name, 	:string
-  #   add_column :reports, :water_pollution_control_manager_name, 	:string
+  def change
+    remove_column :reports, :water_polution_control_manager_name, 	:string
+    add_column :reports, :water_pollution_control_manager_name, 	:string
 
-  #   remove_column :reports, :water_polution_control_manager_company_name, :string
-  #   add_column :reports, :water_pollution_control_manager_company_name,   :string
+    remove_column :reports, :water_polution_control_manager_company_name, :string
+    add_column :reports, :water_pollution_control_manager_company_name,   :string
 
-  #   remove_column :reports, :water_polution_control_manager_phone_number, :string
-  #   add_column :reports, :water_pollution_control_manager_phone_number,   :string
+    remove_column :reports, :water_polution_control_manager_phone_number, :string
+    add_column :reports, :water_pollution_control_manager_phone_number,   :string
 
-  #   remove_column :reports, :water_polution_control_manager_emergency_phone_number, :string
-  #   add_column :reports, :water_pollution_control_manager_emergency_phone_number,   :string
+    remove_column :reports, :water_polution_control_manager_emergency_phone_number, :string
+    add_column :reports, :water_pollution_control_manager_emergency_phone_number,   :string
 
-  #   remove_column :reports, :storm_information_expected_precipitaion_amount, :decimal
-  #   add_column :reports, :storm_information_expected_precipitation_amount,   :decimal
+    remove_column :reports, :storm_information_expected_precipitaion_amount, :decimal
+    add_column :reports, :storm_information_expected_precipitation_amount,   :decimal
 
-  #   remove_column :reports, :storm_information_percipitation_amount_from_last_form, :decimal
-  #   add_column :reports, :storm_information_precipitation_amount_from_last_form,    :decimal
-  # end
+    remove_column :reports, :storm_information_percipitation_amount_from_last_form, :decimal
+    add_column :reports, :storm_information_precipitation_amount_from_last_form,    :decimal
+  end
 end
