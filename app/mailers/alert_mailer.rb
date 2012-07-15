522b7303415cf92372cecb8ca0270e9bdccafb00
Walter Yu <kharma@gmail.com>
2012-07-15 14:34:47 -0700

    begin work on mailers

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,11 +1,6 @@
 class AlertMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default :from => "doolin@inventiumsystems.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.alert_mailer.pop.subject
-  #
   def pop
     @greeting = "Hi"
 
