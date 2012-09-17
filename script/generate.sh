0f4f430af1621b7b8a9b0a238df2aa98e7311ab5
Walter Yu <kharma@gmail.com>
2012-09-16 20:27:06 -0700

    move scripts into respective directory

--- script/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-# rails g scaffold Project user_id:integer name:string description:text startdate:datetime finishdate:datetime active:boolean
-
-# rails g model Dashboard user_id:integer preferences:text
-# rails g controller Dashboard index edit
-
-# rails g scaffold WeatherEvent site_id:integer eventdate:datetime duration:float rainfall:float inspected:boolean qualifying:boolean
-# rails g scaffold InspectionEvent site_id:integer type:string description:text date:datetime submitted:datetime 
