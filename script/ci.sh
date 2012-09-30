9f5c11162aab4914ed960c4a372d145fa206ac69
Walter Yu <kharma@gmail.com>
2012-09-30 16:22:14 -0700

    heroku deploy with bash script

--- script/ci.sh
+++ script/ci.sh
@@ -5,7 +5,7 @@
 # bash ci script.
 
 git add .
-git commit -m 'heroku deploy with bash'
+git commit -m 'heroku deploy with bash script'
 git push git@heroku.com:ssci.git master
 heroku run rake db:migrate --app ssci
 heroku logs --app ssci
