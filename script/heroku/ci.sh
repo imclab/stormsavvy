9711e101e6749b3c2e790227466ce936e352cf73
Walter Yu <kharma@gmail.com>
2012-10-13 10:09:14 -0700

    remove auto add and commit from ci script

--- script/heroku/ci.sh
+++ script/heroku/ci.sh
@@ -4,11 +4,8 @@
 
 # bash ci script.
 
-git add .
-git commit -m 'heroku deploy with bash script'
 git push git@heroku.com:ssci.git master
-heroku run rake db:migrate --app ssci
 heroku logs --app ssci
 
-./script/ci.sh
+.~/src/stormsavvy/script/ci.sh
 sleep 300
