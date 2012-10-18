e33b638a28b8eef5b90680bdbc44d0824604ca08
Walter Yu <kharma@gmail.com>
2012-10-18 00:29:17 -0700

    add staging deployment script

---
+++ script/heroku/staging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# deploy.sh
+
+# Bash staging deployment script.
+
+cd ~/src/stormsavvy
+git push git@heroku.com:stormsavvy2.git master
+heroku run rake db:migrate --app stormsavvy2
+heroku logs --app stormsavvy2
