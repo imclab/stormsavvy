67a09cad171dc7816dc587caf27059cbc8a629f1
Walter Yu <kharma@gmail.com>
2012-10-18 00:13:39 -0700

    update heroku deploy script

--- script/heroku/deploy.sh
+++ script/heroku/deploy.sh
@@ -8,9 +8,9 @@
 cd ~/src/stormsavvy
 git push git@heroku.com:stormsavvy2.git master
 heroku run rake db:migrate --app stormsavvy2
-heroku open --app stormsavvy2
+heroku logs --app stormsavvy2
 
 # Deploy to real production 
 git push git@heroku.com:stormsavvy.git master
 heroku run rake db:migrate --app stormsavvy
-heroku open --app stormsavvy
+heroku logs --app stormsavvy
