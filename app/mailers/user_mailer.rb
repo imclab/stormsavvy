63e50a4307ec154abb7a3561cf3c38237791debc
me <me@me.(none)>
2012-10-20 15:16:19 -0700

    update mailer info

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,26 +1,37 @@
 class UserMailer < ActionMailer::Base
-  default :from => "doolin@inventiumsystems.com"
-  #default :from => "alerts@stormsavvy.com"
-  #default :from => "david.doolin@gmail.com"
+  default :from => "alerts@stormsavvy.com"
+  # default :from => "doolin@inventiumsystems.com"
 
   def pop
-    @greeting = "Hello"
-    mail to: "david.doolin@gmail.com", :subject => 'booyah'
+    @greeting = "Greetings"
+    mail(
+      :to => "#{user.login} <#{user.email}>",
+      :subject => "Storm Savvy POP Alert"
+      )
   end
 
   def pop_alert(user)
-    @greeting = "Hello"
-    mail(:to => user.email, :subject => "Pop alerts")
+    @greeting = "Greetings"
+    mail(
+      :to => "#{user.login} <#{user.email}>",
+      :subject => "Storm Savvy POP Alert"
+      )
   end
 
   def noaa_alert(user)
-    @greeting = "Hello"
-    mail(:to => user.email, :subject => "Daily weather forecasts")
+    @greeting = "Greetings"
+    mail(
+      :to => "#{user.login} <#{user.email}>",
+      :subject => "Daily weather forecasts"
+      )
   end
 
   def pester_admins(email)
     @greeting = "Yo yo yo!"
-    mail(:to => email, :subject => 'Storm Savvy is working great today!')
+    mail(
+      :to => "walter@stormsavvy.com",
+      :subject => 'Storm Savvy is working great today!'
+      )
   end
 
   def mailout(to = nil)
@@ -28,7 +39,10 @@ class UserMailer < ActionMailer::Base
     @users.each do |user|
       @user = user # `@user` is needed for the template
       if user.has_site?
-        mail(:to => user.email, :subject => "Project estimate").deliver
+        mail(
+          :to => user.email, 
+          :subject => "Project Status"
+          ).deliver
       end
     end
   end
