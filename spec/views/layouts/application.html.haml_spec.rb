4d7f346cfd21aae2186d0e9f9a10f8939581b9d0
me <me@me.(none)>
2012-10-20 14:34:15 -0700

    edit application layout view spec

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -3,13 +3,15 @@
 require 'spec_helper'
 
 describe "layouts/application.html.haml" do
-  before do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
     stub_template "shared/_cta-bar.html.haml" => 'sidebar template'
     stub_template "shared/_flash_messages.html.haml" => 'flash message template'
   end
   
   it "renders the sidebar" do
-    render
+    render 
     rendered.should contain 'sidebar template'
   end
   
