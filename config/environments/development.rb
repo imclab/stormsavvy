68cc9cbdc39260974f0db5e3b9f886236e767e77
Walter Yu <kharma@gmail.com>
2012-10-24 08:39:51 -0700

    change mailer development settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,12 +35,18 @@ Stormsavvy::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.delivery_method = :letter_opener
-  #config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # Testing development with production mailer settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    :host => 'stormsavvy.com'
+    }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
 
 end
