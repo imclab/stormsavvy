1d43c299c069b887f1293283b0c8db0dd4bda60a
Walter Yu <kharma@gmail.com>
2012-10-27 00:00:14 -0700

    setting up config/config.yml in production

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -8,16 +8,16 @@
 	  :authentication       => 'plain',
 	  :enable_starttls_auto => true
 	}
-else
-	ActionMailer::Base.smtp_settings = {
-	  :user_name 						=> ENV['STORMSAVVY_GMAIL_USERNAME'],
-	  :password 						=> ENV['STORMSAVVY_GMAIL_PASSWORD'],
-	  :address              => "smtp.gmail.com",
-	  :port                 => 587,
-	  :domain               => 'stormsavvy.com',
-	  :authentication       => 'plain',
-	  :enable_starttls_auto => true
-	}
+	
+	# ActionMailer::Base.smtp_settings = {
+	#   :user_name 						=> ENV['STORMSAVVY_GMAIL_USERNAME'],
+	#   :password 						=> ENV['STORMSAVVY_GMAIL_PASSWORD'],
+	#   :address              => "smtp.gmail.com",
+	#   :port                 => 587,
+	#   :domain               => 'stormsavvy.com',
+	#   :authentication       => 'plain',
+	#   :enable_starttls_auto => true
+	# }
 
 	# ActionMailer::Base.smtp_settings = {
 	#   :user_name 						=> ENV['SENDGRID_USERNAME'],
