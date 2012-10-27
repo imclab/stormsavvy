4c5a6660862c76158b90d2c32424b061ba1621a7
Walter Yu <kharma@gmail.com>
2012-10-26 23:49:33 -0700

    rename heroku deployment scripts

--- script/heroku/staging.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# deploy.sh
-# Bash staging deployment script.
-
-cd ~/src/stormsavvy
-git push git@heroku.com:stormsavvy2.git master
-heroku run rake db:migrate --app stormsavvy2
-heroku logs --app stormsavvy2
