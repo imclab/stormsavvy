d0e06ef392375c67ededd1ae883bfbfe5a433cc3
Walter Yu <kharma@gmail.com>
2012-10-26 23:48:36 -0700

    setting up gmail on development and production

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,20 +1,32 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'stormsavvy.com',
-  :user_name            => APP_CONFIG['STORMSAVVY_GMAIL_USERNAME'],
-  :password             => APP_CONFIG['STORMSAVVY_GMAIL_PASSWORD'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
+if Rails.env == "development"
+# # Authenticates using config/config.yml file
+	ActionMailer::Base.smtp_settings = {
+		:user_name            => APP_CONFIG['STORMSAVVY_GMAIL_USERNAME'],
+	  :password             => APP_CONFIG['STORMSAVVY_GMAIL_PASSWORD'],
+	  :address              => "smtp.gmail.com",
+	  :port                 => 587,
+	  :domain               => 'stormsavvy.com',
+	  :authentication       => 'plain',
+	  :enable_starttls_auto => true
+	}
+else
+	ActionMailer::Base.smtp_settings = {
+	  :user_name 						=> ENV['STORMSAVVY_GMAIL_USERNAME'],
+	  :password 						=> ENV['STORMSAVVY_GMAIL_PASSWORD'],
+	  :address              => "smtp.gmail.com",
+	  :port                 => 587,
+	  :domain               => 'stormsavvy.com',
+	  :authentication       => 'plain',
+	  :enable_starttls_auto => true
+	}
 
-# Sendgrid smtp settings
-# ActionMailer::Base.smtp_settings = {
-#   :user_name 				=> ENV['SENDGRID_USERNAME'],
-#   :password 				=> ENV['SENDGRID_PASSWORD'],
-#   :domain 				=> "heroku.com",
-#   :address 				=> "smtp.sendgrid.net",
-#   :port 					=> 587,
-#   :authentication 		=> :plain,
-#   :enable_starttls_auto 	=> true
-# }
+	# ActionMailer::Base.smtp_settings = {
+	#   :user_name 						=> ENV['SENDGRID_USERNAME'],
+	#   :password 						=> ENV['SENDGRID_PASSWORD'],
+	#   :domain 							=> "heroku.com",
+	#   :address 							=> "smtp.sendgrid.net",
+	#   :port 								=> 587,
+	#   :authentication 			=> :plain,
+	#   :enable_starttls_auto => true
+	# }
+end
