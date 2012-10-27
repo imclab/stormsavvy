389c8bf97f635f956552c579747e087c7dd116fe
Walter Yu <kharma@gmail.com>
2012-10-26 23:59:41 -0700

    setting up config/config.yml in production

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,4 +1,2 @@
 # Loads yaml password configuration file based on Railscasts #85
-if Rails.env == "development"
-	APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-end
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
