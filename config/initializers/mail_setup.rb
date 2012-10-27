dc4bd261b11c1306df88525f934d0b4d7531a4bd
Walter Yu <kharma@gmail.com>
2012-10-26 18:43:27 -0700

    load APP_CONFIG constant for mailer username and password

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -2,8 +2,19 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'stormsavvy.com',
-  :user_name            => ENV["STORMSAVVY_GMAIL_USERNAME"],
-  :password             => ENV["STORMSAVVY_GMAIL_PASSWORD"],
+  :user_name            => APP_CONFIG['STORMSAVVY_GMAIL_USERNAME'],
+  :password             => APP_CONFIG['STORMSAVVY_GMAIL_PASSWORD'],
   :authentication       => 'plain',
   :enable_starttls_auto => true
 }
+
+# Sendgrid smtp settings
+# ActionMailer::Base.smtp_settings = {
+#   :user_name 				=> ENV['SENDGRID_USERNAME'],
+#   :password 				=> ENV['SENDGRID_PASSWORD'],
+#   :domain 				=> "heroku.com",
+#   :address 				=> "smtp.sendgrid.net",
+#   :port 					=> 587,
+#   :authentication 		=> :plain,
+#   :enable_starttls_auto 	=> true
+# }
