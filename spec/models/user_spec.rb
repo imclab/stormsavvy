b35bf3fb0ce3900b97a49cf2dad4c78672a856a4
Walter Yu <kharma@gmail.com>
2012-10-28 15:19:16 -0700

    editing user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,7 +43,14 @@ describe User do
   context :has_site do
 
     before(:each) do 
-      @user = FactoryGirl.create(:user)
+      @user = User.create(
+        :email => 'foo@bar.com', 
+        :password => 'foobarbaz',
+        :password_confirmation => 'foobarbaz'
+        )
+
+      # TODO: Resolve duplicate user factory table error.
+      # @user = FactoryGirl.create(:user)
     end
 
     it "should respond to has_site?" do
