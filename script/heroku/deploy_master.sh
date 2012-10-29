daa14394a131629c486fd0bcce4be7dd065bf102
me <me@me.(none)>
2012-10-29 06:16:31 -0700

    remove heroku and repo script from .gitignore

---
+++ script/heroku/deploy_master.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Bash script for deploying master branch to heroku
+
+git push git@heroku.com:stormsavvy.git master
---
+++ script/repo/deploy_master.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Bash script for deploying master branch
+
+git push bitbucket master
