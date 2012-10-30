f5035011cf3b5eb610a38adb5c0a0fb86737d6bd
Walter Yu <kharma@gmail.com>
2012-10-30 15:27:07 -0700

    relocate user factory to spec/factories/user.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
-# FactoryGirl.define do
-#   factory :user do
-#     firstname             "Walter"
-#     lastname              "Yu"
-#     email                 "walter@stormsavvy.com"
-#     password              "DarkAndStormy"
-#     password_confirmation "DarkAndStormy"
-#     # remember_me           :true
-#   end
-# end
+FactoryGirl.define do
+  factory :user do
+    firstname "Walter"
+    lastname "Yu"
+    email "walter@stormsavvy.com"
+    password "DarkAndStormy"
+    password_confirmation "DarkAndStormy"
+    remember_me :true
+  end
+end
