fec10bb6fa653f5aab07bd70c0f1f2defd89353f
Walter Yu <kharma@gmail.com>
2012-10-30 15:25:51 -0700

    relocate user factory to spec/factories/user.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
 # By using the symbol ':member', we get Factory Girl to simulate the Member model.
 # This is from the Rails Tutorial Listing 7.16
-FactoryGirl.define do
-  factory :user do |u|
-    u.email                 "david.doolin+3@example.com"
-    u.password              "foobar"
-    u.firstname             "Dave"
-    u.lastname              "Doolin"
-  end
-end
+# FactoryGirl.define do
+#   factory :user do |u|
+#     u.email                 "david.doolin+3@example.com"
+#     u.password              "foobar"
+#     u.firstname             "Dave"
+#     u.lastname              "Doolin"
+#   end
+# end
 
 #FactoryGirl.sequence :email do |n|
 #  "person-#{n}@example.com"
