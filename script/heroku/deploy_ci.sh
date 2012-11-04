f51166dd967e686aa6a30613ea38b918db9badec
Walter Yu <kharma@gmail.com>
2012-11-03 22:17:14 -0700

    update heroku ci script

--- script/heroku/deploy_ci.sh
+++ script/heroku/deploy_ci.sh
@@ -8,5 +8,5 @@ git push git@heroku.com:stormsavvy3.git master
 # heroku restart --app stormsavvy3 
 heroku logs --app stormsavvy3
 
-.~/src/stormsavvy/script/heroku/deploy_ci.sh
+./deploy_ci.sh
 sleep 300
