3ef8f9ed38ad8fe7a9ed8d84328f58b723749aa5
Walter Yu <kharma@gmail.com>
2012-11-13 23:41:55 -0800

    start test coverage for pdfs

--- spec/pdfs/first_report_spec.rb
+++ spec/pdfs/first_report_spec.rb
@@ -4,11 +4,23 @@ describe FirstReport do
 
   before(:each) do
     @report = FactoryGirl.create(:report)
-    @site = @project.sites.create!({:name => 'EC Jungle Gym', 
-      :zipcode => '94530'})
+    @filename1 = "#{Prawn::DATADIR}/images/reports/CEM2030-2012_Page_01.png"
+    @img_data = File.open(@filename1, "rb") { |f| f.read }
   end
 
   describe :build_report do
+  	it "builds all pages of pdf" do
+      create_pdf
+    end
+  end
+   
+  it "should read the basic attributes correctly" do
+    jpg = Prawn::Images::JPG.new(@img_data)
     
+    jpg.should be_valid
+    # jpg.width.should == 604
+    # jpg.height.should == 453
+    # jpg.bits.should == 8
+    # jpg.channels.should == 3
   end
 end
