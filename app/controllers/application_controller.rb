ec0d834de7f04592b216bd14d543781bfba09d5e
Walter Yu <kharma@gmail.com>
2012-11-13 23:41:02 -0800

    starting redirect method for pdf app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_subdomain
+=begin
+  def redirect_subdomain
+    if request.env["HTTP_HOST"] == "pdf.stormsavvy.com"
+      redirect_to "http://pdfff.herokuapp.com/"
+    end
+  end
+=end
   def title
     "StormSavvy"
   end
