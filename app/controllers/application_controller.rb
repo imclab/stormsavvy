2cc94ee6686b45126a1040d63cfd2eda6912e0dd
Walter Yu <kharma@gmail.com>
2012-11-15 15:10:27 -0800

    remove redirect_subdomain method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :redirect_subdomain
-=begin
-  def redirect_subdomain
-    if request.env["HTTP_HOST"] == "pdf.stormsavvy.com"
-      redirect_to "http://pdfff.herokuapp.com/"
-    end
-  end
-=end
   def title
     "StormSavvy"
   end
