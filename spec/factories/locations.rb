7e9f9b3243aedbb7a3fb0fa745fa0ea457c90aed
Walter Yu <kharma@gmail.com>
2012-11-15 21:56:50 -0800

    update location factory

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,11 +2,11 @@ FactoryGirl.define do
   factory :location do
     address_1  '6016 B Avila St.'
     address_2  '6016 B Avila St.'
-    city     'El Cerrito'
-    state    'CA'
-    zipcode  94530
-    lat      37.9202057
-    long     -122.293742
-    name     'El Cerrito'
+    city 'El Cerrito'
+    state 'CA'
+    zipcode 94530
+    lat 37.9202057
+    long -122.293742
+    name 'El Cerrito'
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,14 +3,7 @@ require 'spec_helper'
 describe Location do
 
   before(:each) do
-    # @location = FactoryGirl.create(:location)
-    @location = FactoryGirl.create({
-      :address_1 => '6016 B Avila St.',
-      :address_2 => '6016 B Avila St.',
-      :city      => 'El Cerrito',
-      :state     => 'CA',
-      :zipcode   => 94530 
-    })
+    @location = FactoryGirl.create(:location)
   end
 
   it "should have a valid location" do
@@ -18,15 +11,6 @@ describe Location do
   end
 
   it "should geocode on the address" do
-=begin
-    location = Location.new({ :address_1 => '6016 B Avila St.',
-                              :address_2 => '6016 B Avila St.',
-                              :city      => 'El Cerrito',
-                              :state     => 'CA',
-                              :zipcode   => 94530 })
-    location.should be_valid
-    location.save
-=end
     @location.lat.should  == 37.902889
     @location.long.should == -122.304598
   end
