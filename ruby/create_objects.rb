c67ec19d1f9061a72002c9da9d6d519bbeb23b3a
Walter Yu <kharma@gmail.com>
2012-11-18 13:52:32 -0800

    add ruby script dir

---
+++ ruby/create_objects.rb
@@ -0,0 +1,49 @@
+# Run inside console for testing nested attributes and mailers.
+
+user.delete
+project.delete
+site.delete
+site2.delete
+reload!
+user = User.new(
+	:email => 'walter@stormsavvy1.com',
+	:password => 'foobar1'
+	)
+user.save
+project = user.projects.new(
+  :name => 'ec park and rec',
+  :description => 'playground improvements',
+  :startdate => DateTime.new(2011),
+  :finishdate => DateTime.new(2012)
+  )
+project.save
+site = project.sites.new(
+  :name => 'ec jungle gym',
+  :zipcode => 94530
+  )
+site.save
+site2 = project.sites.new(
+  :name => 'ec playground', 
+  :zipcode => 94530
+  )
+site2.save
+
+sites = user.sites
+puts sites
+
+user.sites.each do |s|
+  @site = pp s.name
+  pp @site
+end
+
+we = WeatherEvent.new
+we.save
+
+project = Project.new
+sites = @project.sites.build
+
+report = ReportsPdf.new
+report.testem_fields
+
+email = "walter@stormsavvy.com"
+UserMailer.pester_admins(email)
