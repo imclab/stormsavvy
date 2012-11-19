d234180949758aaecdbe797c449fd2ff062e48d5
Dave Doolin <david.doolin@gmail.com>
2012-11-18 17:35:21 -0800

    uncouple the noaa forecast API

--- lib/weather/noaa_forecast.rb
+++ lib/weather/noaa_forecast.rb
@@ -7,19 +7,18 @@ class NOAAForecast
   include Typhoeus
   include Nokogiri
 
-  attr_reader :noaa_forecast, :xml
 
   def initialize(zipcode, duration = 168, interval = 6)
     @zipcode  = zipcode
     @duration = duration
     @interval = interval
-    @xml = ""
   end
 
   def seven_day_weather
     latlong = get_lat_long(@zipcode)
     response = ping_noaa(latlong, 168, 6)
-    get_weather_data(response.body)
+    forecast = parse_weather_data(response)
+    return forecast
   end
 
   def get_lat_long(zipcode)
@@ -29,17 +28,13 @@ class NOAAForecast
   end
 
   def forecast(lat, long)
-    latlong = [] << lat << long  
+    latlong = [] << lat << long
     response = ping_noaa(latlong, 168, 6)
-    get_weather_data(response.body)
-  end
-
-  def xml
-    @xml
+    return parse_weather_data(response)
   end
 
   def ping_noaa(latlong, duration, interval)
-    @xml = "http://www.wrh.noaa.gov/forecast/xml/xml.php?duration=#{duration}&interval=#{interval}&lat=#{latlong[0]}&lon=#{latlong[1]}"
+    xml = "http://www.wrh.noaa.gov/forecast/xml/xml.php?duration=#{duration}&interval=#{interval}&lat=#{latlong[0]}&lon=#{latlong[1]}"
     request = Typhoeus::Request.new(xml,
               :body          => "this is a request body",
               :method        => :post,
@@ -50,15 +45,15 @@ class NOAAForecast
     hydra = Typhoeus::Hydra.new
     hydra.queue(request)
     hydra.run
-    #response = request.response
-    return request.response
+    return request.response.body
   end
 
-  def get_weather_data(xmldoc)
+  def parse_weather_data(xmldoc)
     doc   = Nokogiri::XML(xmldoc)
     pop = doc.xpath("//pop").map { |n| n.content.to_i }
     qpf = doc.xpath("//qpf").map { |n| n.content.to_i }
-    @noaa_forecast =  [] << pop << qpf
+    noaa_forecast =  [] << pop << qpf
+    return noaa_forecast
   end
 
 end
--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -18,20 +18,6 @@ describe NOAAForecast do
     nf.class.should == NOAAForecast
   end
 
-  xit "NOAA weather forecast for one week should have 29 elements" do
-    nf = NOAAForecast.new(94530,168,6)
-    nf.seven_day_weather
-    puts nf.xml
-    #p nf.noaa_forecast[0].size
-    nf.noaa_forecast[0].size.should == @fullcount
-  end
-
-  it "NOAA weather forecast blows its gourd if given nil zipcode" do
-    #nf = NOAAForecast.new(94530,168,6)
-    @nf.seven_day_weather
-    @nf.noaa_forecast[0].size.should == @fullcount
-  end
-
   it "returns latitude and longitude for a given zipcode" do
     latlong = @nf.get_lat_long 94530
     latlong.size.should == 2
@@ -40,9 +26,11 @@ describe NOAAForecast do
     latlong[1].should be_within(tol).of(-122.2937428)
   end
 
-  it "gets data from noaa" do
-   response = @nf.ping_noaa([-122.0, 38.00], 168, 6)
-   # PUT AN ASSERTION HERE
+  it "parses weather data from noaa for one week" do
+    response = @nf.ping_noaa([-122.0, 38.00], 168, 6)
+    nf = NOAAForecast.new(94530,168,6)
+    forecast = nf.parse_weather_data(response)
+    forecast[0].size.should == @fullcount
   end
 
 end
