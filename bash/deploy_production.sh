7da53c56b49766c4e994161206c5bdb4dfeaa108
Walter Yu <kharma@gmail.com>
2012-11-25 19:49:13 -0800

    organize deployment scripts

--- bash/deploy_production.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Bash staging deployment script.
-
-git push git@heroku.com:stormsavvy.git master
-heroku logs --app stormsavvy
--- bash/deploy_staging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Bash staging deployment script.
-
-cd ~/src/stormsavvy
-git push git@heroku.com:stormsavvy2.git master
-heroku run rake db:migrate --app stormsavvy2
-heroku logs --app stormsavvy2
