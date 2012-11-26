3a6f4e60b9787b4be6a6f23e0b280be7e8962811
Dave Doolin <david.doolin@gmail.com>
2012-11-25 16:55:32 -0800

    noaa alert email now passing spec

--- spec/views/alert_mailer/noaa_alert.html.haml_spec.rb
+++ spec/views/alert_mailer/noaa_alert.html.haml_spec.rb
@@ -4,7 +4,7 @@ describe "alert_mailer/noaa_alert" do
 
   it "renders the noaa alert email view" do
     render
-    rendered.should =~ /jghslafkjgalkjsfhl/
+    rendered.should =~ /Future mailer with NOAA forecast/
   end
 
 end
