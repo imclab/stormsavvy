1e30e4258272d3a1a71d32d2093eed6524a86218
Dave Doolin <david.doolin@gmail.com>
2012-11-25 19:54:15 -0800

    moved back to letter_operer for testing emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,10 +55,10 @@ Stormsavvy::Application.configure do
     :domain               => 'stormsavvy.com',
     :authentication       => 'plain',
     :enable_starttls_auto => true
-    }
+  }
 
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default :charset => "utf-8"
