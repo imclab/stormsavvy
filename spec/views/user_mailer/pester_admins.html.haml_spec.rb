ec6e50c272415b89670a8dc66ddf4a6414277bb5
Dave Doolin <david.doolin@gmail.com>
2012-12-02 14:20:11 -0800

    pester admin view template and spec updated

--- spec/views/user_mailer/pester_admins.html.haml_spec.rb
+++ spec/views/user_mailer/pester_admins.html.haml_spec.rb
@@ -7,9 +7,17 @@ describe "user_mailer/pester_admins" do
     rendered.should =~ /pester admin email/
   end
 
-  it "contains table" do
+  it "contains  a projects table" do
     render
-    rendered.should have_selector 'td'
-    rendered.should have_selector 'tr'
+    rendered.should have_selector 'table.projects'
+    rendered.should have_selector 'th', :text => "Projects"
   end
+
+  it "contains  a forecast table" do
+    render
+    rendered.should have_selector 'table.forecast'
+    rendered.should have_selector 'th', :text => "Forecast"
+  end
+
+
 end
