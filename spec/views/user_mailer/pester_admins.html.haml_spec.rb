0a560d7e0e541b08550bb42b2b2a7c77900d9bbf
Walter Yu <kharma@gmail.com>
2012-12-02 14:02:22 -0800

    spec out pester admin mailer view

--- spec/views/user_mailer/pester_admins.html.haml_spec.rb
+++ spec/views/user_mailer/pester_admins.html.haml_spec.rb
@@ -7,4 +7,9 @@ describe "user_mailer/pester_admins" do
     rendered.should =~ /pester admin email/
   end
 
+  it "contains table" do
+    render
+    rendered.should have_selector 'td'
+    rendered.should have_selector 'tr'
+  end
 end
