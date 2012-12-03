414ece2ddbd07e800926237f3dafaf9dee09218f
Dave Doolin <david.doolin@gmail.com>
2012-12-02 17:02:01 -0800

    cleaned up the view specs for the admin emailer

--- spec/views/user_mailer/pester_admins.html.haml_spec.rb
+++ spec/views/user_mailer/pester_admins.html.haml_spec.rb
@@ -4,19 +4,27 @@ describe "user_mailer/pester_admins" do
 
   it "greet the admin user" do
     render
-    rendered.should =~ /pester admin email/
+    rendered.should =~ /daily admin email/
   end
 
   it "contains a projects table" do
     render
-    rendered.should have_selector 'table.projects'
+    rendered.should have_selector 'table'
     rendered.should have_selector 'th', :text => "Projects"
   end
 
   it "contains a forecast table" do
     render
-    rendered.should have_selector 'table.forecast'
+    rendered.should have_selector 'table'
     rendered.should have_selector 'th', :text => "Forecast"
   end
 
+end
+
+describe "user_mailer/_pester_intro_block" do
+
+  it "renders the intro partial" do
+    render
+    rendered.should have_content "Storm Sumo"
+  end
 end
