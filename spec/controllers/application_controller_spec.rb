69c3168dd5df286262f497bd857975e33b0fcc68
Walter Yu <kharma@gmail.com>
2012-12-11 20:20:31 -0800

    update application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  render_views
 
 	describe "layouts/application.html.haml" do
 	  before(:each) do
 	    @user = FactoryGirl.create(:user)
 	    sign_in @user
-	    stub_template "shared/_cta-bar.html.haml" => 'sidebar template'
-	    stub_template "shared/_flash_messages.html.haml" => 'flash message template'
+      get :index
+	    # stub_template "shared/_cta-bar.html.haml" => 'sidebar template'
+	    # stub_template "shared/_flash_messages.html.haml" => 'flash message template'
 	  end
 
 	  it "renders the sidebar" do
-	    render
-	    rendered.should contain_template 'sidebar template'
+	    response.should render_template 'sidebar template'
 	  end
-	  
 	end
 end
