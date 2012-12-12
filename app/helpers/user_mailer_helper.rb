57dee9a7c074f688a0e2fc759007425bba07faa0
Walter Yu <kharma@gmail.com>
2012-12-11 21:20:08 -0800

    update user mailer and spec

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -2,8 +2,9 @@ module UserMailerHelper
 
  # def forecast_array
  # 	@weather = NOAAForecast.new(94605)
- #  	@forecast = @weather.seven_day_weather[0].to_s
- #  end
+ #  @forecast = @weather.seven_day_weather[0].to_s
+ # end
+
  @pester_forecast = [{ :date => "Date.today", :weather => "90" }]
 
   def fa
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,13 @@ class UserMailer < ActionMailer::Base
 
     @greeting = "Greetings"
 
-    @forecast1 = [{ :date => "Date.today", :weather => "90" }]
+    @forecast1 = [{ :date => Date.today, :weather => "90" },
+                  { :date => Date.today + 1.day, :weather => "85"},
+                  { :date => Date.today + 2.day, :weather => "80"},
+                  { :date => Date.today + 3.day, :weather => "75"},
+                  { :date => Date.today + 4.day, :weather => "70"},
+                  { :date => Date.today + 5.day, :weather => "65"},
+                  { :date => Date.today + 6.day, :weather => "60"}]
 
     mail(
       :from     => "alerts@stormsavvy.com",
