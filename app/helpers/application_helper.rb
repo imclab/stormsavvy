6c65ccf0dc33ba94c7b4e1c74168894b1e444805
me <me@me.(none)>
2013-01-13 00:12:28 -0800

    add user sign-up to landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,4 +38,16 @@ module ApplicationHelper
         type.to_s
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
