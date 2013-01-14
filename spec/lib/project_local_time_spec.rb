fe6fc381f844a2b8ce4d22a865375f1d924f6bd6
me <me@me.(none)>
2013-01-13 18:10:14 -0800

    add project local time spec

---
+++ spec/lib/project_local_time_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'project_local_time'
+
+describe ProjectLocalTime do
+
+  it "formats date and time for email" do
+    datetime = "Sunday, 13 January 2013 18:00 PST"
+    time = Time.new(2013, 01, 13, 18, 0, 0)
+    ProjectLocalTime::format_projectime(time).should == datetime
+    puts time
+  end
+end
