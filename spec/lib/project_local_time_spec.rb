68ec7517e88ea1ceffba823ca97be70b43b4ddf2
me <me@me.(none)>
2013-01-13 20:28:22 -0800

    edit strftime

--- spec/lib/project_local_time_spec.rb
+++ spec/lib/project_local_time_spec.rb
@@ -6,7 +6,7 @@ describe ProjectLocalTime do
   it "formats date and time for email" do
     datetime = "Sunday, 13 January 2013 18:00 PST"
     time = Time.new(2013, 01, 13, 18, 0, 0)
-    ProjectLocalTime::format_projectime(time).should == datetime
+    ProjectLocalTime::format_project_time(time).should == datetime
     puts time
   end
 end
