b318d9055e5fc8513550dfe4b6a5c4e7774fc131
Walter Yu <kharma@gmail.com>
2013-01-20 10:55:45 -0800

    add devise login helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,4 +10,20 @@ describe ApplicationHelper do
     helper.twitterized_type(:success).should == "success"
   end
 
+  it "has devise login help methods" do
+    helper.should respond_to :resource_name
+    helper.should respond_to :resource
+    helper.should respond_to :devise_mapping
+  end
+
+  it "has user and devise resource helpers" do
+    user = self.resource_name
+    user.should == :user
+
+    resource= self.resource
+    resource.should == @resource
+
+    devise_mapping = self.devise_mapping
+    devise_mapping.should == @devise_mapping
+  end
 end
