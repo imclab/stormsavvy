a81fcaf3e37f741df68534e8eea3983effbeeffd
Walter Yu <kharma@gmail.com>
2013-01-19 21:54:49 -0800

    add hero partial spec

---
+++ spec/views/pages/_hero.html.haml_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "pages/index" do
+
+=begin
+  def site_attributes
+    { :name => 'Foo bar', :zipcode => '94530' }
+  end
+
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @p1 = FactoryGirl.create(:project)
+    @p2 = FactoryGirl.create(:project)
+    @projects = [@p1, @p2]
+    @needs_attention_reports = Report.needs_attention
+    #@s1 = @p1.sites.create(site_attributes)
+    #@s2 = @p2.sites.create(site_attributes)
+    sign_in @user
+  end
+=end
+
+  describe "index page components" do
+
+    it "renders cta-bar partial" do
+      render_template 'pages/_hero'
+      # rendered.should have_selector('div#modules')
+      # rendered.should have_selector('div#hero-unit')
+      # rendered.should =~ /Active Projects/
+    end
+
+    it "renders maincopy partial" do
+      # render
+      # rendered.should have_selector('div#dashboard-sidebar')
+      # view.should render_template('pages/_maincopy')
+    end
+
+    it "shows correct links on index page" do
+      render_template 'pages/_hero'
+      # rendered.should have_selector('div#homeContent')
+      rendered.should =~ /Making SWPPP Easy/
+      rendered.should =~ /Free Alerts & Reports By Email/
+      rendered.should =~ /Sign Up Now For Free Access/
+    end
+  end
+end
