537cb324a166db0c7054651a3723fe1c1cbca0c8
Walter Yu <kharma@gmail.com>
2013-01-20 13:20:27 -0800

    pass local time spec

--- spec/lib/project_local_time_spec.rb
+++ spec/lib/project_local_time_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 require 'project_local_time'
 
 describe ProjectLocalTime do
+  before :each do
+    time = Time.new(2013, 01, 13, 18, 0, 0)
+    @local_time = ProjectLocalTime::format(time)
+  end
 
   it "formats date and time for email" do
     datetime = "Sunday, 13 January 2013 18:00 PST"
     time = Time.new(2013, 01, 13, 18, 0, 0)
-    ProjectLocalTime::format_project_time(time).should == datetime
-    puts time
+    ProjectLocalTime::format(time).should == datetime
+    @local_time.should == datetime
   end
 end
