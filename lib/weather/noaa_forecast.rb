02920d3b6656326abd08673c8d124975962496b9
wyu <kharma@gmail.com>
2013-01-20 20:51:36 -0800

    arrays work in spec but not in method

--- lib/weather/noaa_forecast.rb
+++ lib/weather/noaa_forecast.rb
@@ -38,14 +38,10 @@ class NOAAForecast
 
   def get_time_array
     nf = NOAAForecast.new(94530,168,6)
+    pop2 = nf.get_forecast_array
     pt = []
-=begin
-    pop.each_with_index do |(i, p), index|
-      pt << { ProjectLocalTime::format(Date.today + (index*6).hours) => i } << { nf.seven_day_weather => p }
-    end
-=end
-    pop.each_with_index do |(i, p), index|
-      pt << { :date => ProjectLocalTime::format(Date.today + (index*6).hours), :weather => i  }
+    pop2.each_with_index do |(i, p), index|
+      pt << { :date => ProjectLocalTime::format(Date.today + (index*6).hours), :weather => i.to_s  }
     end
   end
 
