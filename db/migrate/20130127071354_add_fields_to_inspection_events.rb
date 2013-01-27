7e447b85dc302176c7ee703bb2a309a74c58a0b0
wyu <kharma@gmail.com>
2013-01-27 11:48:14 -0800

    add and run inspection_events migration

---
+++ db/migrate/20130127071354_add_fields_to_inspection_events.rb
@@ -0,0 +1,22 @@
+class AddFieldsToInspectionEvents < ActiveRecord::Migration
+  def change
+    # add_column :inspection_events, :site_id,                :integer
+    add_column :inspection_events, :inspection_type,        :string
+    add_column :inspection_events, :inspection_description, :text
+    add_column :inspection_events, :inspection_date,        :datetime
+    add_column :inspection_events, :submitted_by,           :string
+    add_column :inspection_events, :inspected_by,           :string
+=begin
+    create_table :inspection_events do |t|
+      t.integer :site_id
+      t.string :inspection_type
+      t.text :inspection_description
+      t.datetime :inspection_date
+      t.string :submitted_by
+      t.string :inspected_by
+
+      t.timestamps
+    end
+=end
+  end
+end
