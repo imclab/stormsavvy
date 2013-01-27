758429ebf9a62ec9a5bbb76bdc6e4a52858fe114
wyu <kharma@gmail.com>
2013-01-26 23:43:00 -0800

    update logo image link

--- spec/features/inspection_events_spec.rb
+++ spec/features/inspection_events_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe "InspectionEvents" do
   describe "GET /inspection_events" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get inspection_events_path
-      response.status.should be(200)
+    it "visits inspection_events path" do
+      visit inspection_events_path
+      current_path.should == inspection_events_path
     end
   end
 end
--- spec/features/weather_events_spec.rb
+++ spec/features/weather_events_spec.rb
@@ -2,11 +2,9 @@ require 'spec_helper'
 
 describe "WeatherEvents" do
   describe "GET /weather_events" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "visits weather_events path" do
       visit weather_events_path
-      last_response.should be_successful
-      # response.status.should be(200)
+      current_path.should == weather_events_path
     end
   end
 end
