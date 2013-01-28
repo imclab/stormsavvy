41c94e51cbde7d4a8c87db044456a8439f18e757
wyu <kharma@gmail.com>
2013-01-27 18:10:09 -0800

    add sampling event model spec

--- spec/models/sampling_event_spec.rb
+++ spec/models/sampling_event_spec.rb
@@ -40,14 +40,50 @@ describe SamplingEvent do
     :analysis_type_5 => "pH",
     :analysis_result_5 => "7.20",
     :analysis_average_5 => "7.01"
-=begin
-	  :site_id => 1,
-	  :inspection_type => "weekly",
-	  :inspection_description => "ec jungle gym inspection",
-	  :inspection_date => "2013-01-26 23:13:55",
-	  :submitted_by => "wyu",
-	  :inspected_by => "wyu"
-=end
   	}
+  end
+
+  it "has correct attributes" do
+    @sampling_event.site_id.should == 1
+    @sampling_event.sampling_type.should == "pH"
+    @sampling_event.sampling_description.should == "ec jungle gym samplng"
+    @sampling_event.sampling_date.should == "2013-01-27 15:34:07"
+    @sampling_event.submitted_by.should == "wyu"
+    @sampling_event.sampled_by.should => "wyu"
+    @sampling_event.sample_id_1.should => 1
+    @sampling_event.sample_location_1.should => "jungle gym"
+    @sampling_event.sample_time_1.should => "2013-01-27 15:34:07"
+    @sampling_event.analysis_type_1.should => "pH"
+    @sampling_event.analysis_result_1.should => "7.00"
+    @sampling_event.analysis_average_1.should => "7.01"
+    @sampling_event.sample_id_2.should => 1
+    @sampling_event.sample_location_2.should => "playground slide"
+    @sampling_event.sample_time_2.should => "2013-01-27 15:34:07"
+    @sampling_event.analysis_type_2.should => "pH"
+    @sampling_event.analysis_result_2.should => "7.10"
+    @sampling_event.analysis_average_2.should => "7.01"
+    @sampling_event.sample_id_3.should => 1
+    @sampling_event.sample_location_3.should => "playground seesaw"
+    @sampling_event.sample_time_3.should => "2013-01-27 15:34:07"
+    @sampling_event.analysis_type_3.should => "pH"
+    @sampling_event.analysis_result_3.should => "6.90"
+    @sampling_event.analysis_average_3.should=> "7.01"
+    @sampling_event.sample_id_4.should => 1
+    @sampling_event.sample_location_4.should => "merry go round"
+    @sampling_event.sample_time_4.should => "2013-01-27 15:34:07"
+    @sampling_event.analysis_type_4.should => "pH"
+    @sampling_event.analysis_result_4.should => "6.95"
+    @sampling_event.analysis_average_4.should => "7.01"
+    @sampling_event.sample_id_5.should => 1
+    @sampling_event.sample_location_5.should => "playground swings"
+    @sampling_event.sample_time_5.should => "2013-01-27 15:34:07"
+    @sampling_event.analysis_type_5.should => "pH"
+    @sampling_event.analysis_result_5.should => "7.20"
+    @sampling_event.analysis_average_5.should => "7.01"
+  end
+  it "creates new instance given valid attributes" do
+    sampling_event = SamplingEvent.new(@attr)
+    sampling_event.save
+    sampling_event.should be_valid
   end
 end
