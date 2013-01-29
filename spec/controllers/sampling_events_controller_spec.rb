1a56895e611ad6bf2527a7b81e35e7178a9d1dd5
wyu <kharma@gmail.com>
2013-01-28 22:33:41 -0800

    update span selectors in views

--- spec/controllers/sampling_events_controller_spec.rb
+++ spec/controllers/sampling_events_controller_spec.rb
@@ -46,9 +46,6 @@ describe SamplingEventsController do
     }
   end
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # SamplingEventsController. Be sure to keep this updated too.
   def valid_session
     {}
   end
