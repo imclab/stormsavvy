6780d43c4b04c5ec0cbc373dc11a0fa4f9d3ff0d
Walter Yu <kharma@gmail.com>
2013-01-28 20:23:33 -0800

    use create table in inspection event migration

--- db/migrate/20130127071354_add_fields_to_inspection_events.rb
+++ db/migrate/20130127071354_add_fields_to_inspection_events.rb
@@ -1,12 +1,14 @@
 class AddFieldsToInspectionEvents < ActiveRecord::Migration
   def change
+=begin
     # add_column :inspection_events, :site_id,                :integer
     add_column :inspection_events, :inspection_type,        :string
     add_column :inspection_events, :inspection_description, :text
     add_column :inspection_events, :inspection_date,        :datetime
     add_column :inspection_events, :submitted_by,           :string
     add_column :inspection_events, :inspected_by,           :string
-=begin
+=end
+    # use this if running migrations for first time
     create_table :inspection_events do |t|
       t.integer :site_id
       t.string :inspection_type
@@ -17,6 +19,5 @@ class AddFieldsToInspectionEvents < ActiveRecord::Migration
 
       t.timestamps
     end
-=end
   end
 end
