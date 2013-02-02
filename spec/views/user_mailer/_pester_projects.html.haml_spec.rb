f13a637d3941bc58e39b57c82a628b61e2cac240
wyu <kharma@gmail.com>
2013-02-02 10:09:40 -0800

    edit daily notice view and spec

--- spec/views/user_mailer/_pester_projects.html.haml_spec.rb
+++ spec/views/user_mailer/_pester_projects.html.haml_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "user_mailer/_pester_projects" do
   it "contains a projects table" do
     render
+    rendered.should =~ /current stats/
     rendered.should have_selector 'table'
     rendered.should have_selector 'th', :text => "Projects"
     rendered.should have_selector 'th', :text => "Users"
--- spec/views/user_mailer/pester_admins.html.haml_spec.rb
+++ spec/views/user_mailer/pester_admins.html.haml_spec.rb
@@ -4,7 +4,21 @@ describe "user_mailer/pester_admins" do
 
   before(:all) do
     @greeting = "Greetings"
-    @forecast1 = [{ :date => "today", :weather => "90" }]
+    @forecast1 = [{ :date => Date.today, :weather => "90" },
+                  { :date => Date.today + 1.day, :weather => "85"},
+                  { :date => Date.today + 2.day, :weather => "80"},
+                  { :date => Date.today + 3.day, :weather => "75"},
+                  { :date => Date.today + 4.day, :weather => "70"},
+                  { :date => Date.today + 5.day, :weather => "65"},
+                  { :date => Date.today + 6.day, :weather => "60"}]
+    @date1 = Date.today
+    @date2 = Date.today + 1.day
+    @date3 = Date.today + 2.day
+    @date4 = Date.today + 3.day
+    @date5 = Date.today + 4.day
+    @date6 = Date.today + 5.day
+    @date7 = Date.today + 6.day
+    # @forecast1 = [{ :date => "today", :weather => "90" }]
   end
 
   it "greet the admin user" do
