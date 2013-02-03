c18b62d4b9a62b24de6f51eb5c3c9c658953b8a2
wyu <kharma@gmail.com>
2013-02-03 13:45:50 -0800

    edit alert mailer text

--- spec/views/alert_mailer/noaa_alert.html.haml_spec.rb
+++ spec/views/alert_mailer/noaa_alert.html.haml_spec.rb
@@ -4,7 +4,7 @@ describe "alert_mailer/noaa_alert" do
 
   it "renders the noaa alert email view" do
     render
-    rendered.should =~ /Future mailer with NOAA forecast/
+    rendered.should =~ /NOAA POP Alert/
+    rendered.should =~ /Please be advised that there is a forecast rain event in your area./
   end
-
 end
--- spec/views/alert_mailer/noaa_forecast.html.haml_spec.rb
+++ spec/views/alert_mailer/noaa_forecast.html.haml_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe "alert_mailer/noaa_forecast" do
 
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @project = @user.projects.build(
+      :name => "foo", 
+      :description => "bar",
+      :startdate => Date.today,
+      :finishdate => Date.today + 30.days
+    )
+    @project.save
+    sign_in @user
+  end
+
   it "renders the noaa forecast alert email view" do
     render
-    rendered.should =~ /Future mailer for NOAA forecasts/
+    rendered.should =~ /Here is your NOAA forecast:/
   end
 
 end
