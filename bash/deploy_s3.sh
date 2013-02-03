b9a87b033aa2c87abe37ce0234a6b70ef1e3abea
wyu <kharma@gmail.com>
2013-02-02 21:43:20 -0800

    fix spelling typo

--- bash/deploy_s3.sh
+++ bash/deploy_s3.sh
@@ -2,6 +2,6 @@
 
 git push git@heroku.com:stormsavvy3.git master
 heroku restart worker.1 -a stormsavvy3
-heroku restart -a stormsavy3
+heroku restart -a stormsavvy3
 heroku logs -a stormsavvy3
 heroku open -a stormsavvy3
