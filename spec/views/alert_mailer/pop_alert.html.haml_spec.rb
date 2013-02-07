95a8c141d1fc4309ed0932e66cbe3dda85df7859
Walter Yu <kharma@gmail.com>
2013-02-07 15:25:33 -0800

    review mailer spec and run bundler

--- spec/views/alert_mailer/pop_alert.html.haml_spec.rb
+++ spec/views/alert_mailer/pop_alert.html.haml_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "alert_mailer/northbay_forecast.html.haml" do
 
   it "renders POP alert" do
-    @user = FactoryGirl.build(:user)
+    # @user = FactoryGirl.build(:user)
     render
     rendered.should =~ /NOAA POP Alert/
   end
--- spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
+++ spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
@@ -2,6 +2,35 @@ require 'spec_helper'
 
 describe "user_mailer/_pester_forecast" do
 
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @project1 = FactoryGirl.create(
+      :project,
+      :user => @user,
+      :created_at => 1.day.ago
+      )
+    @project2 = FactoryGirl.create(
+      :project,
+      :user => @user,
+      :created_at => 1.hour.ago
+      )
+    @projects = [@project1, @project2]
+
+    @site1 = FactoryGirl.create(
+      :site,
+      :project => @project1,
+      :name => 'ec jungle gym',
+      :zipcode => 94530
+      )
+    @site2 = FactoryGirl.create(
+      :site,
+      :project => @project2,
+      :name => 'ec playground slide',
+      :zipcode => 94530
+      )
+    @sites = [@site1, @site2]
+  end
+
   it "renders the forecast partial" do
     render
     rendered.should have_content "Site"
