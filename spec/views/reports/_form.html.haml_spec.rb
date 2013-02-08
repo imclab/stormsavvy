68a17374afce5d7e9e04ffdd80f5c9facb4a5399
Walter Yu <kharma@gmail.com>
2013-02-07 23:34:16 -0800

    add report partial view spec

---
+++ spec/views/reports/_form.html.haml_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "reports/_form" do
+  it "renders form partial with correct elements" do
+    # render 'reports/_form.html.haml'
+    # rendered.should =~ /Upload Attachment/
+    # rendered.should =~ /Cancel/
+    # rendered.should have_selector 'table'
+  end
+end
