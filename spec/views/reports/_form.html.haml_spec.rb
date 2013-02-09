f3e7c677d9e7dcb304077924f1b73f837eb9721b
wyu <kharma@gmail.com>
2013-02-09 15:00:52 -0800

    edit report view spec

--- spec/views/reports/_form.html.haml_spec.rb
+++ spec/views/reports/_form.html.haml_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
-describe "reports/_form" do
-  it "renders form partial with correct elements" do
-    # render 'reports/_form.html.haml'
+describe "reports/_form.html.haml" do
+
+  it "renders form partial found on new report view" do
+    visit new_report_path
+    page.should_not be_nil
+    # render
     # rendered.should =~ /Upload Attachment/
     # rendered.should =~ /Cancel/
     # rendered.should have_selector 'table'
