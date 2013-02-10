d3dc8150291bc9ca1e9df206409a7ad9ca9ad855
wyu <kharma@gmail.com>
2013-02-09 21:02:13 -0800

    add partial render block

--- spec/views/reports/_report_status.html.haml_spec.rb
+++ spec/views/reports/_report_status.html.haml_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe "reports/_reports_status" do
+
+  before(:each) do
+    assign(:reports, [
+      stub_model(Report),
+      stub_model(Report)
+    ])
+  end
+
   it "renders form partial found on index page" do
     visit reports_path
     page.should_not be_nil
-    # render 'reports/_form.html.haml'
-    # rendered.should =~ /Upload Attachment/
-    # rendered.should =~ /Cancel/
-    # rendered.should have_selector 'table'
+
+    render
+    rendered.should =~ /Upload Attachment/
+    rendered.should =~ /Cancel/
+    rendered.should have_selector 'table'
   end
 end
