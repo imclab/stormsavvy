7370ac83009f8106a9ed0b1ba60df8598e3c6f04
wyu <kharma@gmail.com>
2013-02-09 23:34:32 -0800

    add paperclip spec

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -63,4 +63,14 @@ describe Report do
         {:status => "completed"}
     end
   end
+
+  before do
+    Report.any_instance.stub(:save_attached_files).and_return(true) 
+    @attachment = FactoryGirl.create :report
+  end
+  describe "#your_method" do
+    it "returns correct url" do
+      @attachment.attachment.url.should_not be_nil
+    end
+  end
 end
