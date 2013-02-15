46772aa9e1df01522777142b2f02c336ec689300
Walter Yu <kharma@gmail.com>
2013-02-15 00:01:46 -0800

    add forecast methods

--- lib/weather/redis_forecast.rb
+++ lib/weather/redis_forecast.rb
@@ -1,5 +1,19 @@
 require 'redis'
+require 'weather/noaa_forecast'
 
 class RedisForecast
 
+  def set_lat_long(zipcode)
+    nf = NOAAForecast.new(zipcode)
+    lat_long = nf.get_lat_long(zipcode)
+    $redis.set('lat', lat_long[0])
+    $redis.set('long', lat_long[1])
+  end
+
+  def get_lat_long(zipcode)
+    nf = NOAAForecast.new(zipcode)
+    lat_long = nf.get_lat_long(zipcode)
+    $redis.zadd(zipcode, lat_long[0], lat_long[1])
+    return $redis.get('zipcode')
+  end
 end
