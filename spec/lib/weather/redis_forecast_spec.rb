23c1434d1cfbb6c70fafe2362584e675a95810e8
Walter Yu <kharma@gmail.com>
2013-02-15 00:01:57 -0800

    add redis forecast spec

--- spec/lib/weather/redis_forecast_spec.rb
+++ spec/lib/weather/redis_forecast_spec.rb
@@ -19,22 +19,39 @@ describe RedisForecast do
       latlong = @nf.get_lat_long(94530)
       @nf.get_forecast(latlong)
     end
+
+    @rf = RedisForecast.new
   end
 
   it "creates redis object" do
-    rf = RedisForecast.new
-    rf.class.should == RedisForecast
+    @rf.class.should == RedisForecast
   end
 
   it "returns nf stub values" do
     @nf.get_lat_long(94530).should == [37.9202057, -122.2937428]
   end
 
-  it "stores lat long values" do
+  it "sets lat long values" do
     lat_long = @nf.get_lat_long(94530)
     $redis.set('lat', lat_long[0])
     $redis.set('long', lat_long[1])
     $redis.get('lat').should == lat_long[0].to_s
     $redis.get('long').should == lat_long[1].to_s
+  end
+
+  it "sets zipcode using set_lat_long" do
+    @rf.set_lat_long(94530)
+    $redis.get('lat').should == 37.9202057.to_s
+    $redis.get('long').should == -122.2937428.to_s
+  end
+
+  it "gets lat long" do
+    zipcode = 94530
+    nf = NOAAForecast.new(zipcode)
+    lat_long = nf.get_lat_long(zipcode)
+    $redis.zadd(zipcode, lat_long[0], lat_long[1])
+    print $redis.get('zipcode')
+
+    # @rf.get_lat_long(94530).should == [37.9202057.to_s, -122.2937428.to_s]
   end
 end
