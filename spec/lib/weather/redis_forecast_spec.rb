0243303fa563a496f279f179909e500ca996482a
Walter Yu <kharma@gmail.com>
2013-02-16 00:37:28 -0800

    edit noaa forecast before block

--- spec/lib/weather/redis_forecast_spec.rb
+++ spec/lib/weather/redis_forecast_spec.rb
@@ -3,37 +3,4 @@ require 'weather/redis_forecast'
 require 'weather/noaa_forecast'
 
 describe RedisForecast do
-
-  before(:each) do
-    @nf = double(NOAAForecast)
-    @nf.stub(:get_lat_long).with(94530).and_return([37.9202057, -122.2937428])
-    @nf.stub(:ping_noaa).with([37.92, -122.29], 168, 6) do
-      IO.read("./spec/lib/weather/noaa_response.xml")
-    end
-    @nf.stub(:get_forecast).with(@nf.get_lat_long(94530)) do
-      response = @nf.ping_noaa([37.92, -122.29], 168, 6)
-      nf = NOAAForecast.new(94530)
-      nf.parse_weather_data(response)
-    end
-    @nf.stub(:seven_day_weather) do
-      latlong = @nf.get_lat_long(94530)
-      @nf.get_forecast(latlong)
-    end
-  end
-
-  it "sets zipcode using set_lat_long" do
-    @rf.set_lat_long(94530)
-    $redis.get('lat').should == 37.9202057.to_s
-    $redis.get('long').should == -122.2937428.to_s
-  end
-
-  it "gets lat long" do
-    zipcode = 94530
-    nf = NOAAForecast.new(zipcode)
-    lat_long = nf.get_lat_long(zipcode)
-    $redis.zadd(zipcode, lat_long[0], lat_long[1])
-    print $redis.get('zipcode')
-
-    # @rf.get_lat_long(94530).should == [37.9202057.to_s, -122.2937428.to_s]
-  end
 end
