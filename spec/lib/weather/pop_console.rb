bc84e406e95f89c44911384a40ed5d7f385331e1
Walter Yu <kharma@gmail.com>
2013-02-16 23:23:26 -0800

    add pop_console script

---
+++ spec/lib/weather/pop_console.rb
@@ -0,0 +1,10 @@
+# ruby script for testing pop array in console
+nf = NOAAForecast.new(94530,168,6)
+nf.get_forecast([@lat, @long])
+pop = nf.pop
+print pop
+
+pt = []
+pop.each do |i|
+  pt << { :date => ProjectLocalTime::format(Date.today + (i*6).hours), :weather => i.to_s }
+end
