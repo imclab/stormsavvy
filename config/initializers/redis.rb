41f12ba72ec4ec9a1f21b2d047db969771035d65
wyu <kharma@gmail.com>
2013-02-20 15:30:28 -0800

    comment out gems for debugging heroku deploy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
-require 'redis'
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+# require 'redis'
+# $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 # uri = URI.parse(ENV["REDISTOGO_URL"])
 # $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
