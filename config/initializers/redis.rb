f91fd3cddc3b564dcd7d407c1dcc2e0a24779598
Walter Yu <kharma@gmail.com>
2013-02-21 23:48:27 -0800

    edit redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,24 @@
 require 'redis'
-# $redis = Redis.new(:host => 'localhost', :port => 6379)
 
-# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDISTOGO_URL = "redis://localhost:6379/"
+uri = URI.parse(REDISTOGO_URL)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+=begin
+$redis = Redis.new(:host => 'localhost', :port => 6379)
+
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 $redis = Redis.new(url: ENV["REDISTOGO_URL"])
+
+uri = URI.parse(ENV['REDISTOGO_URL'])
+begin
+  $redis ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  $redis.keys # needed to know if connection failed
+  REDIS = $redis
+rescue
+  puts("Redis not loaded on #{uri.port}")
+  REDIS = nil
+end
+=end
