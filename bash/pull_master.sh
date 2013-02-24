eee7eafaba6e8339cebc779b1a1cfecbbdc8a8a2
Walter Yu <kharma@gmail.com>
2013-02-23 23:28:31 -0800

    update deploy scripts

--- bash/pull_master.sh
+++ bash/pull_master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
 git pull --rebase bitbucket master
+git pull --rebase github master
--- bash/push_master.sh
+++ bash/push_master.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-git pull bitbucket master
 git push bitbucket master
-
-git pull github master
 git push github master
