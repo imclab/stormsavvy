8eb9ce3d4b44713c16a1268ac168e7ee581316cc
Walter Yu <kharma@gmail.com>
2013-02-25 10:42:41 -0800

    move sampaling event spec to features

---
+++ spec/features/sampling_events_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "SamplingEvents" do
+  describe "GET /sampling_events" do
+    it "works! (now write some real specs)" do
+      get sampling_events_path
+      response.status.should be(200)
+    end
+  end
+end
