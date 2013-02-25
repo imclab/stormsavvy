46585ea5256640e23e5acf6562883d0db971fc8a
Walter Yu <kharma@gmail.com>
2013-02-25 10:15:41 -0800

    update scheduler script

--- bash/run_scheduler.sh
+++ bash/run_scheduler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# heroku run rake scheduler:dailynotice -a stormsavvy
+heroku run rake scheduler:dailynotice -a stormsavvy
 heroku run rake scheduler:northbay_forecast -a stormsavvy
 heroku run rake scheduler:southbay_forecast -a stormsavvy
 heroku run rake scheduler:eastbay_forecast -a stormsavvy
