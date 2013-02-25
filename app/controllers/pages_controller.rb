0d6c41008a0c1e61871a831665728927412cbe96
Walter Yu <kharma@gmail.com>
2013-02-25 15:05:45 -0800

    add authentication for pdf page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!, 
+    :except => [:index, :about, :terms, :about_us, :privacy]
+
   def index
     if user_signed_in?
       redirect_to sites_path
     end
+  end
+
+  def pdf
   end
 
   def about
@@ -14,32 +20,32 @@ class PagesController < ApplicationController
   def about_us
   end
 
-  def faq
-  end
-
   def privacy
   end
 
-  def contact
-  end
+  # def faq
+  # end
 
-  def plans
-  end
+  # def contact
+  # end
 
-  def features
-  end
+  # def plans
+  # end
 
-  def team
-  end
+  # def features
+  # end
 
-  def popemail
-    # TODO: Alert mailer necessary here?
-    # AlertMailer.pop_alert(user).deliver
-  end
+  # def team
+  # end
 
-  def sendemail
-  end
+  # def popemail
+  #   TODO: Alert mailer necessary here?
+  #   AlertMailer.pop_alert(user).deliver
+  # end
 
-  def consulting
-  end
+  # def sendemail
+  # end
+
+  # def consulting
+  # end
 end
