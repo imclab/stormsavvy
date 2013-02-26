102c38084c02a05734cae98dea3420fbeb008fb3
Walter Yu <kharma@gmail.com>
2013-02-25 20:29:50 -0800

    add inspection event file attachment partial and spec

---
+++ spec/views/inspection_events/_attachments.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'inspection_events/attachments' do
+
+  before(:each) do
+    assign(:inspection_events, [
+      stub_model(InspectionEvent),
+      stub_model(InspectionEvent)
+    ])
+    @inspection_event = FactoryGirl.create(:inspection_event, :id => 1)
+  end
+  
+  it "renders partial correctly" do    
+    render
+    rendered.should_not be_nil
+    rendered.should =~ /Instructions:/
+    rendered.should =~ /Cancel/
+  end
+end
