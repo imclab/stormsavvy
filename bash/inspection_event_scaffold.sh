803e0e554c6f878236c9a1203b547acdd1b46575
Walter Yu <kharma@gmail.com>
2013-02-25 16:45:48 -0800

    update migraction scripts

--- bash/inspection_event_scaffold.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rails g scaffold InspectionEvent\
-  site_id:integer\
-  inspection_type:string\
-  inspection_description:text\
-  inspection_date:datetime\
-  submitted_by:string\
-  inspected_by:string
