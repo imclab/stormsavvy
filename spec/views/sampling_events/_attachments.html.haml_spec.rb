68e92e714a598e4f7cf03d19accd5694c931c922
Walter Yu <kharma@gmail.com>
2013-02-25 20:30:05 -0800

    add sampling event file attachment partial and spec

---
+++ spec/views/sampling_events/_attachments.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'sampling_events/attachments' do
+
+  before(:each) do
+    assign(:sampling_events, [
+      stub_model(SamplingEvent),
+      stub_model(SamplingEvent)
+    ])
+    @sampling_event = FactoryGirl.create(:inspection_event, :id => 1)
+  end
+  
+  it "renders partial correctly" do    
+    render
+    rendered.should_not be_nil
+    rendered.should =~ /Instructions:/
+    rendered.should =~ /Cancel/
+  end
+end
