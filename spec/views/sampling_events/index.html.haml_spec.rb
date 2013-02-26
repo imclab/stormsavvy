3d760e64cab82eae4485068dbfb251544a3e0999
Walter Yu <kharma@gmail.com>
2013-02-25 20:26:14 -0800

    remove ze auto-comment

--- spec/views/sampling_events/index.html.haml_spec.rb
+++ spec/views/sampling_events/index.html.haml_spec.rb
@@ -72,7 +72,6 @@ describe "sampling_events/index" do
 
   it "renders a list of sampling_events" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => "Sampling Type".to_s, :count => 2
     # assert_select "tr>td", :text => "MyText".to_s, :count => 2
