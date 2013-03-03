a08af8d6b6fd41432b6b53a68bb036972ba117bc
wyu <kharma@gmail.com>
2013-03-03 11:43:55 -0800

    add spec to inspection event workflow

--- spec/models/inspection_event_workflow_spec.rb
+++ spec/models/inspection_event_workflow_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe InspectionEventWorkflow do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @iew = FactoryGirl.create(:inspection_event_workflow)
+  end
+
+  it "default model is valid" do
+    @iew.should be_valid
+  end
+
+  it "should check on pre-storm inspection status 24 hours before rain" do
+    @iew.hours_before_rain = 24
+    @rew.chance_of_rain = 55
+    @rew.pre_storm_inspection = false
+    @rew.check_cem2030.should =~ /CEM2030 prepared/
+  end
+
+  it "should check on pre-storm inspection status 24 hours before rain" do
+    @rew.hours_before_rain = 24
+    @rew.chance_of_rain = 55
+    @rew.reap = true
+    @rew.check_reap.should_not =~ /Prepare CEM2030/
+  end
+
+  it "should check to see if rain is imminent" do
+    @rew.hours_before_rain = 23
+    @rew.chance_of_rain = 55
+    @rew.reap = false
+    lambda do
+      if @rew.rain_imminent?
+        @rew.start_inspection_event_workflow
+      end
+    end.should change(Report, :count).by(4) # double-count?
+  end
 end
