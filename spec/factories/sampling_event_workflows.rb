9370ac6d651523a36a93185c3205a2ca5a612867
wyu <kharma@gmail.com>
2013-03-02 22:44:45 -0800

    remove ze comment

--- spec/factories/sampling_event_workflows.rb
+++ spec/factories/sampling_event_workflows.rb
@@ -1,6 +1,15 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :sampling_event_workflow do
+    hours_before_rain 1
+    chance_of_rain 35
+    amount_of_rain 0.45
+    cem2051 false
+    cem2052 false
+    ph_sample false
+    turbidity false
+    report_sent false
+    report_received false
+    forecast_rain 0.45
+    sampling_event false
   end
 end
