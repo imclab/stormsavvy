184977190a2c28e07cacf3f8d96153938b249195
wyu <kharma@gmail.com>
2013-03-02 23:48:18 -0800

    update sampling event variable name

--- spec/models/sampling_event_workflow_spec.rb
+++ spec/models/sampling_event_workflow_spec.rb
@@ -20,37 +20,37 @@ describe SamplingEventWorkflow do
     @sew.hours_before_rain = 24
     @sew.chance_of_rain = 55
     @sew.cem2051 = true
-    @sew.check_reap.should_not =~ /Prepare CEM2051/
+    @sew.check_cem2051.should_not =~ /Prepare CEM2051/
   end
 
   it "should check on CEM2052 status 24 hours before rain" do
     @sew.hours_before_rain = 24
     @sew.chance_of_rain = 55
     @sew.cem2052 = false
-    @sew.check_cem2052.should =~ /CEM2030 prepared/    
+    @sew.check_cem2052.should =~ /CEM2052 prepared/    
   end
 
   it "should check on CEM2052 status 24 hours before rain" do
     @sew.hours_before_rain = 24
     @sew.chance_of_rain = 55
     @sew.cem2052 = true
-    @sew.check_cem2052.should_not =~ /CEM2030 prepared/
+    @sew.check_cem2052.should_not =~ /CEM2052 prepared/
   end
   
   it "should check to see if sampling is imminent" do
     @sew.hours_before_rain = 23
     @sew.chance_of_rain = 55
     @sew.forecast_rain = 0.55
-    @rew.sampling_event = false
+    @sew.sampling_event = false
     lambda do
-      if @rew.sampling_imminent?
-        @rew.start_sampling_event_workflow
+      if @sew.sampling_imminent?
+        @sew.start_sampling_event_workflow
       end
-    end.should change(SamplingEvent, :count).by(1)
+    end.should change(SamplingEvent, :count).by(2) # double-count?
   end
 
   it "should check to see whether it has rained" do
-    @rew.amount_of_rain = 0.51
-    @rew.has_rained?.should be_true
+    @sew.amount_of_rain = 0.51
+    @sew.has_rained?.should be_true
   end
 end
