3cac35f85c50c8327f0895cc62e11fb9a9f0a166
wyu <kharma@gmail.com>
2013-03-03 13:32:15 -0800

    update prep db script

--- bash/deploy_2030.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-git add app/assets/pdfs/CEM2030_production.pdf
-git commit -m 'editing fields in 2030'
-git push bitbucket master
-git push git@heroku.com:stormsavvy2.git master
--- bash/prep_db.sh
+++ bash/prep_db.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 rake db:migrate
 rake db:seed
 rake db:test:prepare
-bundle exec rspec spec
