b4c94391a5a4df250568185f36490d2088ee6a8c
wyu <kharma@gmail.com>
2013-03-02 23:48:02 -0800

    update comment

--- spec/models/rain_event_workflow_spec.rb
+++ spec/models/rain_event_workflow_spec.rb
@@ -47,7 +47,7 @@ describe RainEventWorkflow do
       if @rew.rain_imminent?
         @rew.start_rain_event_workflow
       end
-    end.should change(Report, :count).by(4) # why 4?
+    end.should change(Report, :count).by(4) # double-count?
   end
 
   it "should check to see whether it has rained" do
