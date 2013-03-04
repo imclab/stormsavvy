6af19df4a7230de1b6cc6c24485224cc112e9a59
wyu <kharma@gmail.com>
2013-03-03 21:07:08 -0800

    edit check report methods in inspection event workflow

--- app/models/inspection_event_workflow.rb
+++ app/models/inspection_event_workflow.rb
@@ -38,20 +38,26 @@ class InspectionEventWorkflow < ActiveRecord::Base
   def start_inspection_event_workflow
     check_inspection_event
     if inspection_event?
-      "Inspection Event prepared"
+      print "Inspection Event prepared"
       InspectionEvent.create
     end
 
     check_cem2023
     if cem2023?
-      "CEM2023 prepared"
-      Report.create(:type => 'CEM2023')
+      print "CEM2023 prepared"
+      Report.create(
+        :type => 'CEM2023',
+        :needs_attention => true
+        )
     end
 
     check_cem2024
     if cem2024?
-      "CEM2024 prepared"
-      Report.create(:type => 'CEM2024')
+      print "CEM2024 prepared"
+      Report.create(
+        :type => 'CEM2024',
+        :needs_attention => true
+        )
     end
 =begin
     check_cem2030
@@ -62,20 +68,29 @@ class InspectionEventWorkflow < ActiveRecord::Base
 =end
     check_cem2034
     if cem2034?
-      "CEM2034 prepared"
-      Report.create(:type => 'CEM2034')
+      print "CEM2034 prepared"
+      Report.create(
+        :type => 'CEM2034',
+        :needs_attention => true
+      )
     end
 
     check_cem2035
     if cem2035?
-      "CEM2035 prepared"
-      Report.create(:type => 'CEM2035')
+      print "CEM2035 prepared"
+      Report.create(
+        :type => 'CEM2035',
+        :needs_attention => true
+        )
     end
 
     check_cem2040
     if cem2040?
-      "CEM2040 prepared"
-      Report.create(:type => 'CEM2040')
+      print "CEM2040 prepared"
+      Report.create(
+        :type => 'CEM2040',
+        :needs_attention => true
+        )
     end
   end
 
