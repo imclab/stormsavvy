c1fdf787ecacd947886b15a658b56b4307e4c1dc
Walter Yu <kharma@gmail.com>
2013-03-07 22:46:08 -0800

    add dashboard spec and variables

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -4,6 +4,36 @@ describe DashboardController do
 
   before(:each) do
     @user = FactoryGirl.create(:user)
+    @project = FactoryGirl.create(
+      :project,
+      :user => @user,
+      :created_at => 1.day.ago
+    )
+    @projects = [@project]
+
+    @site = FactoryGirl.create(
+      :site,
+      :project => @project,
+      :name => 'ec jungle gym',
+      :zipcode => 94530
+    )
+    @sites = [@site]
+
+=begin
+    # comment back in when model is ready
+    @weather_event = FactoryGirl.create(
+      :weather_event,
+      :site => @site
+    )
+    @weather_events = [@weather_event]
+=end
+
+    @inspection_event = FactoryGirl.create(
+      :inspection_event,
+      :site => @site
+    )
+    @inspection_events = [@inspection_event]
+
     sign_in @user
   end
 
