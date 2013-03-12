6998a23096d090ddf89cf5efd74b91e58f8dfebc
wyu <kharma@gmail.com>
2013-03-11 22:55:46 -0700

    organize application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe ApplicationHelper do
 
-  it "responds to twitterized_type" do
-    helper.should respond_to :twitterized_type
-    helper.twitterized_type(:alert).should == "warning"
-    helper.twitterized_type(:notice).should == "info"
-    helper.twitterized_type(:error).should == "error"
-    helper.twitterized_type(:success).should == "success"
+  describe '#twitterized_type' do
+    it "responds to twitterized_type" do
+      helper.should respond_to :twitterized_type
+      helper.twitterized_type(:alert).should == "warning"
+      helper.twitterized_type(:notice).should == "info"
+      helper.twitterized_type(:error).should == "error"
+      helper.twitterized_type(:success).should == "success"
+    end
   end
 
   it "has devise login help methods" do
@@ -16,14 +18,24 @@ describe ApplicationHelper do
     helper.should respond_to :devise_mapping
   end
 
-  it "has user and devise resource helpers" do
-    user = self.resource_name
-    user.should == :user
+  describe '#resource_name' do
+    it "returns user resource" do
+      user = self.resource_name
+      user.should == :user
+    end
+  end
 
-    resource= self.resource
-    resource.should == @resource
+  describe '#resource' do
+    it 'returns user variable' do
+      resource= self.resource
+      resource.should == @resource
+    end
+  end
 
-    devise_mapping = self.devise_mapping
-    devise_mapping.should == @devise_mapping
+  describe '#devise_mapping' do
+    it 'returns devise variable' do
+      devise_mapping = self.devise_mapping
+      devise_mapping.should == @devise_mapping
+    end
   end
 end
