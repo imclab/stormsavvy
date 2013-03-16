12dd438ae02fec51aaeae14d42fe2691acaea7be
Walter Yu <kharma@gmail.com>
2013-03-16 11:40:51 -0700

    add unicorn blocks for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,27 @@
+# handles unicorn process on heroku: http://goo.gl/YiWOA
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
 # unicorn gem setup
 worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+end
