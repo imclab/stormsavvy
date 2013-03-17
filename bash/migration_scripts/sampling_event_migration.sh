aa0a2a959770ad8cb8fc257ec6e154c7f0d4d0a6
wyu <kharma@gmail.com>
2013-03-17 14:32:32 -0700

    edit sampling event and workflow migrations

--- bash/migration_scripts/sampling_event_migration.sh
+++ bash/migration_scripts/sampling_event_migration.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-rails g migration AddFieldsToSamplingEventWorkflow \
-  hours_before_rain:integer \
-  cem2051:boolean \
-  cem2052:boolean \
-  ph_sample:boolean \
-  turbidity:boolean \
-  report_sent:boolean \
-  report_received:boolean \
-  chance_of_rain:integer \
-  amount_of_rain:float \
-  forecast_rain:float \
-  sampling_event:boolean
+
+rails g migration AddStatusToSamplingEvent status:boolean
