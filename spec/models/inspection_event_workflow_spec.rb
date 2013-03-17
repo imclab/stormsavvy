0df4c9799ef18e7de95d750b75c31856552caf2e
wyu <kharma@gmail.com>
2013-03-17 10:56:02 -0700

    edit check and prepare inspection spec

--- spec/models/inspection_event_workflow_spec.rb
+++ spec/models/inspection_event_workflow_spec.rb
@@ -18,47 +18,26 @@ describe InspectionEventWorkflow do
     @iew.should be_valid
   end
 
-  it "should check on pre-storm inspection status 24 hours before rain" do
+  it "checks on pre-storm inspection status 24 hours before rain" do
     @iew.hours_before_rain = 24
     @iew.chance_of_rain = 55
     @iew.pre_storm_inspection = false
     @iew.check_inspection_event.should =~ /Inspection event prepared/
   end
 
-  it "should check on pre-storm inspection status 24 hours before rain" do
+  it "checks on pre-storm inspection status 24 hours before rain" do
     @iew.hours_before_rain = 24
     @iew.chance_of_rain = 55
     @iew.pre_storm_inspection = true
     @iew.check_inspection_event.should_not =~ /Prepare inspection event/
   end
 
-  describe '#inspection_needed' do
-    it "should check to see if inspection is needed" do
-      lambda do
-        if @iew.inspection_needed?
-          @iew.start_inspection_event_workflow
-        end
-      end.should change(Report, :count).by(0) # double-count?
-    end
-  end
-
   describe '#check_inspection_event' do
     it 'creates new inspection event if none exists' do
-      lambda do
-        if @iew.check_inspection_event
-          @iew.prepare_inspection_event
-        end
-      end.should change(InspectionEvent, :count).by(0) # change count later
-    end
-  end
-
-  describe '#check_cem2023' do
-    it 'creates new CEM2023 if none exists' do
-      lambda do
-        if @iew.check_cem2023
-          @iew.prepare_cem2023
-        end
-      end.should change(Report, :count).by(0) # change count later
+      expect {
+        @iew.inspection_event = false
+        @iew.prepare_inspection_event
+      }.to change(InspectionEvent, :count).by(1)
     end
   end
 
