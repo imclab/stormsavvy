0f5fda51bd57684d843482fd826005162021ae12
wyu <kharma@gmail.com>
2013-03-23 12:06:26 -0700

    unicorn setup from heroku docs

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,8 @@
-# handles unicorn process on heroku: http://goo.gl/YiWOA
+preload_app true
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
 before_fork do |server, worker|
 
   Signal.trap 'TERM' do
@@ -10,18 +14,42 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end
 
-# unicorn gem setup
-worker_processes 4 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
-preload_app true
-
 after_fork do |server, worker|
 
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-
 end
+
+=begin
+  # old unicorn setup
+  worker_processes 4 # amount of unicorn workers to spin up
+  timeout 30         # restarts workers that hang for 30 seconds
+  preload_app true
+
+  # handles unicorn process on heroku: http://goo.gl/YiWOA
+  before_fork do |server, worker|
+
+    Signal.trap 'TERM' do
+      puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+      Process.kill 'QUIT', Process.pid
+    end
+
+    defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+  end
+
+  after_fork do |server, worker|
+
+    Signal.trap 'TERM' do
+      puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    end
+
+    defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+
+  end
+=end
