7aff2f9839c7e4716bbd2c53ee205f265720730f
wyu <kharma@gmail.com>
2013-03-26 10:31:20 -0700

    debug form field spec

--- spec/views/projects/edit.html.haml_spec.rb
+++ spec/views/projects/edit.html.haml_spec.rb
@@ -4,20 +4,25 @@ describe "projects/edit" do
 
   before(:each) do
     @user = FactoryGirl.create(:user)
-    @project = @user.projects.create!({
-      :name        => "MyString",
-      :description => "MyText",
-      :startdate   => 5.days.ago,
-      :finishdate  => 1.day.ago,
-      :active      => false})
+    @project = @user.projects.create!(
+      {
+        :name        => "MyString",
+        :description => "MyText",
+        :startdate   => 5.days.ago,
+        :finishdate  => 1.day.ago,
+        :active      => false
+      }
+    )
   end
 
   it "renders the edit project form" do
     render
     assert_select "form", :action => projects_path(@project), :method => "post" do
       assert_select "input#project_name", :name => "project[name]"
-      assert_select "textarea#project_description", :name => "project[description]"
+      # assert_select "input#project_start_date", :name => "project[start_date]"
+      # assert_select "input#project_finish_date", :name => "project[finish_date]"
       assert_select "input#project_active", :name => "project[active]"
+      assert_select "textarea#project_description", :name => "project[description]"
     end
   end
 end
