e63b28690b1ca7e7ed1d6bfd867775f5c0aeb776
Walter Yu <kharma@gmail.com>
2013-03-30 00:25:07 -0700

    update fields in view spec

--- spec/views/inspection_events/show.html.haml_spec.rb
+++ spec/views/inspection_events/show.html.haml_spec.rb
@@ -19,6 +19,16 @@ describe "inspection_events/show" do
     rendered.should match(/ec jungle gym inspection/)
   end
 
+  it "renders correct columns" do
+    render
+    rendered.should match(/Site ID:/)
+    rendered.should match(/Inspection Type:/)
+    rendered.should match(/Description:/)
+    rendered.should match(/Date:/)
+    rendered.should match(/Submitted By:/)
+    rendered.should match(/Inspected By:/)
+  end
+
   it "renders edit / back / delete links" do
     render
     rendered.should match(/Edit/)
