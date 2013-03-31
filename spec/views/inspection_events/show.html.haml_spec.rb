55fb54ead4e179b21d607d29c59f8888d7a59a53
Walter Yu <kharma@gmail.com>
2013-03-30 19:38:51 -0700

    factor out duplicate render calls

--- spec/views/inspection_events/show.html.haml_spec.rb
+++ spec/views/inspection_events/show.html.haml_spec.rb
@@ -2,25 +2,27 @@ require 'spec_helper'
 
 describe "inspection_events/show" do
   before(:each) do
-    @inspection_event = assign(:inspection_event, stub_model(InspectionEvent,
-      :site_id => 1,
-      :inspection_type => "weekly",
-      :inspection_description => "ec jungle gym inspection",
-      :inspection_date => "2013-01-26 23:13:55",
-      :submitted_by => "wyu",
-      :inspected_by => "wyu"
-    ))
+    @inspection_event = assign(
+      :inspection_event, stub_model(
+        InspectionEvent,
+        :site_id => 1,
+        :inspection_type => "weekly",
+        :inspection_description => "ec jungle gym inspection",
+        :inspection_date => "2013-01-26 23:13:55",
+        :submitted_by => "wyu",
+        :inspected_by => "wyu"
+      )
+    )
+    render
   end
 
   it "renders attributes in <p>" do
-    render
     rendered.should match(/1/)
     rendered.should match(/weekly/)
     rendered.should match(/ec jungle gym inspection/)
   end
 
   it "renders correct columns" do
-    render
     rendered.should match(/Site ID:/)
     rendered.should match(/Inspection Type:/)
     rendered.should match(/Description:/)
