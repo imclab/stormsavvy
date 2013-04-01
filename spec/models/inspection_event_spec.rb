039848a1dd8f6b9f2491b7c1ba9778eb5b40c4c4
wyu <kharma@gmail.com>
2013-03-31 18:44:31 -0700

    indent attr block

--- spec/models/inspection_event_spec.rb
+++ spec/models/inspection_event_spec.rb
@@ -5,12 +5,12 @@ describe InspectionEvent do
   before :each do
   	@inspection_event = FactoryGirl.create(:inspection_event)
   	@attr = {
-	  :site_id => 1,
-	  :inspection_type => "weekly",
-	  :inspection_description => "ec jungle gym inspection",
-	  :inspection_date => "2013-01-26 23:13:55",
-	  :submitted_by => "wyu",
-	  :inspected_by => "wyu"
+      :site_id => 1,
+      :inspection_type => "weekly",
+      :inspection_description => "ec jungle gym inspection",
+      :inspection_date => "2013-01-26 23:13:55",
+      :submitted_by => "wyu",
+      :inspected_by => "wyu"
   	}
   end
 
