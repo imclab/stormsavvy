0466acb084b060e783ce25bd41313b88ca294d6f
wyu <kharma@gmail.com>
2013-04-01 13:38:50 -0700

    update page routes

--- config/routes.rb
+++ config/routes.rb
@@ -44,19 +44,15 @@ Stormsavvy::Application.routes.draw do
     match "/reports/#{name}" => "reports##{name}"
   end
 
-  match '/footer',    :to => "pages#footer",         :as => :footer
   match '/index',     :to => "pages#index",          :as => :index
   match '/about',     :to => "pages#about",          :as => :about
   match '/terms',     :to => "pages#terms",          :as => :terms
   match '/faq',       :to => "pages#faq",            :as => :faq
   match '/privacy',   :to => "pages#privacy",        :as => :privacy
   match '/contact',   :to => "pages#contact",        :as => :contact
-  match '/plans-and-pricing', :to => "pages#plans",  :as => :plans
+  match '/plans',     :to => "pages#plans",          :as => :plans
   match '/features',  :to => "pages#features",       :as => :features
   match '/team',      :to => "pages#team",           :as => :team
-  match '/popemail',  :to => "pages#popemail",       :as => :popemail
-  match '/sendemail', :to => "pages#sendemail",      :as => :sendemail
-  match '/consulting', :to => "pages#consulting",    :as => :consulting
   match '/pdf',       :to => "pages#pdf",            :as => :pdf
 
   root :to => "dashboard#index"
