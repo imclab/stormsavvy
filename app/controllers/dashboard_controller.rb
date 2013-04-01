742025b256478c4b8749caf6394e6ac8d87387b2
wyu <kharma@gmail.com>
2013-03-31 18:43:56 -0700

    add current_user scope to dashboard controller pig pen

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,19 +4,22 @@ class DashboardController < ApplicationController
   def index
     redirect_to index_path unless user_signed_in?
     if current_user
-      # Throws no method error on projects
-      @projects = Project.all
-      @sites = Site.all
-
-      # Comment back in along with spec
-      # @weather_events = WeatherEvent.all
-
+      @projects = current_user.projects.all
+      @sites = current_user.sites.all
+=begin
+      inspection_events = @sites.each do |site|
+        site.inspection_events.where(:completed => false)
+      end
+      @inspection_events = inspection_events
+=end
       @inspection_events = InspectionEvent.where(:completed => false)
       # @inspection_events = InspectionEvent.needs_attention.all
 
       @completed_reports = Report.completed
       @needs_attention_reports = Report.needs_attention
+
+      # Comment back in along with spec
+      # @weather_events = WeatherEvent.all
     end
   end
-
 end
