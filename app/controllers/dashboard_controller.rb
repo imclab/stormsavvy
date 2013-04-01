342c81312438a6a9d1a74a361a4ab35176690fcf
wyu <kharma@gmail.com>
2013-03-31 22:42:21 -0700

    edit inspection event attribute scope

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,12 +7,25 @@ class DashboardController < ApplicationController
       @projects = current_user.projects.all
       @sites = current_user.sites.all
 
+      @inspection_events = []
+      current_user.sites.each do |site|
+        site.inspection_events.each do |ie|
+          if ie.completed.blank?
+            'No inspection events.'
+          else
+            @inspection_events[] << ie.completed
+          end
+        end
+      end
 
-      @inspection_events = InspectionEvent.where(:completed => false)
+      # @inspection_events = InspectionEvent.where(:completed => false)
       # @inspection_events = InspectionEvent.needs_attention.all
 
       @completed_reports = Report.completed
       @needs_attention_reports = Report.needs_attention
+
+      # @completed_reports = Report.completed
+      # @needs_attention_reports = Report.needs_attention
 
       # Comment back in along with spec
       # @weather_events = WeatherEvent.all
