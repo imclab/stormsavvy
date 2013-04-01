429c901976bd744eb38225f322c7c1148135d16a
wyu <kharma@gmail.com>
2013-04-01 09:42:13 -0700

    save render forecast block spec for later

--- spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
+++ spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
@@ -24,7 +24,6 @@ describe "user_mailer/_mailout_forecast" do
     @date6 = Date.today + 5.day
     @date7 = Date.today + 6.day
 
-
     @user = FactoryGirl.create(:user)
     @project1 = FactoryGirl.create(
       :project,
@@ -67,5 +66,33 @@ describe "user_mailer/_mailout_forecast" do
     rendered.should have_selector 'table'
     rendered.should have_selector 'th', :text => "Date"
     rendered.should have_selector 'th', :text => "Forecast"
+  end
+
+  it "renders date with data" do
+    render 'user_mailer/mailout_forecast'
+=begin
+    rendered.should have_content ProjectLocalTime::format(@date1)
+    rendered.should have_content ProjectLocalTime::format(@date2)
+    rendered.should have_content ProjectLocalTime::format(@date3)
+    rendered.should have_content ProjectLocalTime::format(@date4)
+    rendered.should have_content ProjectLocalTime::format(@date5)
+    rendered.should have_content ProjectLocalTime::format(@date6)
+    rendered.should have_content ProjectLocalTime::format(@date7)
+
+    rendered.should have_content "85"
+    rendered.should have_content "80"
+    rendered.should have_content "75"
+    rendered.should have_content "70"
+    rendered.should have_content "65"
+    rendered.should have_content "60"
+
+    # rendered.should have_content "0"
+    # rendered.should have_content "1"
+    # rendered.should have_content "2"
+    # rendered.should have_content "3"
+    # rendered.should have_content "4"
+    # rendered.should have_content "5"
+    # rendered.should have_content "6"
+=end
   end
 end
--- spec/views/user_mailer/mailout.html.haml_spec.rb
+++ spec/views/user_mailer/mailout.html.haml_spec.rb
@@ -4,22 +4,6 @@ describe "user_mailer/mailout" do
 
 	before(:all) do
     @greeting = "Greetings"
-=begin
-    @forecast1 = [{ :date => Date.today + 0.day, :weather => "90" },
-                  { :date => Date.today + 1.day, :weather => "85"},
-                  { :date => Date.today + 2.day, :weather => "80"},
-                  { :date => Date.today + 3.day, :weather => "75"},
-                  { :date => Date.today + 4.day, :weather => "70"},
-                  { :date => Date.today + 5.day, :weather => "65"},
-                  { :date => Date.today + 6.day, :weather => "60"}]
-    @date1 = Date.today + 0.day
-    @date2 = Date.today + 1.day
-    @date3 = Date.today + 2.day
-    @date4 = Date.today + 3.day
-    @date5 = Date.today + 4.day
-    @date6 = Date.today + 5.day
-    @date7 = Date.today + 6.day
-=end
     @user = FactoryGirl.create(:user)
     @project1 = FactoryGirl.create(
       :project,
