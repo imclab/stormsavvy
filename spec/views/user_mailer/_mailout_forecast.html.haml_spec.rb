253c4759e7f0df333833388a66f0fe7a4e3f596f
wyu <kharma@gmail.com>
2013-03-31 18:07:31 -0700

    edit mailout mailer spec

--- spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
+++ spec/views/user_mailer/_mailout_forecast.html.haml_spec.rb
@@ -7,13 +7,15 @@ describe "user_mailer/_mailout_forecast" do
     @mailer = UserMailer.mailout
     @greeting = "Greetings"
 
-    @forecast = [{ :date => Date.today, :weather => "90" },
-                  { :date => Date.today + 1.day, :weather => "85"},
-                  { :date => Date.today + 2.day, :weather => "80"},
-                  { :date => Date.today + 3.day, :weather => "75"},
-                  { :date => Date.today + 4.day, :weather => "70"},
-                  { :date => Date.today + 5.day, :weather => "65"},
-                  { :date => Date.today + 6.day, :weather => "60"}]
+    @forecast = [
+      { :date => Date.today, :weather => "90" },
+      { :date => Date.today + 1.day, :weather => "85"},
+      { :date => Date.today + 2.day, :weather => "80"},
+      { :date => Date.today + 3.day, :weather => "75"},
+      { :date => Date.today + 4.day, :weather => "70"},
+      { :date => Date.today + 5.day, :weather => "65"},
+      { :date => Date.today + 6.day, :weather => "60"}
+    ]
     @date1 = Date.today
     @date2 = Date.today + 1.day
     @date3 = Date.today + 2.day
