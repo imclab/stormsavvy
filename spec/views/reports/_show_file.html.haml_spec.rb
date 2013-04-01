eaf6ea51d0b66e305008c835d15394ca5826724c
wyu <kharma@gmail.com>
2013-04-01 13:07:11 -0700

    remove unused fields from report view spec

--- spec/views/reports/_show_file.html.haml_spec.rb
+++ spec/views/reports/_show_file.html.haml_spec.rb
@@ -9,12 +9,11 @@ describe "reports/_show_file" do
     ])
     @report = FactoryGirl.create(:report, :id => 1)
   end
-  
-  it "renders show_file partial correctly" do    
+
+  it "renders show_file partial correctly" do
     render
     rendered.should_not be_nil
     rendered.should =~ /Report ID/
-    rendered.should =~ /Report Type/
     rendered.should =~ /Report Date/
   end
 end
