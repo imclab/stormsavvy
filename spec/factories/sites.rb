64a2dad4f662e3e4008bb3f8e526f454966e8f82
Walter Yu <kharma@gmail.com>
2013-04-11 08:54:21 -0700

    edit site factory calls

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -1,17 +1,15 @@
 FactoryGirl.define do
   factory :site do
-    name          'Oakland Adams Point'
-    address_1     '111 Adams Street'
-    address_2     'Suite 181'
-    city          'Oakland'
-    state         'CA'
-    zipcode       94610
+    name 'Oakland Adams Point'
+    address_1 '111 Adams Street'
+    address_2 'Suite 181'
+    city 'Oakland'
+    state 'CA'
+    zipcode 94610
 
-    description   'North of Lake Merritt'
-    costcode      '450AZC'
-    size          '20 acres'
-    exposed_area  '10 acres'
-
-    # project
+    description 'North of Lake Merritt'
+    costcode '450AZC'
+    size '20 acres'
+    exposed_area '10 acres'
   end
 end
