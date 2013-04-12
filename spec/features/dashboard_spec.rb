7ea07ac629fce54fdddc77fd5872c8eea5b86b98
Walter Yu <kharma@gmail.com>
2013-04-11 18:28:35 -0700

    include site name with pending inspection

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -122,19 +122,11 @@ describe "Dashboards" do
 
       visit '/'
       page.should have_text('UTC')
-      page.should have_text('weekly for 2013-04-01 00:00:00 UTC')
+      page.should have_text('Inspection for berkeley high on 2013-04-01 00:00:00 UTC')
       page.should_not have_text('No pending inspections.')
     end
-  end
 
-  describe "dashboard/sidebar" do
-    it "creates new report" do
-      # visit '/reports'
-      # page.should have_text('')
-
-      # click_link 'New Report'
-      # page.should have_text('Report was successfully created.')
-
+    it "displays new report" do
       visit '/'
       page.should have_text('Report for berkeley high')
       page.should_not have_text('No pending reports.')
