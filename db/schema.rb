c857e8d19761bf7d9917085c90a202cc54fdb3c9
wyu <kharma@gmail.com>
2013-04-14 11:50:52 -0700

    re-run migration after removing nested attribute

--- db/schema.rb
+++ db/schema.rb
@@ -283,4 +283,16 @@ ActiveRecord::Schema.define(:version => 20130401174609) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "workflows", :force => true do |t|
+    t.integer  "hours_before_rain"
+    t.boolean  "reap"
+    t.boolean  "cem2030"
+    t.boolean  "ph_sample"
+    t.boolean  "turbidity"
+    t.boolean  "report_sent"
+    t.boolean  "report_received"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
 end
