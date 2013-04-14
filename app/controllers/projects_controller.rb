edb608d3b297decf397fa5391bd14d8fdf03fd13
Walter Yu <kharma@gmail.com>
2013-04-14 00:10:37 -0700

    edit dashboard variables in project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,15 +17,15 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
+    @projects = current_user.projects.all
 
     # dashboard controller variables
-    @sites = @project.sites
-    @weather_events = WeatherEvent.all
-    @inspection_events = InspectionEvent.all
-    @completed_reports = Report.completed
-    @needs_attention_reports = Report.needs_attention
+    # @sites = @project.sites
+    # @weather_events = WeatherEvent.all
+    # @inspection_events = InspectionEvent.all
+    # @completed_reports = Report.completed
+    # @needs_attention_reports = Report.needs_attention
     # @project = current_user.projects.find(params[:id])
-    # @projects = current_user.projects.all
 
     respond_to do |format|
       format.html # show.html.erb
