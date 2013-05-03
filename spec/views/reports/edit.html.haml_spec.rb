02f0ba36be4b632f8115169028c6b4412c0d4b2f
wyu <kharma@gmail.com>
2013-05-02 20:19:33 -0700

    add file attachment spec

--- spec/views/reports/edit.html.haml_spec.rb
+++ spec/views/reports/edit.html.haml_spec.rb
@@ -13,5 +13,24 @@ describe "reports/edit" do
   it "renders edit page correctly" do
     render
     rendered.should =~ /Site/
+    rendered.should =~ /Save/
+    rendered.should =~ /Back/
+  end
+
+  describe 'file attachment partial' do
+    before(:each) do
+      assign(:reports, [
+        stub_model(Report),
+        stub_model(Report)
+      ])
+      @report = FactoryGirl.create(:report)
+      render
+    end
+
+    it "renders partial correctly" do
+      rendered.should_not be_nil
+      rendered.should =~ /Instructions: Attach File & Complete Description Below/
+      rendered.should =~ /Cancel/
+    end
   end
 end
