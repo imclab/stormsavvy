0767b224129212a52d775bb5cd36c3e7cb0fe1dd
wyu <kharma@gmail.com>
2013-05-05 16:01:51 -0700

    add fields to report index page

--- spec/views/reports/index.html.haml_spec.rb
+++ spec/views/reports/index.html.haml_spec.rb
@@ -2,16 +2,22 @@ require 'spec_helper'
 
 describe "reports/index" do
   before(:each) do
-    assign(:reports, [
+    @report = assign(:reports, [
       stub_model(Report),
       stub_model(Report)
     ])
+    render
   end
 
-  it "renders index page correctly" do
-    render
-    rendered.should =~ /ID/
-    rendered.should =~ /Date/
-    rendered.should =~ /URL/
+  it "renders correct header text" do
+    rendered.should =~ /Site ID/
+    rendered.should =~ /Inspection Type/
+    rendered.should =~ /Report ID/
+    rendered.should =~ /Report Date/
+    rendered.should =~ /Attachment URL/
+  end
+
+  it 'has new report button' do
+    rendered.should have_link('New Report')
   end
 end
