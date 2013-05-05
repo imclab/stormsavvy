77d2229dd78b46e10cccfb38c31e609f1b00e242
wyu <kharma@gmail.com>
2013-05-05 14:35:53 -0700

    add fields to report index page spec

--- spec/views/reports/show.html.haml_spec.rb
+++ spec/views/reports/show.html.haml_spec.rb
@@ -7,24 +7,30 @@ describe "reports/show" do
       stub_model(Report)
     ])
 
+    @report = FactoryGirl.create(:report)
     # @project = assign(:project, stub_model(Project,
     #   :user_id => 1,
     #   :name => "Name",
     #   :description => "MyText",
     #   :active => false
     # ))
-    @report = FactoryGirl.create(:report)
-    # @user = FactoryGirl.create(:user)
-    # @needs_attention_reports = Report.needs_attention
-    # @sites = @project.sites
-    # sign_in @user
   end
 
-  it "renders edit / back / delete" do
+  it "renders correct text" do
     render
-    rendered.should =~ /Edit/
-    rendered.should =~ /Delete This Report/
-    rendered.should =~ /Download PDF/
     rendered.should =~ /View Report/
+    rendered.should =~ /Delete This Report/
+
+    rendered.should have_text('Report ID')
+    rendered.should have_text('Report Date')
+    rendered.should have_text('Site ID')
+    rendered.should have_text('Inspection Type')
+    rendered.should have_text('Status')
+    rendered.should have_text('')
+    rendered.should have_text('')
+
+    rendered.should =~ /Download File/
+    rendered.should =~ /Edit/
+    rendered.should =~ /Back/
   end
 end
