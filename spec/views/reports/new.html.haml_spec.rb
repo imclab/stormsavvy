d8b7f87f6306f7a8c77ffcbed213f95a8a0670f6
wyu <kharma@gmail.com>
2013-05-06 22:36:58 -0700

    add new report page spec

--- spec/views/reports/new.html.haml_spec.rb
+++ spec/views/reports/new.html.haml_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe "reports/new" do
   before(:each) do
     assign(:report, stub_model(Report).as_new_record)
+    render
   end
 
-  it "renders new report form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+  it "renders new report fields" do
     assert_select "form", :action => reports_path, :method => "post" do
+      assert_select "input#report_site_id", :name => "report[site_id]"
+      assert_select "input#report_inspection_type", :name => "report[inspection_type]"
+      assert_select "input#report_status", :name => "report[status]"
     end
   end
 end
