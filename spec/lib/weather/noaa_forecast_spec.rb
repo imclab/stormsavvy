d05f9faa5d9047a7284259aa4220dc4bf7205350
wyu <kharma@gmail.com>
2013-05-18 17:23:19 -0700

    50134397 - remove with method due to error

--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -77,8 +77,8 @@ describe NOAAForecast do
       # qpf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 33, 45, 77, 77, 64, 64, 18, 18, 19, 19, 28, 28, 24, 24, 24, 24, 22]
     }
 
-    @nf2.stub(:get_pop_array).with(@zipcode) {
-
+    # @nf2.stub(:get_pop_array).with(@zipcode) {
+    @nf2.stub(:get_pop_array) {
 =begin
       # array not being returned correctly
       pop_array = @pop
