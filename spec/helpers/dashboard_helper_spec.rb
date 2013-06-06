4bd49285fbfe55dea846f0ba1190a7e79e15d941
wyu <kharma@gmail.com>
2013-06-05 20:55:59 -0700

    51205835 - port class spec into helper

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -130,19 +130,70 @@ describe DashboardHelper do
     end
   end
 
-  describe 'dashboard variables' do
-    it 'returns instance variables' do
-=begin
-      # spec out later
-      @projects.should == @user.project.all
-      @sites.should == @user.site.all
+  describe "#get_projects" do
+    it "returns all projects to current user" do
+      @all_projects.should include(@current_project)
+      @all_projects.should include(@other_project)
+      @all_projects.should_not be_nil
+    end
 
-      @weather_events.should == WeatherEvent.all
-      @inspection_events.should == InspectionEvent.all
+    it "returns correct projects to each user" do
+      @current_user.projects.all.should == @current_projects
+      @current_user.projects.all.should_not == @other_projects
+      @other_user.projects.all.should == @other_projects
+      @other_user.projects.all.should_not == @current_projects
+    end
+  end
 
-      @completed_reports.should == Report.completed
-      @needs_attention_reports.should == Report.needs_attention
-=end
+  describe "#get_sites" do
+    it "returns all sites to current user" do
+      @all_sites.should include(@current_site)
+      @all_sites.should include(@other_site)
+      @all_sites.should_not be_nil
+    end
+
+    it "returns correct sites to each user" do
+      @current_user.sites.all.should == @current_sites
+      @current_user.sites.all.should_not == @other_sites
+      @other_user.sites.all.should == @other_sites
+      @other_user.sites.all.should_not == @current_sites
+    end
+  end
+
+  describe "#get_ie" do
+    it "returns all reports to current user" do
+      @all_ie_array.should include(@current_ie)
+      @all_ie_array.should include(@other_ie)
+      @all_ie_array.should_not be_nil
+    end
+
+    it "returns correct inspection_events to each user" do
+      @current_site.inspection_events.all.should == @current_ie_array
+      @current_site.inspection_events.all.should_not == @other_ie_array
+      @other_site.inspection_events.all.should == @other_ie_array
+      @other_site.inspection_events.all.should_not == @current_ie_array
+    end
+  end
+
+  describe "#get_reports" do
+    it "returns all reports to current user" do
+      @all_reports.should include(@pending_report)
+      @all_reports.should include(@completed_report)
+      @all_reports.should_not be_nil
+    end
+  end
+
+  describe "#pending_reports" do
+    it "returns pending reports to current user" do
+      @pending_reports.should include(@pending_report)
+      @pending_reports.should_not include(@completed_report)
+    end
+
+    it "returns correct reports to each user" do
+      # @current_site.reports.all.should == @pending_reports
+      # @current_site.reports.all.should_not == @completed_reports
+      # @other_site.reports.all.should == @other_reports
+      # @other_site.reports.all.should_not == @current_reports
     end
   end
 end
