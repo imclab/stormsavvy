e72ae66efc33877c0ca0206a2c2251ed76341eb9
Walter Yu <kharma@gmail.com>
2013-06-08 23:38:59 -0700

    51205835 - comment out lib class method calls

--- app/helpers/inspection_events_helper.rb
+++ app/helpers/inspection_events_helper.rb
@@ -1,7 +1,7 @@
 require 'current_user_object'
 
 module InspectionEventsHelper
-  @projects = CurrentUserObject::get_projects(user)
-  @sites = CurrentUserObject::get_sites(user)
-  @pending_ie = CurrentUserObject::get_ie(user)
+  # @projects = CurrentUserObject::get_projects(user)
+  # @sites = CurrentUserObject::get_sites(user)
+  # @pending_ie = CurrentUserObject::get_ie(user)
 end
