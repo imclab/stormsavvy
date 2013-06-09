33a96374f58a96c6e58b1ce74cc28a7e2b78c09e
Walter Yu <kharma@gmail.com>
2013-06-08 19:50:32 -0700

    51205835 - remove unused get_projects call

--- app/helpers/inspection_events_helper.rb
+++ app/helpers/inspection_events_helper.rb
@@ -1,2 +1,7 @@
+require 'current_user_object'
+
 module InspectionEventsHelper
+  @projects = CurrentUserObject::get_projects(user)
+  @sites = CurrentUserObject::get_sites(user)
+  @pending_ie = CurrentUserObject::get_ie(user)
 end
