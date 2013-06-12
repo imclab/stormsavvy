58a154d2fbedd3ddef36133c89d541ccc53c8a5e
Walter Yu <kharma@gmail.com>
2013-06-12 13:32:07 -0700

    51205835 - add before block to spec

--- spec/controllers/sampling_events_controller_spec.rb
+++ spec/controllers/sampling_events_controller_spec.rb
@@ -1,6 +1,75 @@
 require 'spec_helper'
 
 describe SamplingEventsController do
+  before(:each) do
+    @current_user = FactoryGirl.build(
+      :user,
+      :email => '@stormsavvy.com'
+    )
+    @other_user = FactoryGirl.build(
+      :user,
+      :email => 'info@stormsavvy.com'
+    )
+    @all_users = [ @current_user, @other_user ]
+
+    @current_project = FactoryGirl.create(
+      :project,
+      :user => @current_user
+    )
+    @other_project = FactoryGirl.create(
+      :project,
+      :user => @other_user
+    )
+    @current_projects = [ @current_project ]
+    @other_projects = [ @other_project ]
+    @all_projects = [ @current_project, @other_project ]
+
+    @current_site = FactoryGirl.create(
+      :site,
+      :project => @current_project,
+      :name => 'ec jungle gym',
+      :zipcode => 94530
+    )
+    @other_site = FactoryGirl.create(
+      :site,
+      :project => @other_project,
+      :name => 'berkeley high',
+      :zipcode => 94709
+    )
+    @current_sites = [ @current_site ]
+    @other_sites = [ @other_site ]
+    @all_sites = [ @current_site, @other_site ]
+
+    @current_ie = FactoryGirl.create(
+      :inspection_event,
+      :site => @current_site
+    )
+    @other_ie = FactoryGirl.create(
+      :inspection_event,
+      :site => @other_site
+    )
+    @current_ie_array = [ @current_ie ]
+    @other_ie_array = [ @other_ie ]
+    @all_ie_array = [ @current_ie, @other_ie ]
+
+    @completed_report = FactoryGirl.create(
+      :report,
+      :site => @current_site,
+      :status => "completed"
+    )
+    @pending_report = FactoryGirl.create(
+      :report,
+      :site => @current_site,
+      :status => "needs_attention"
+    )
+    @completed_reports = [ @completed_report ]
+    @pending_reports = [ @pending_report ]
+    @all_reports = [ @completed_report, @pending_report ]
+
+    sign_in @current_user
+    sign_in @other_user
+  end
+
   def valid_attributes
     {
       :site_id => 1,
