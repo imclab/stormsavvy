9bb0869bf349ce37145246e680996b3063117f07
Walter Yu <kharma@gmail.com>
2013-06-15 17:22:19 -0700

    51763943 - add new method

--- app/controllers/sampling_events_controller.rb
+++ app/controllers/sampling_events_controller.rb
@@ -17,6 +17,16 @@ class SamplingEventsController < InheritedResources::Base
       @pending_se = cu.get_se(user)
       @sampling_events = cu.all_se(user)
     end
+
+  end
+
+  def new
+    @sampling_event = SamplingEvent.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @sampling_event }
+    end
   end
 
   # GET /sampling_events/1/edit
