ae75e613470f52f26d731a62e1fe03b2da60d7d1
Walter Yu <kharma@gmail.com>
2013-06-16 12:47:08 -0700

    51763943 - add flash message and spec to controller

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -198,6 +198,14 @@ describe InspectionEventsController do
         put :update, {:id => inspection_event.to_param, :inspection_event => valid_attributes}, valid_session
         response.should redirect_to(inspection_event)
       end
+
+      it "responds with flash message" do
+        sign_in @current_user
+        inspection_event = InspectionEvent.create! valid_attributes
+        put :update, {:id => inspection_event.to_param, :inspection_event => valid_attributes}, valid_session
+        # post :create, {:site => valid_attributes, :project_id => @project.id}
+        flash[:notice].should == "Inspection event was successfully updated."
+      end
     end
 
     describe "with invalid params" do
