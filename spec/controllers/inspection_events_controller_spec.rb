7d293c691352beaf972bbd74f9963746113496bd
Walter Yu <kharma@gmail.com>
2013-06-16 10:33:14 -0700

    51763943 - pass pending assign spec

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -87,10 +87,11 @@ describe InspectionEventsController do
   end
 
   describe "GET index" do
-    xit "assigns all inspection_events as @inspection_events" do
+    it "assigns all inspection_events as @inspection_events" do
       inspection_event = InspectionEvent.create! valid_attributes
       get :index, {}, valid_session
-      assigns(:inspection_events).should eq([inspection_event])
+      assigns(:inspection_events).should eq(InspectionEvent.all)
+      # assigns(:inspection_events).should eq([inspection_event])
     end
   end
 
