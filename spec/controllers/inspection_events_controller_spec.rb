26de1d1589af4d0e0c41b16cb05ba3452a79463e
Walter Yu <kharma@gmail.com>
2013-06-30 13:28:11 -0700

    47594229 - replace current_user instance var with let statement

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -236,7 +236,7 @@ describe InspectionEventsController do
       end
 
       it "responds with flash message" do
-        sign_in @current_user
+        sign_in current_user
         inspection_event = InspectionEvent.create! valid_attributes
         InspectionEvent.any_instance.stub(:save).and_return(false)
         put :update, {:id => inspection_event.to_param, :inspection_event => {}}, valid_session
