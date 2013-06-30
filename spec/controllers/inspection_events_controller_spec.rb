392e3e419d995c2e65991035c6df015f478a7d5b
Walter Yu <kharma@gmail.com>
2013-06-30 14:53:12 -0700

    47594229 - add let! statement for lib class spec

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -283,8 +283,8 @@ describe InspectionEventsController do
     end
 
     it "returns pending inspection events" do
-      @current_ie_array.should == current_site.inspection_events.where(:completed => false)
-      @current_ie_array.should_not include(@other_ie)
+      current_ie_array.should == current_site.inspection_events.where(:completed => false)
+      current_ie_array.should_not include(other_ie)
     end
   end
 
