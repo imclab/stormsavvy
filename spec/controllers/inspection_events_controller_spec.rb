806c75048f37f9b24298b59ed130580507a98b37
Walter Yu <kharma@gmail.com>
2013-06-30 13:28:20 -0700

    47594229 - replace current_user instance var with let statement

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -270,12 +270,12 @@ describe InspectionEventsController do
 
   describe "current_user variables: users, projects and sites" do
     it 'returns current projects and sites' do
-      sign_in @current_user
-      @current_projects.should == @current_user.projects.all
+      sign_in current_user
+      @current_projects.should == current_user.projects.all
       @current_projects.should_not include(@other_projects)
       @current_projects.should_not be_nil
 
-      @current_sites.should == @current_user.sites.all
+      @current_sites.should == current_user.sites.all
       @current_sites.should_not include(@other_sites)
       @current_sites.should_not be_nil
     end
