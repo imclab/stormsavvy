25d4c9425db3d1d81ebc73bd766141ce60f9d9b0
Walter Yu <kharma@gmail.com>
2013-06-30 23:16:33 -0700

    47594229 - replace se variables with let statements

--- spec/controllers/sampling_events_controller_spec.rb
+++ spec/controllers/sampling_events_controller_spec.rb
@@ -224,7 +224,7 @@ describe SamplingEventsController do
       end
 
       it "responds with flash message" do
-        sign_in @current_user
+        sign_in current_user
         post :create, {:sampling_event => valid_attributes}, valid_session
         flash[:notice].should == "Sampling event was successfully created."
       end
