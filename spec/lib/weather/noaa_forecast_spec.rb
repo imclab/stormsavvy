2575fad203a0e2c34d343d31c652ecb5b1495d0b
wyu <kharma@gmail.com>
2013-07-03 22:23:44 -0700

    47594229 - remove unused arrays

--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -402,23 +402,12 @@ describe NOAAForecast do
 
   describe "#get_pop" do
     it "returns pop results" do
-      # pop = [0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0]
-      # may need to revert back later
-      # pop = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 33, 45, 77, 77, 64, 64, 18, 18, 19, 19, 28, 28, 24, 24, 24, 24, 22]
-
-      # test by number of array elements instead
       nf2.get_pop(zipcode).should == pop
     end
   end
 
   describe "#get_qpf" do
     it "returns qpf results" do
-      # qpf = [99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99]
-      # may need to revert back later
-      # qpf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-      # qpf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 33, 45, 77, 77, 64, 64, 18, 18, 19, 19, 28, 28, 24, 24, 24, 24, 22]
-
-      # test by number of array elements instead
       nf2.get_qpf(zipcode).should == qpf
     end
   end
