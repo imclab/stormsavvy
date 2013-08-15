5590227d8153a5f7d412c2570b08ad661a91cafe
wyu <kharma@gmail.com>
2013-08-14 20:31:39 -0700

    49800309 - correct attribute name typo

--- spec/controllers/sampling_events_controller_spec.rb
+++ spec/controllers/sampling_events_controller_spec.rb
@@ -5,7 +5,7 @@ describe SamplingEventsController do
     {
       :site_id => 1,
       :sampling_type => "pH",
-      :sampling_description => "ec jungle gym samplng",
+      :sampling_description => "ec jungle gym sampling",
       :sampling_date => "2013-01-27 15:34:07",
       :submitted_by => "wyu",
       :sampled_by => "wyu",
