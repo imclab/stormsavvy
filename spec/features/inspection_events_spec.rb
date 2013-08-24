61a2035cafdb7ac5cbce08e4a9c85c0f5a53ec16
wyu <kharma@gmail.com>
2013-08-24 12:25:00 -0700

    55554478 - pass failing spec after adding before_filter statement

--- spec/features/inspection_events_spec.rb
+++ spec/features/inspection_events_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe "InspectionEvents" do
   describe "GET /inspection_events" do
+
+    let(:user) {
+      FactoryGirl.create(:user)
+    }
+
     it "visits inspection_events path" do
+      login_as(user, :scope => :user)
       visit inspection_events_path
       current_path.should == inspection_events_path
     end
