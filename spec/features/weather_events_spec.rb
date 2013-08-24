0d40fcf55eea75123dbff32fb6005839ffab2784
wyu <kharma@gmail.com>
2013-08-24 12:15:31 -0700

    55554478 - pass failing spec after adding before_filter statement

--- spec/features/weather_events_spec.rb
+++ spec/features/weather_events_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe "WeatherEvents" do
   describe "GET /weather_events" do
+    let!(:user) { FactoryGirl.create(:user) }
+
     it "visits weather_events path" do
+      login_as(user, :scope => :user)
       visit weather_events_path
       current_path.should == weather_events_path
     end
