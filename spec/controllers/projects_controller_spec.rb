dd92e31a4612e639dd856d3242ca54fea6d19c01
wyu <kharma@gmail.com>
2013-08-24 17:04:03 -0700

    55554478 - use devise helper to pass broken controller spec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -55,8 +55,8 @@ describe ProjectsController do
   describe "GET edit" do
     it "assigns the requested project as @project" do
       #project = Project.create! valid_attributes
-      sign_in @user
-      project = @user.projects.create! valid_attributes
+      # sign_in @user
+      project = user.projects.create! valid_attributes
       get :edit, {:id => project.to_param}
       assigns(:project).should eq(project)
     end
