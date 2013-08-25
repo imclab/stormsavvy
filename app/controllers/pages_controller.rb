d5d5994b0f249801db31869afac64d845634c4c6
wyu <kharma@gmail.com>
2013-08-24 23:54:57 -0700

    55771350 - route to correct index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, 
+  before_filter :authenticate_user!,
     :except => [:index, :about, :contact, :terms, :about_us, :privacy]
 
   def index
     if user_signed_in?
       redirect_to sites_path
+    else
+      redirect_to new_user_session_path
     end
   end
 
