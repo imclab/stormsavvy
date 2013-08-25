3e50b2042f075842419869a742ba59c45e019b49
wyu <kharma@gmail.com>
2013-08-24 18:33:28 -0700

    55554478 - add devise helper and pass broken inspection event controller spec

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -204,7 +204,6 @@ describe InspectionEventsController do
       end
 
       it "responds with flash message" do
-        sign_in current_user
         inspection_event = InspectionEvent.create! valid_attributes
         put :update, {:id => inspection_event.to_param, :inspection_event => valid_attributes}, valid_session
         # post :create, {:site => valid_attributes, :project_id => @project.id}
