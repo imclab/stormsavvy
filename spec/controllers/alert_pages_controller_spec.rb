025affe68f1da2a68a9009b5b50a34a4dea1880c
wyu <kharma@gmail.com>
2013-08-25 11:28:16 -0700

    Revert "55554478 - add devise helper and pass failing controller spec"
    This reverts commit 81d91279920829ed2cc0a0b621f462547002d9f9.

--- spec/controllers/alert_pages_controller_spec.rb
+++ spec/controllers/alert_pages_controller_spec.rb
@@ -2,18 +2,6 @@ require 'spec_helper'
 
 describe AlertPagesController do
 
-  include Devise::TestHelpers
-
-  let(:user) {
-    FactoryGirl.create(:user)
-    # login_as(user, :scope => :user)
-  }
-
-  before :each do
-    sign_in user
-  end
-
-
   describe "GET 'sender'" do
     it "returns http success" do
       get 'sender'
