0b35439e80bfe1d4b3665ae16082a896a4a5db62
wyu <kharma@gmail.com>
2013-08-28 21:01:38 -0700

    49800309: run site_id migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401174609) do
+ActiveRecord::Schema.define(:version => 20130829033045) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20130401174609) do
     t.boolean  "inspection_event"
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
+    t.integer  "site_id"
   end
 
   create_table "inspection_events", :force => true do |t|
