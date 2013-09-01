0878484bd592c26c1ea6518a5e2bd885c3f6941c
Adam Barber <adam@adambarber.tv>
2013-09-01 05:19:46 -0700

    refactored the business out of the dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,51 +1,14 @@
 class DashboardController < ApplicationController
+  skip_before_filter :authenticate_user!
 
   def index
     redirect_to index_path unless user_signed_in?
-    if current_user
-      @projects = current_user.projects
-
-      # pending_reports
-      # @reports = Report.where(:status => "needs_attention")
-
-      # get_ie
-      # @inspection_events = []
+    if user_signed_in?
+      @projects = current_user.projects.includes(sites: [:reports, :inspection_events])
+      @sites = @projects.each.collect { |p| p.sites }.flatten
+      @pending_ie = @sites.collect{|s| s.inspection_events.needs_attention }.flatten
+      @pending_reports = @sites.collect{|s| s.reports.select{ |r| r.status == 'needs_attention' } }.flatten
     end
   end
 
-  def get_ie
-    @sites = @projects.sites
-    @pending_ie = []
-    @sites.each do |s|
-      s.inspection_events.each do |ie|
-        if ie.completed == false
-          @pending_ie << ie
-        end
-      end
-    end
-
-    return @pending_ie
-  end
-
-  def get_reports
-    @all_reports = []
-    @projects.sites.each do |s|
-      s.reports.each { |r| @all_reports << r }
-    end
-
-    return @all_reports
-  end
-
-  def pending_reports
-    get_reports
-    @pending_reports = []
-
-    @all_reports.each do |r|
-      if r.status == 'needs_attention'
-        @pending_reports << r
-      end
-    end
-
-    return @pending_reports
-  end
 end
