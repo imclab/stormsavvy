24b0e81c5b3574cfe8f90c8ab90c8613a1a3c8d4
Adam Barber <adam@adambarber.tv>
2013-08-31 17:52:49 -0700

    Start refactoring the view to be less database intensive

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,45 +3,18 @@ class DashboardController < ApplicationController
   def index
     redirect_to index_path unless user_signed_in?
     if current_user
-      get_projects
-      # @projects = current_user.projects.all
+      @projects = current_user.projects
 
-      get_sites
-      # @sites = current_user.sites.all
-
-      pending_reports
+      # pending_reports
       # @reports = Report.where(:status => "needs_attention")
 
-      get_ie
+      # get_ie
       # @inspection_events = []
     end
   end
 
-  def get_projects
-    @projects = []
-    projects = current_user.projects.all
-
-    projects.each do |p|
-      @projects << p
-    end
-
-    return @projects
-  end
-
-  def get_sites
-    get_projects
-    @sites = []
-    @projects.each do |p|
-      p.sites.each do |s|
-        @sites << s
-      end
-    end
-
-    return @sites
-  end
-
   def get_ie
-    get_sites
+    @sites = @projects.sites
     @pending_ie = []
     @sites.each do |s|
       s.inspection_events.each do |ie|
@@ -55,12 +28,9 @@ class DashboardController < ApplicationController
   end
 
   def get_reports
-    get_sites
     @all_reports = []
-    @sites.each do |s|
-      s.reports.each do |r|
-        @all_reports << r
-      end
+    @projects.sites.each do |s|
+      s.reports.each { |r| @all_reports << r }
     end
 
     return @all_reports
