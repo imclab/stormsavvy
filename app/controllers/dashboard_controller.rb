56f8e02396bd6f2faa935e9cea9c383368809388
wyu <kharma@gmail.com>
2013-09-01 11:45:40 -0700

    56187152: resolve dashboard controller merge conflict

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  skip_before_filter :authenticate_user!
 
   def index
     redirect_to index_path unless user_signed_in?
@@ -56,19 +57,11 @@ class DashboardController < ApplicationController
       end
     end
 
-    return @all_reports
-  end
-
-  def pending_reports
-    get_reports
-    @pending_reports = []
-
-    @all_reports.each do |r|
-      if r.status == 'needs_attention'
-        @pending_reports << r
-      end
+    if user_signed_in?
+      @projects = current_user.projects.includes(sites: [:reports, :inspection_events])
+      @sites = @projects.each.collect { |p| p.sites }.flatten
+      @pending_ie = @sites.collect{|s| s.inspection_events.needs_attention }.flatten
+      @pending_reports = @sites.collect{|s| s.reports.select{ |r| r.status == 'needs_attention' } }.flatten
     end
-
-    return @pending_reports
   end
 end
