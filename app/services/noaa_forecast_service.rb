72f60524eac3dc2da0dba2a76d6304e63fc198fa
Adam Barber <adam@adambarber.tv>
2013-09-02 20:33:06 -0700

    working noaa_forecast service class

--- app/services/noaa_forecast_service.rb
+++ app/services/noaa_forecast_service.rb
@@ -8,24 +8,36 @@ class NoaaForecastService
     @lng = @site.lng
     @duration = opts[:duration] || 168
     @interval = opts[:interval] || 3
+    @forecast_periods = []
   end
 
   def get_forecast
     @response ||= fetch_noaa_data
   end
 
+  def save_results
+    @weather_update.save
+    save_forecast_periods
+  end
+
   private
+
+  def save_forecast_periods
+    @forecast_periods.each do |f|
+      puts f
+    end
+    # @forecast_periods.map(&:save)
+  end
 
   def fetch_noaa_data
     contact_noaa
     process_xml_response
-    # process_weather_update
+    process_weather_update
     process_forecast_periods
   end
 
   def contact_noaa
     url = "#{API_URL}duration=#{@duration}&interval=#{@interval}&lat=#{@lat}&lon=#{@lng}"
-    puts url
     @response = Unirest::get(url)
   end
 
@@ -36,7 +48,6 @@ class NoaaForecastService
   def process_weather_update
     @weather_update = @site.weather_updates.new
     @weather_update.build_from_xml(@xml)
-    @weather_update.save!
   end
 
   def process_forecast_periods
@@ -44,32 +55,20 @@ class NoaaForecastService
     forecast_days.each do |forecast_day|
       parse_forecase_day(forecast_day)
     end
+    puts @forecast_periods
   end
 
   def parse_forecase_day(forecast_day)
     validDate = Date.parse(forecast_day.xpath("validDate").text)
     forecast_day.xpath("period").each do |period|
-      puts parse_period(period, validDate)
+      @forecast_periods << create_forecast_period_record(period, validDate)
     end
   end
 
-  def parse_period(period, validDate)
-    validTime = period.xpath("validTime").text
-    fullTime = DateTime.parse "#{validDate} #{validTime}"
-    temperature = period.xpath("temperature").text
-    dewpoint = period.xpath("dewpoint").text
-    rh = period.xpath("rh").text
-    skyCover = period.xpath("skyCover").text
-    windSpeed = period.xpath("windSpeed").text
-    windDirection = period.xpath("windDirection").text
-    windGust = period.xpath("windGust").text
-    pop = period.xpath("pop").text
-    qpf = period.xpath("qpf").text
-    snowAmt = period.xpath("snowAmt").text
-    snowLevel = period.xpath("snowLevel").text
-    wx = period.xpath("wx").text
-    data = {forecast_prediction_time: fullTime, temperature: temperature, dewpoint: dewpoint, rh: rh, sky_cover: skyCover, wind_speed: windSpeed, wind_direction: windDirection, wind_gust: windGust, pop: pop, qpf: qpf, snow_amount: snowAmt, snow_level: snowLevel, wx: wx}
-    return data
+  def create_forecast_period_record(period, validDate)
+    forecast_period = @weather_update.forecast_periods.build
+    forecast_period.build_from_xml(period, validDate, @site.id)
+    forecast_period
   end
 
 end
