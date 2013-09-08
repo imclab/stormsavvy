04d965e50df1d4d85f05e227082d565f6531ccaa
wyu <kharma@gmail.com>
2013-09-08 16:03:12 -0700

    49800309: add site attribute spec

--- spec/models/sampling_event_spec.rb
+++ spec/models/sampling_event_spec.rb
@@ -43,56 +43,63 @@ describe SamplingEvent do
   	}
   end
 
-  it "has correct attributes" do
-    @sampling_event.site_id.should == 1
-    @sampling_event.sampling_type.should == "pH"
-    @sampling_event.sampling_description.should == "ec jungle gym sampling"
-    @sampling_event.sampling_date.should == "2013-01-27 15:34:07"
-    @sampling_event.submitted_by.should == "wyu"
-    @sampling_event.sampled_by.should == "wyu"
-    @sampling_event.sample_id_1.should == 1
-    @sampling_event.sample_location_1.should == "jungle gym"
-    @sampling_event.sample_time_1.should == "2000-01-01 15:34:07 UTC"
-    @sampling_event.analysis_type_1.should == "pH"
-    @sampling_event.analysis_result_1.should == "7.00"
-    @sampling_event.analysis_average_1.should == "7.01"
-    @sampling_event.sample_id_2.should == 1
-    @sampling_event.sample_location_2.should == "playground slide"
-    @sampling_event.sample_time_2.should == "2000-01-01 15:34:07 UTC"
-    @sampling_event.analysis_type_2.should == "pH"
-    @sampling_event.analysis_result_2.should == "7.10"
-    @sampling_event.analysis_average_2.should == "7.01"
-    @sampling_event.sample_id_3.should == 1
-    @sampling_event.sample_location_3.should == "playground seesaw"
-    @sampling_event.sample_time_3.should == "2000-01-01 15:34:07 UTC"
-    @sampling_event.analysis_type_3.should == "pH"
-    @sampling_event.analysis_result_3.should == "6.90"
-    @sampling_event.analysis_average_3.should== "7.01"
-    @sampling_event.sample_id_4.should == 1
-    @sampling_event.sample_location_4.should == "merry go round"
-    @sampling_event.sample_time_4.should == "2000-01-01 15:34:07 UTC"
-    @sampling_event.analysis_type_4.should == "pH"
-    @sampling_event.analysis_result_4.should == "6.95"
-    @sampling_event.analysis_average_4.should == "7.01"
-    @sampling_event.sample_id_5.should == 1
-    @sampling_event.sample_location_5.should == "playground swings"
-    @sampling_event.sample_time_5.should == "2000-01-01 15:34:07 UTC"
-    @sampling_event.analysis_type_5.should == "pH"
-    @sampling_event.analysis_result_5.should == "7.20"
-    @sampling_event.analysis_average_5.should == "7.01"
+  describe 'valid attributes' do
+    it "has correct attributes" do
+      @sampling_event.site_id.should == 1
+      @sampling_event.sampling_type.should == "pH"
+      @sampling_event.sampling_description.should == "ec jungle gym sampling"
+      @sampling_event.sampling_date.should == "2013-01-27 15:34:07"
+      @sampling_event.submitted_by.should == "wyu"
+      @sampling_event.sampled_by.should == "wyu"
+      @sampling_event.sample_id_1.should == 1
+      @sampling_event.sample_location_1.should == "jungle gym"
+      @sampling_event.sample_time_1.should == "2000-01-01 15:34:07 UTC"
+      @sampling_event.analysis_type_1.should == "pH"
+      @sampling_event.analysis_result_1.should == "7.00"
+      @sampling_event.analysis_average_1.should == "7.01"
+      @sampling_event.sample_id_2.should == 1
+      @sampling_event.sample_location_2.should == "playground slide"
+      @sampling_event.sample_time_2.should == "2000-01-01 15:34:07 UTC"
+      @sampling_event.analysis_type_2.should == "pH"
+      @sampling_event.analysis_result_2.should == "7.10"
+      @sampling_event.analysis_average_2.should == "7.01"
+      @sampling_event.sample_id_3.should == 1
+      @sampling_event.sample_location_3.should == "playground seesaw"
+      @sampling_event.sample_time_3.should == "2000-01-01 15:34:07 UTC"
+      @sampling_event.analysis_type_3.should == "pH"
+      @sampling_event.analysis_result_3.should == "6.90"
+      @sampling_event.analysis_average_3.should== "7.01"
+      @sampling_event.sample_id_4.should == 1
+      @sampling_event.sample_location_4.should == "merry go round"
+      @sampling_event.sample_time_4.should == "2000-01-01 15:34:07 UTC"
+      @sampling_event.analysis_type_4.should == "pH"
+      @sampling_event.analysis_result_4.should == "6.95"
+      @sampling_event.analysis_average_4.should == "7.01"
+      @sampling_event.sample_id_5.should == 1
+      @sampling_event.sample_location_5.should == "playground swings"
+      @sampling_event.sample_time_5.should == "2000-01-01 15:34:07 UTC"
+      @sampling_event.analysis_type_5.should == "pH"
+      @sampling_event.analysis_result_5.should == "7.20"
+      @sampling_event.analysis_average_5.should == "7.01"
+    end
+
+    it 'returns correct pulldown menu values' do
+      SamplingEvent::TYPES.should == [ 'CEM2050', 'CEM2051', 'CEM2052' ]
+    end
+
+    it "creates new instance given valid attributes" do
+      sampling_event = SamplingEvent.new(@attr)
+      sampling_event.save
+      sampling_event.should be_valid
+    end
+
+    it 'requires site_id for new sampling_event' do
+      no_name = SamplingEvent.new
+      no_name.should_not be_valid
+    end
   end
 
-  it 'returns correct pulldown menu values' do
-    SamplingEvent::TYPES.should == [ 'CEM2050', 'CEM2051', 'CEM2052' ]
-  end
-
-  it "creates new instance given valid attributes" do
-    sampling_event = SamplingEvent.new(@attr)
-    sampling_event.save
-    sampling_event.should be_valid
-  end
-
-  describe "sampling_event associations" do
+  describe "associations" do
     before(:each) do
       @sampling_event = SamplingEvent.new(@attr)
     end
@@ -114,7 +121,7 @@ describe SamplingEvent do
       SamplingEvent.any_instance.stub(:destroy_attached_files).and_return(true) 
       @attachment = FactoryGirl.create :sampling_event
     end
-    
+
     describe "#attachment" do
       it "returns correct url" do
         @attachment.attachment.url.should_not be_nil
