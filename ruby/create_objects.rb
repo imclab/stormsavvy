e13869194b5893620a13b45988ee62ce9e051945
wyu <kharma@gmail.com>
2013-09-25 22:59:24 -0700

    56187152: add script for generating sites and forecast periods

--- ruby/create_objects.rb
+++ ruby/create_objects.rb
@@ -51,6 +51,16 @@ zipcode = 94901
 nf = NOAAForecast.new(zipcode)
 forecast = nf.seven_day_weather(zipcode)
 
+site = Site.last
+site.lat = 38.2470
+site.lng = -122.5620
+site.save
+noaa = NoaaForecastService.new(:site => site)
+noaa.get_forecast
+noaa.save_results
+site.forecast_periods.where('forecast_prediction_time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).map(&:pop)
+ForecastPeriod.where('pop = ?', 66)
+
 time = Time.new
 values = time.to_a
 p values
