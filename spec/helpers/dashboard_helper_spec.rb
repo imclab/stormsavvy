dbb691f77b8e9278e7caef19d4586892569c092a
wyu <kharma@gmail.com>
2013-10-03 09:12:32 -0700

    49801121: remove uncalled instance vars

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -3,113 +3,18 @@ require 'current_user_object'
 
 describe DashboardHelper do
 
-  before(:each) do
-    @fullcount = 29
-    @zipcode = 94530
-    @lat = 37.9202057
-    @long = -122.2937428
-
-    @nf = double(NOAAForecast)
-    @nf2 = NOAAForecast.new(@zipcode,168,6)
-    @pop = @nf2.pop
-    @qpf = @nf2.qpf
-
-    @user = FactoryGirl.create(:user)
-    @project = FactoryGirl.create(
-      :project,
-      :user => @user,
-      :created_at => 1.day.ago
-    )
-    @projects = [@project]
-
-    @site = FactoryGirl.create(
-      :site,
-      :project => @project,
-      :name => 'ec jungle gym',
-      :zipcode => 94530
-    )
-    @sites = [@site]
-
-    @ie = FactoryGirl.create(
-      :inspection_event,
-      :site => @site
-    )
-
-    @se = FactoryGirl.create(
-      :sampling_event,
-      :site => @site
-    )
-
-    # factories for instance variable method spec
-    @current_user = FactoryGirl.build(
-      :user,
-      :email => '@stormsavvy.com'
-    )
-    @other_user = FactoryGirl.build(
-      :user,
-      :email => 'info@stormsavvy.com'
-    )
-    @all_users = [ @current_user, @other_user ]
-
-    @current_project = FactoryGirl.create(
-      :project,
-      :user => @current_user
-    )
-    @other_project = FactoryGirl.create(
-      :project,
-      :user => @other_user
-    )
-    @current_projects = [ @current_project ]
-    @other_projects = [ @other_project ]
-    @all_projects = [ @current_project, @other_project ]
-
-    @current_site = FactoryGirl.create(
-      :site,
-      :project => @current_project,
-      :name => 'ec jungle gym',
-      :zipcode => 94530
-    )
-    @other_site = FactoryGirl.create(
-      :site,
-      :project => @other_project,
-      :name => 'berkeley high',
-      :zipcode => 94709
-    )
-    @current_sites = [ @current_site ]
-    @other_sites = [ @other_site ]
-    @all_sites = [ @current_site, @other_site ]
-
-    @current_ie = FactoryGirl.create(
-      :inspection_event,
-      :site => @current_site
-    )
-    @other_ie = FactoryGirl.create(
-      :inspection_event,
-      :site => @other_site
-    )
-    @current_ie_array = [ @current_ie ]
-    @other_ie_array = [ @other_ie ]
-    @all_ie_array = [ @current_ie, @other_ie ]
-
-    @completed_report = FactoryGirl.create(
-      :report,
-      :site => @current_site,
-      :status => "completed"
-    )
-    @pending_report = FactoryGirl.create(
-      :report,
-      :site => @current_site,
-      :status => "needs_attention"
-    )
-    @completed_reports = [ @completed_report ]
-    @pending_reports = [ @pending_report ]
-    @all_reports = [ @completed_report, @pending_report ]
-  end
+  let(:fullcount) { 29 }
+  let(:zipcode) { 94530 }
+  let(:lat) { 37.9202057 }
+  let(:long) { -122.2937428 }
+  let(:nf) { double(NOAAForecast) }
+  let(:nf2) { NOAAForecast.new(zipcode, 168, 6) }
+  let(:pop) { nf.pop }
+  let(:qpf) { nf.qpf }
 
   describe '#chance_of_rain' do
     it "returns @max_rain" do
-      zipcode = 94530
-      @nf2.seven_day_weather(zipcode)
+      nf2.seven_day_weather(zipcode)
 
       # where is method called in noaa forecast?
       # @max_rain = nf.noaa_forecast[0][0..4].max
@@ -118,7 +23,6 @@ describe DashboardHelper do
 
   describe '#precipitation_state' do
     it "returns @rain_state" do
-      # finish spec later
       forecast = []
       if forecast == [[],[]]
         @rain_state = :null
