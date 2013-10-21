7d5d322b04a7bd920425913ef4d1a30cd2b09efd
wyu <kharma@gmail.com>
2013-10-21 07:19:54 -0700

    58308942: revert to Project.new in lieu of current user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
+    # @project = current_user.projects.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
