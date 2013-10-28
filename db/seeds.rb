89df5bc0fdd58584a3e908cab24cdcde79c238c8
wyu <kharma@gmail.com>
2013-10-28 09:00:05 -0700

    58308942: remove unused variable

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ Location.transaction do
 
   File.open(DATAFILE, 'r').each_with_index do |line,idx|
     zip,name,lat,long = line.split("\t").values_at(1,5,9,10)
-    l = Location.create({ 
-      :zipcode => zip, 
-      :lat => lat, 
-      :long => long, 
-      :name => name 
+    Location.create({
+      :zipcode => zip,
+      :lat => lat,
+      :long => long,
+      :name => name
     })
     puts "#{idx} lines imported" if idx % 100 == 0
 
