c14e3eeb65677a9837f2de67a6176e55f7947e52
wyu <kharma@gmail.com>
2013-10-28 08:02:51 -0700

    58308942: correct email validation typo with .com

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,16 @@
 FactoryGirl.define do
-  sequence :email do |n|
-    'email#{n}@stormsavvy.com'
-  end
-
   factory :user do
+    sequence(:email) { |n| 'email#{n}@stormsavvy.com' }
     firstname "Walter"
     lastname "Yu"
-    email
+    # email
     password "passwood"
     password_confirmation "passwood"
     remember_me :true
 
     factory :user_with_projects do
+      sequence(:email) { |n| 'email#{n}@stormsavvy.com' }
+
       ignore do
         projects_count 1
       end
