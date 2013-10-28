12b0948f2a0aefbb790901afaa5bf4b2df514686
wyu <kharma@gmail.com>
2013-10-28 07:54:53 -0700

    58308942: add factory sequence

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    'email#{n}@stormsavvy.com'
+  end
+
   factory :user do
     firstname "Walter"
     lastname "Yu"
-    email "walter@stormsavvy.com"
+    email
     password "passwood"
     password_confirmation "passwood"
     remember_me :true
