cad39eb69b2f8665b81ed625b4dd31c96e5f04ea
wyu <kharma@gmail.com>
2013-10-28 08:06:30 -0700

    58308942: test out nested site factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,11 +8,14 @@ FactoryGirl.define do
     password_confirmation "passwood"
     remember_me :true
 
-    factory :user_with_projects do
+    factory :user_with_sites do
       sequence(:email) { |n| 'email#{n}@stormsavvy.com' }
 
       ignore do
         projects_count 1
+        ignore do
+          sites_count 2
+        end
       end
 
       after(:create) do | user, evaluator|
