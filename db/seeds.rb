88810d13c80ae5c6aa44f36eb24e1068336c4d5a
wyu <kharma@gmail.com>
2013-10-28 09:30:50 -0700

    58308942: update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,6 @@ Location.delete_all
 
 # Read the contents of the dump file from geonames
 Location.transaction do 
-  #puts "Skipping location import"
-  #break
-
   File.open(DATAFILE, 'r').each_with_index do |line,idx|
     zip,name,lat,long = line.split("\t").values_at(1,5,9,10)
     Location.create({
@@ -18,104 +15,104 @@ Location.transaction do
       :long => long,
       :name => name
     })
-    puts "#{idx} lines imported" if idx % 100 == 0
+    pp "#{idx} lines imported" if idx % 100 == 0
 
     break if idx > 500
   end
 end
 
-puts "User seed data"
+pp "User seed data"
 User.destroy_all
-@user = User.create!(
+user = User.create!(
   :firstname => "Jim",
   :lastname => "Nelson",
-  :email => "jim.nelson@test.com", 
+  :email => "jim.nelson@test.com",
   :password => "tester"
-)
+  )
 
-puts "Project seed data"
+pp "Project seed data"
 Project.destroy_all
-@project1 = Project.create(
+hwy_101 = Project.create!(
   :name => "Hwy 101 Corridor",
   :description => "Highway irmprovements in Marin/Sonoma counties", 
   :startdate => "2011-02-20 12:01:00",
   :finishdate => "2012-02-20 12:01:00"
   )
 
-@project2 = Project.create!(
+dt_oakland = Project.create!(
   :name => "DT Oakland",
   :description => "Downtown Oakland Junction",
   :startdate => "2012-03-01 12:01:00",
   :finishdate => "2012-03-12 12:01:00"
   )
 
-@user.projects << @project1
-@user.projects << @project2
+user.projects << hwy_101
+user.projects << dt_oakland
 
-puts "Sites seed data"
+pp "Sites seed data"
 Site.destroy_all
 
 Site.create!(
-  :name => "Sausalito 101", 
-  :address_1 => "HWY 101", 
+  :name => "Sausalito 101",
+  :address_1 => "HWY 101",
   :city => "Sausalito",
   :zipcode => "99999"
   )
-  
+
 Site.create!(
-  :name => "Sears Oakland", 
-  :address_1 => "1955 Broadway", 
+  :name => "Sears Oakland",
+  :address_1 => "1955 Broadway",
   :city => "Oakland",
   :zipcode => "99998"
   )
 
 Site.create!(
-  :name => "Portland", 
-  :address_1 => "100 Main Street", 
+  :name => "Portland",
+  :address_1 => "100 Main Street",
   :city => "Portland",
   :zipcode => "99997"
   )
 
 Site.create!(
-  :name => "San Francisco", 
-  :address_1 => "100 Main Street", 
+  :name => "San Francisco",
+  :address_1 => "100 Main Street",
   :city => "San Francisco",
   :zipcode => "99996"
   )
 
 Site.create!(
-  :name => "Seattle", 
-  :address_1 => "100 Main Street", 
+  :name => "Seattle",
+  :address_1 => "100 Main Street",
   :city => "Seattle",
   :zipcode => "99995"
   )
 
 Site.create!(
-  :name => "Los Angeles", 
-  :address_1 => "100 Main Street", 
+  :name => "Los Angeles",
+  :address_1 => "100 Main Street",
   :city => "Los Angeles",
   :zipcode => "99994"
   )
 
-@project1.sites << Site.all.to_a
+hwy_101.sites << Site.all.to_a
 
 # Site seed data
-Site.create(
+Site.create!(
   :name => "MSN-A1",
   :description => "Hwy 101 Novato - PM 18.6-22.3",
-  :costcode => "264064", 
-  :size => "20 acres", 
+  :costcode => "264064",
+  :size => "20 acres",
   #:address1 => "320 Deere Lane",
-  #:address2 => "", 
-  :state => "California", 
-  :zipcode => "99999", 
-  :city => "Novato", 
+  #:address2 => "",
+  :state => "California",
+  :zipcode => "99999",
+  :city => "Novato",
   :exposed_area => "10 acres"
   )
 
 # User seed data
-User.create(
-  :email => "name@stormsavvy.com", 
+User.create!(
+  :email => "name@stormsavvy.com",
   :password => "Dark&Stormy",
   :password_confirmation => "Dark&Stormy"
   )
