cad5599b9f2ee7328e52f6d3542fbfa6720bf0e8
wyu <kharma@gmail.com>
2013-10-28 21:27:36 -0700

    58308942: use let! statement, not instance vars

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -49,129 +49,90 @@ describe Project do
     end
   end
 
-  before :each do
-  	@project = FactoryGirl.create(:project_with_sites)
-  	@attr = {
-  	  :name => "Hwy 99",
-  	  :description => "Pothole Repair",
-  	  :startdate => DateTime.new(2010),
-  	  :finishdate => DateTime.new(2011),
-  	  :active => true
-  	}
-
-    @project1 = FactoryGirl.create(
-      :project,
-      :user => @user,
-      :created_at => 1.day.ago
-      )
-    @project2 = FactoryGirl.create(
-      :project,
-      :user => @user,
-      :created_at => 1.hour.ago
-      )
-    @projects = [@project1, @project2]
-
-    @site1 = FactoryGirl.create(
-      :site,
-      :project => @project1,
-      :name => 'ec jungle gym',
-      :zipcode => 94530
-      )
-    @site2 = FactoryGirl.create(
-      :site,
-      :project => @project2,
-      :name => 'ec playground slide',
-      :zipcode => 94530
-      )
-    @sites = [@site1, @site2]
-  end
-
-  it "has correct project attributes" do
-    # @project.name.should == "Hwy 101 Corridor"
-    @project.startdate.should == DateTime.new(2011)
-  	@project.finishdate.should == DateTime.new(2012)
-  	@project.active.should == false
-
-  end
-
   describe "validations" do
     it "should require a name" do
-      @project.name = ''
-      @project.should_not be_valid
+      project.name = ''
+      project.should_not be_valid
     end
 
     it "should require a description" do
-      @project.description = ''
-      @project.should_not be_valid
+      project.description = ''
+      project.should_not be_valid
     end
 
     it "requires a starting date" do
-      @project.startdate = ''
-      @project.should be_valid
+      project.startdate = ''
+      project.should be_valid
     end
 
     it "requires a finishing date" do
-      @project.finishdate = ''
-      @project.should be_valid
+      project.finishdate = ''
+      project.should be_valid
+    end
+
+    it "has correct project attributes" do
+      # @project.name.should == "Hwy 101 Corridor"
+      project.startdate.should == DateTime.new(2011)
+      project.finishdate.should == DateTime.new(2012)
+      project.active.should == false
     end
   end
 
   describe "project associations" do
-
     context :user do
       it "should be associated with a user" do
-        @project.should respond_to(:user)
+        project.should respond_to(:user)
       end
 
       it "should have the correct associated user" do
-        @project.should == @project
+        project.should == project
       end
     end
 
     context :site do
       it "should be associated with sites" do
-        @project.should respond_to(:sites)
+        project.should respond_to(:sites)
       end
 
       it "should be able to add 1 site" do
         lambda{
           FactoryGirl.create(
             :site,
-            :project => @project
+            :project => project
           )
-        }.should change(@project.sites, :count).by(1)
+        }.should change(project.sites, :count).by(1)
       end
 
       it "should not able to add invalid sites" do
         lambda{
-          @project.sites << Site.new
-        }.should change(@project.sites, :count).by(0)
+          project.sites << Site.new
+        }.should change(project.sites, :count).by(0)
       end
 
       it "should be able to add multiple sites" do
         lambda{
-          @project.sites << @site1
-          @site1.save
-        }.should change(@project.sites, :count).by(1)
+          project.sites << site1
+          site1.save
+        }.should change(project.sites, :count).by(1)
       end
 
       it "should be able to delete an added site" do
-        precount = @project.sites_count
-        @project.sites << @site1
-        @site1.save
-        @site1.delete
-        @project.sites_count.should == precount
+        precount = project.sites_count
+        project.sites << site1
+        site1.save
+        site1.delete
+        project.sites_count.should == precount
       end
     end
   end
 
   describe "date format validations" do
     it "start date should be less than finish date" do
-      @project.startdate.should < @project.finishdate
+      project.startdate.should < project.finishdate
     end
 
     it "start date should not be greater than finish date" do
-      @project.startdate.should_not > @project.finishdate
+      project.startdate.should_not > project.finishdate
     end
 
     it "finish date should raise error" do
@@ -186,11 +147,11 @@ describe Project do
   describe '#get_site_zipcodes' do
     it 'returns array of site zipcodes' do
       zipcodes = []
-      @project.sites.each do |site|
+      project.sites.each do |site|
         zipcodes << site.zipcode
       end
 
-      @project.get_site_zipcodes.should == zipcodes
+      project.get_site_zipcodes.should == zipcodes
     end
   end
 end
