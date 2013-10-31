a63372b9901c6d4e43ebfa4cf3af415b6eed8103
wyu <kharma@gmail.com>
2013-10-30 18:27:22 -0700

    58308942: use let! statements, not instance vars

--- spec/models/sampling_event_spec.rb
+++ spec/models/sampling_event_spec.rb
@@ -53,42 +53,42 @@ describe SamplingEvent do
 
   describe 'valid attributes' do
     it "has correct attributes" do
-      @sampling_event.site_id.should == 1
-      @sampling_event.sampling_type.should == "pH"
-      @sampling_event.sampling_description.should == "ec jungle gym sampling"
-      @sampling_event.sampling_date.should == "2013-01-27 15:34:07"
-      @sampling_event.submitted_by.should == "wyu"
-      @sampling_event.sampled_by.should == "wyu"
-      @sampling_event.sample_id_1.should == 1
-      @sampling_event.sample_location_1.should == "jungle gym"
-      @sampling_event.sample_time_1.should == "2000-01-01 15:34:07 UTC"
-      @sampling_event.analysis_type_1.should == "pH"
-      @sampling_event.analysis_result_1.should == "7.00"
-      @sampling_event.analysis_average_1.should == "7.01"
-      @sampling_event.sample_id_2.should == 1
-      @sampling_event.sample_location_2.should == "playground slide"
-      @sampling_event.sample_time_2.should == "2000-01-01 15:34:07 UTC"
-      @sampling_event.analysis_type_2.should == "pH"
-      @sampling_event.analysis_result_2.should == "7.10"
-      @sampling_event.analysis_average_2.should == "7.01"
-      @sampling_event.sample_id_3.should == 1
-      @sampling_event.sample_location_3.should == "playground seesaw"
-      @sampling_event.sample_time_3.should == "2000-01-01 15:34:07 UTC"
-      @sampling_event.analysis_type_3.should == "pH"
-      @sampling_event.analysis_result_3.should == "6.90"
-      @sampling_event.analysis_average_3.should== "7.01"
-      @sampling_event.sample_id_4.should == 1
-      @sampling_event.sample_location_4.should == "merry go round"
-      @sampling_event.sample_time_4.should == "2000-01-01 15:34:07 UTC"
-      @sampling_event.analysis_type_4.should == "pH"
-      @sampling_event.analysis_result_4.should == "6.95"
-      @sampling_event.analysis_average_4.should == "7.01"
-      @sampling_event.sample_id_5.should == 1
-      @sampling_event.sample_location_5.should == "playground swings"
-      @sampling_event.sample_time_5.should == "2000-01-01 15:34:07 UTC"
-      @sampling_event.analysis_type_5.should == "pH"
-      @sampling_event.analysis_result_5.should == "7.20"
-      @sampling_event.analysis_average_5.should == "7.01"
+      sampling_event.site_id.should == 1
+      sampling_event.sampling_type.should == "pH"
+      sampling_event.sampling_description.should == "ec jungle gym sampling"
+      sampling_event.sampling_date.should == "2013-01-27 15:34:07"
+      sampling_event.submitted_by.should == "wyu"
+      sampling_event.sampled_by.should == "wyu"
+      sampling_event.sample_id_1.should == 1
+      sampling_event.sample_location_1.should == "jungle gym"
+      sampling_event.sample_time_1.should == "2000-01-01 15:34:07 UTC"
+      sampling_event.analysis_type_1.should == "pH"
+      sampling_event.analysis_result_1.should == "7.00"
+      sampling_event.analysis_average_1.should == "7.01"
+      sampling_event.sample_id_2.should == 1
+      sampling_event.sample_location_2.should == "playground slide"
+      sampling_event.sample_time_2.should == "2000-01-01 15:34:07 UTC"
+      sampling_event.analysis_type_2.should == "pH"
+      sampling_event.analysis_result_2.should == "7.10"
+      sampling_event.analysis_average_2.should == "7.01"
+      sampling_event.sample_id_3.should == 1
+      sampling_event.sample_location_3.should == "playground seesaw"
+      sampling_event.sample_time_3.should == "2000-01-01 15:34:07 UTC"
+      sampling_event.analysis_type_3.should == "pH"
+      sampling_event.analysis_result_3.should == "6.90"
+      sampling_event.analysis_average_3.should== "7.01"
+      sampling_event.sample_id_4.should == 1
+      sampling_event.sample_location_4.should == "merry go round"
+      sampling_event.sample_time_4.should == "2000-01-01 15:34:07 UTC"
+      sampling_event.analysis_type_4.should == "pH"
+      sampling_event.analysis_result_4.should == "6.95"
+      sampling_event.analysis_average_4.should == "7.01"
+      sampling_event.sample_id_5.should == 1
+      sampling_event.sample_location_5.should == "playground swings"
+      sampling_event.sample_time_5.should == "2000-01-01 15:34:07 UTC"
+      sampling_event.analysis_type_5.should == "pH"
+      sampling_event.analysis_result_5.should == "7.20"
+      sampling_event.analysis_average_5.should == "7.01"
     end
 
     it 'returns correct pulldown menu values' do
