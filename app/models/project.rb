b8f8506eb2a3dda5570ab441c775a1439d23b4d0
wyu <kharma@gmail.com>
2013-10-31 22:18:24 -0700

    58308942: simplify project model for debugging

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,23 +9,23 @@ class Project < ActiveRecord::Base
     :active,
     :sites_attributes
 
+  belongs_to :user
+  accepts_nested_attributes_for :user
+
   has_many :sites, :dependent => :destroy
   accepts_nested_attributes_for :sites
 
-  validates_presence_of :name,
-    :description
-    # :startdate,
-    # :finishdate
+  # validates_presence_of :name, :description, :startdate, :finishdate
 
-    # before_save :check_dates
+  # before_save :check_dates
 
-    # def check_dates
-    #   return false if self.startdate > self.finishdate
+  # def check_dates
+  #   return false if self.startdate > self.finishdate
 
-    #   if self.startdate > self.finishdate
-    #     raise ActiveRecord::RecordNotSaved, 'Starting date must precede finishing date'
-    #   end
-    # end
+  #   if self.startdate > self.finishdate
+  #     raise ActiveRecord::RecordNotSaved, 'Starting date must precede finishing date'
+  #   end
+  # end
 
   def get_site_zipcodes
     zipcodes = []
