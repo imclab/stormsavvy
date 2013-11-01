b1253d2a3e3e0898ce7d00755d655bce4b8c1f4d
wyu <kharma@gmail.com>
2013-10-31 21:41:02 -0700

    58308942: use let! statement, not instance vars

--- spec/models/sampling_event_workflow_spec.rb
+++ spec/models/sampling_event_workflow_spec.rb
@@ -1,102 +1,104 @@
 require 'spec_helper'
 
 describe SamplingEventWorkflow do
-  before(:each) do
-    let!(:user) { FactoryGirl.create(:user) }
-    let!(:project) { FactoryGirl.create(
-      :project,
-      :user => @user
-      )
-    }
-    @site = FactoryGirl.create(
-      :site,
-      :project => @project
+
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:project) { FactoryGirl.create(
+    :project,
+    :user => user
     )
-    @se = FactoryGirl.create(
-      :sampling_event,
-      :site => @site
+  }
+  let!(:site) { FactoryGirl.create(
+    :site,
+    :project => project
     )
-    @sew = FactoryGirl.create(
-      :sampling_event_workflow,
-      :site => @site
+  }
+  let!(:se) { FactoryGirl.create(
+    :sampling_event,
+    :site => site
     )
-  end
+  }
+  let!(:sew) { FactoryGirl.create(
+    :sampling_event_workflow,
+    :site => site
+    )
+  }
 
   it "default model is valid" do
-    @sew.should be_valid
+    sew.should be_valid
   end
 
   describe '#check_cem2051' do
     xit "should add CEM2051 if status = false, 24 hours before rain" do
       expect{
-        @sew.hours_before_rain = 24
-        @sew.chance_of_rain = 55
-        @sew.cem2051 = false
-        @sew.check_cem2051
+        sew.hours_before_rain = 24
+        sew.chance_of_rain = 55
+        sew.cem2051 = false
+        sew.check_cem2051
       }.to change(SamplingEvent, :count).by(1)
 
       # se = SamplingEvent.create(
       #   :completed => false
       # )
       # SamplingEvent.needs_attention.all.should == 1
-      # @sew.check_cem2051.should =~ /CEM2051 prepared/
+      # sew.check_cem2051.should =~ /CEM2051 prepared/
     end
 
     it "should not add CEM2051 if status = true, 24 hours before rain" do
       expect{
-        @sew.hours_before_rain = 24
-        @sew.chance_of_rain = 55
-        @sew.cem2051 = true
-        @sew.check_cem2051
+        sew.hours_before_rain = 24
+        sew.chance_of_rain = 55
+        sew.cem2051 = true
+        sew.check_cem2051
       }.to change(SamplingEvent, :count).by(0)
 
-      # @sew.check_cem2051.should_not =~ /Prepare CEM2051/
+      # sew.check_cem2051.should_not =~ /Prepare CEM2051/
     end
   end
 
   describe '#check_cem2052' do
     xit "should add CEM2052 if status = false, 24 hours before rain" do
       expect{
-        @sew.hours_before_rain = 24
-        @sew.chance_of_rain = 55
-        @sew.cem2052 = false
-        @sew.check_cem2052
+        sew.hours_before_rain = 24
+        sew.chance_of_rain = 55
+        sew.cem2052 = false
+        sew.check_cem2052
       }.to change(SamplingEvent, :count).by(1)
 
-      # @sew.check_cem2052.should =~ /CEM2052 prepared/
+      # sew.check_cem2052.should =~ /CEM2052 prepared/
     end
 
     it "should not add CEM2052 if status = true, 24 hours before rain" do
       expect{
-        @sew.hours_before_rain = 24
-        @sew.chance_of_rain = 55
-        @sew.cem2052 = true
-        @sew.check_cem2052
+        sew.hours_before_rain = 24
+        sew.chance_of_rain = 55
+        sew.cem2052 = true
+        sew.check_cem2052
       }.to change(SamplingEvent, :count).by(0)
 
-      # @sew.check_cem2052.should_not =~ /CEM2052 prepared/
+      # sew.check_cem2052.should_not =~ /CEM2052 prepared/
     end
   end
 
   describe '#sampling_event' do
     it "checks to see if sampling is imminent" do
       expect{
-        @sew.hours_before_rain = 23
-        @sew.chance_of_rain = 55
-        @sew.forecast_rain = 0.55
-        @sew.sampling_event = false
+        sew.hours_before_rain = 23
+        sew.chance_of_rain = 55
+        sew.forecast_rain = 0.55
+        sew.sampling_event = false
       }.to change(SamplingEvent, :count).by(0)
 
       # lambda do
-      #  if @sew.sampling_imminent?
-      #    @sew.start_sampling_event_workflow
+      #  if sew.sampling_imminent?
+      #    sew.start_sampling_event_workflow
       #  end
       # end.should change(SamplingEvent, :count).by(1) # double-count?
     end
 
     it "should check to see whether it has rained" do
-      @sew.amount_of_rain = 0.51
-      @sew.has_rained?.should be_true
+      sew.amount_of_rain = 0.51
+      sew.has_rained?.should be_true
     end
   end
 end
