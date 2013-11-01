3a7c97b5c18f9cf94f60f083696d29fe5f5cd1fd
wyu <kharma@gmail.com>
2013-11-01 08:22:13 -0700

    49799949: store arrays into fixture files

--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -121,82 +121,22 @@ describe NOAAForecast do
 
     nf3 = NOAAForecast.new(zipcode,168,6)
     nf3.stub(:get_pop_table_hash) {
-      time_pop_hash = nf2.get_time_pop_hash
-      new_qpf_array = @qpf
-      IO.read("./spec/fixtures/get_pop_table_hash.rb")
+
+      # time_pop_hash = nf2.get_time_pop_hash
+      # new_qpf_array = @qpf
 
       # array not being returned correctly
       # pop_table_hash = []
       # for i in 0..27
       #   pop_table_hash << Hash[time_pop_hash[i]].update(Hash[new_qpf_array[i]])
       # end
-=begin
-      [
-        time_pop_hash[1].update(new_qpf_array[1]),
-        time_pop_hash[2].update(new_qpf_array[2]),
-        time_pop_hash[3].update(new_qpf_array[3]),
-        time_pop_hash[4].update(new_qpf_array[4]),
-        time_pop_hash[5].update(new_qpf_array[5]),
-        time_pop_hash[6].update(new_qpf_array[6]),
-        time_pop_hash[7].update(new_qpf_array[7]),
-        time_pop_hash[8].update(new_qpf_array[8]),
-        time_pop_hash[9].update(new_qpf_array[9]),
-        time_pop_hash[10].update(new_qpf_array[10]),
-        time_pop_hash[11].update(new_qpf_array[11]),
-        time_pop_hash[12].update(new_qpf_array[12]),
-        time_pop_hash[13].update(new_qpf_array[13]),
-        time_pop_hash[14].update(new_qpf_array[14]),
-        time_pop_hash[15].update(new_qpf_array[15]),
-        time_pop_hash[16].update(new_qpf_array[16]),
-        time_pop_hash[17].update(new_qpf_array[17]),
-        time_pop_hash[18].update(new_qpf_array[18]),
-        time_pop_hash[19].update(new_qpf_array[19]),
-        time_pop_hash[20].update(new_qpf_array[20]),
-        time_pop_hash[21].update(new_qpf_array[21]),
-        time_pop_hash[22].update(new_qpf_array[22]),
-        time_pop_hash[23].update(new_qpf_array[23]),
-        time_pop_hash[24].update(new_qpf_array[24]),
-        time_pop_hash[25].update(new_qpf_array[25]),
-        time_pop_hash[26].update(new_qpf_array[26]),
-        time_pop_hash[27].update(new_qpf_array[27]),
-      ]
-=end
+
       IO.read("./spec/fixtures/get_pop_table_hash.rb")
       return zipcode
     }
 
     nf2.stub(:get_forecast_array) {
-      [
-        { :date => ProjectLocalTime::format(Date.today + 0.hours), :weather => pop[0], :rainfall => qpf[0] },
-        { :date => ProjectLocalTime::format(Date.today + 6.hours), :weather => pop[1], :rainfall => qpf[1] },
-        { :date => ProjectLocalTime::format(Date.today + 12.hours), :weather => pop[2], :rainfall => qpf[2] },
-        { :date => ProjectLocalTime::format(Date.today + 18.hours), :weather => pop[3], :rainfall => qpf[3] },
-        { :date => ProjectLocalTime::format(Date.today + 24.hours), :weather => pop[4], :rainfall => qpf[4] },
-        { :date => ProjectLocalTime::format(Date.today + 30.hours), :weather => pop[5], :rainfall => qpf[5] },
-        { :date => ProjectLocalTime::format(Date.today + 36.hours), :weather => pop[6], :rainfall => qpf[6] },
-        { :date => ProjectLocalTime::format(Date.today + 42.hours), :weather => pop[7], :rainfall => qpf[7] },
-        { :date => ProjectLocalTime::format(Date.today + 48.hours), :weather => pop[8], :rainfall => qpf[8] },
-        { :date => ProjectLocalTime::format(Date.today + 54.hours), :weather => pop[9], :rainfall => qpf[9] },
-        { :date => ProjectLocalTime::format(Date.today + 60.hours), :weather => pop[10], :rainfall => qpf[10] },
-        { :date => ProjectLocalTime::format(Date.today + 66.hours), :weather => pop[11], :rainfall => qpf[11] },
-        { :date => ProjectLocalTime::format(Date.today + 72.hours), :weather => pop[12], :rainfall => qpf[12] },
-        { :date => ProjectLocalTime::format(Date.today + 78.hours), :weather => pop[13], :rainfall => qpf[13] },
-        { :date => ProjectLocalTime::format(Date.today + 84.hours), :weather => pop[14], :rainfall => qpf[14] },
-        { :date => ProjectLocalTime::format(Date.today + 90.hours), :weather => pop[15], :rainfall => qpf[15] },
-        { :date => ProjectLocalTime::format(Date.today + 96.hours), :weather => pop[16], :rainfall => qpf[16] },
-        { :date => ProjectLocalTime::format(Date.today + 102.hours), :weather => pop[17], :rainfall => qpf[17] },
-        { :date => ProjectLocalTime::format(Date.today + 108.hours), :weather => pop[18], :rainfall => qpf[18] },
-        { :date => ProjectLocalTime::format(Date.today + 114.hours), :weather => pop[19], :rainfall => qpf[19] },
-        { :date => ProjectLocalTime::format(Date.today + 120.hours), :weather => pop[20], :rainfall => qpf[20] },
-        { :date => ProjectLocalTime::format(Date.today + 126.hours), :weather => pop[21], :rainfall => qpf[21] },
-        { :date => ProjectLocalTime::format(Date.today + 132.hours), :weather => pop[22], :rainfall => qpf[22] },
-        { :date => ProjectLocalTime::format(Date.today + 138.hours), :weather => pop[23], :rainfall => qpf[23] },
-        { :date => ProjectLocalTime::format(Date.today + 144.hours), :weather => pop[24], :rainfall => qpf[24] },
-        { :date => ProjectLocalTime::format(Date.today + 150.hours), :weather => pop[25], :rainfall => qpf[25] },
-        { :date => ProjectLocalTime::format(Date.today + 156.hours), :weather => pop[26], :rainfall => qpf[26] },
-        { :date => ProjectLocalTime::format(Date.today + 162.hours), :weather => pop[27], :rainfall => qpf[27] },
-        { :date => ProjectLocalTime::format(Date.today + 168.hours), :weather => pop[28], :rainfall => qpf[28] }
-      ]
+      IO.read("./spec/fixtures/get_forecast_array.rb")
       return zipcode
     }
   end # need this here for before :each block!
