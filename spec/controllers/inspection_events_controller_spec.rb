3cffa2968fb0ce2bf9b0d78e23de7a621553d80c
wyu <kharma@gmail.com>
2013-11-27 22:43:12 -0800

    58308942: correct sites factories

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -53,14 +53,16 @@ describe InspectionEventsController do
 
   let!(:current_site) { FactoryGirl.create(
       :site,
-      :project => current_project,
+      :user => current_user,
+      # :project => current_project,
       :name => 'ec jungle gym',
       :zipcode => 94530
     )
   }
   let(:other_site) { FactoryGirl.create(
       :site,
-      :project => other_project,
+      :user => other_user,
+      # :project => other_project,
       :name => 'berkeley high',
       :zipcode => 94709
     )
