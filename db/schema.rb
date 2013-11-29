a5989ddec0d72de58796419372304f988eeae6ef
wyu <kharma@gmail.com>
2013-11-29 15:35:57 -0800

    58308942: update site model lng to long

--- db/schema.rb
+++ db/schema.rb
@@ -251,18 +251,18 @@ ActiveRecord::Schema.define(:version => 20130903022521) do
     t.string   "size"
     t.string   "exposed_area"
     t.string   "location"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.integer  "user_id"
     t.integer  "project_id"
     t.string   "zipcode"
+    t.float    "lat"
+    t.float    "long"
     t.string   "address_1"
     t.string   "address_2"
     t.string   "city"
     t.string   "state"
     t.string   "risk_level"
-    t.float    "lat",          :default => 0.0, :null => false
-    t.float    "lng",          :default => 0.0, :null => false
   end
 
   create_table "users", :force => true do |t|
