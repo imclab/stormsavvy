66c31d0855b9ecff2755e7bd43c80de6e3b88556
wyu <kharma@gmail.com>
2013-12-01 14:44:33 -0800

    44671865: remove unused bash scripts

--- bash/run_scheduler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-heroku run rake scheduler:dailynotice -a stormsavvy
-heroku run rake scheduler:northbay_forecast -a stormsavvy
-heroku run rake scheduler:southbay_forecast -a stormsavvy
-heroku run rake scheduler:eastbay_forecast -a stormsavvy
-# heroku run rake scheduler:stormpopnotice -a stormsavvy
-# heroku run rake scheduler:noaaalert -a stormsavvy
