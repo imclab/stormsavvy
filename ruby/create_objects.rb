f461b9b7f455f3ee7c741400214e0b048b9c8f7e
wyu <kharma@gmail.com>
2013-12-01 15:48:00 -0800

    44671865: remove unused migration scripts

--- ruby/create_objects.rb
+++ ruby/create_objects.rb
@@ -1,58 +1,11 @@
 # generates objects in console
-user.delete
-project.delete
-site.delete
-site2.delete
-reload!
-user = User.new(
-	:email => 'walter@stormsavvy1.com',
-	:password => 'foobar1'
-	)
-user.save
-project = user.projects.new(
-  :name => 'ec park and rec',
-  :description => 'playground improvements',
-  :startdate => DateTime.new(2011),
-  :finishdate => DateTime.new(2012)
-  )
-project.save
-site = project.sites.new(
-  :name => 'ec jungle gym',
-  :zipcode => 94530
-  )
-site.save
-site2 = project.sites.new(
-  :name => 'ec playground',
-  :zipcode => 94530
-  )
-site2.save
-
-sites = user.sites
-puts sites
-
-user.sites.each do |s|
-  site = pp s.name
-  pp site
-end
 
 we = WeatherEvent.new
 we.save
 
-user = User.last
-project = user.projects.build
-project.save
-pp user.projects
-
-project = Project.new(:name => 'foo', :description => 'bar')
-project.save
-site = project.sites.build(:name => 'ecp', :zipcode => 94530)
-site.save
-
-report = ReportsPdf.new
-report.testem_fields
-
-email = "walter@stormsavvy.com"
-AlertMailer.pester_admins(email).deliver
+time = Time.new
+values = time.to_a
+p values
 
 zipcode = 94901
 nf = NOAAForecast.new(zipcode)
