faa2942e5ec36821f67476e8234a2e0c0f116979
wyu <kharma@gmail.com>
2013-12-01 15:32:07 -0800

    49799949: refactor pdf controller methods

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -9,85 +9,61 @@ class PdfController < ApplicationController
     send_data pdf.render, type: "application/pdf", disposition: "inline"
   end
 
+  def CEM2030
+    # filename = "#{Prawn::DATADIR}/images/reports/CEM2030_production_v1.pdf"
+    @file = "CEM2030_production_v1"
+    self.set_defaults
+  end
+
   def CEM2034
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2034_production_v1.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2034_production_v1.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2034_production_v1"
+    self.set_defaults
   end
 
   def CEM2035
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2035_production_v1.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2035_production_v1.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2035_production_v1"
+    self.set_defaults
   end
 
   def CEM2040
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2040_production_v1.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2040_production_v1.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2040_production_v1"
+    self.set_defaults
   end
 
   def CEM2045
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2045_production.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2045_production.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2045_production"
+    self.set_defaults
   end
 
   def CEM2050
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2050_production.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2050_production.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2050_production"
+    self.set_defaults
   end
 
   def CEM2051
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2051_production.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2051_production.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2051_production"
+    self.set_defaults
   end
 
   def CEM2052
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2052_production.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM2052_production.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM2051_production"
+    self.set_defaults
   end
 
   def CEM4601
     # filename = "#{Prawn::DATADIR}/images/reports/CEM4601_production.pdf"
-    filename = "#{Rails.root}/app/assets/pdfs/CEM4601_production.pdf"
-    pdf = Prawn::Document.new(
-      :template => filename,
-      :page_size => "A4"
-    )
-    send_data pdf.render, type: "application/pdf", disposition: "inline"
+    @file = "CEM4601_production"
+    self.set_defaults
   end
+
+=begin
 
   def CEM2030
     # filename = "#{Prawn::DATADIR}/images/reports/CEM2030_production.pdf"
