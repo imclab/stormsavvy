d100a0984bae332cc73fe61f6c051bb3fd68c7c0
wyu <kharma@gmail.com>
2013-12-26 15:54:55 -0800

    45459461: update secret token settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Stormsavvy::Application.config.secret_token = '3987930edc64fedc4cc8a510be09cdce08b28fe59376164f37789233ece07f300989e8235a62240dd3657cb8692e64827ad3fafd92f137324c55e76525fff6ba'
+Stormsavvy::Application.config.secret_key_base = '91ea1d359bed0b4897a1450cd87c346ddbd9166740370d3b292299ced0cd619e98313e05ddf3eef0320ac52c57382f67011d26ea7cbee75ef2759082e249ef34'
