cd31ad6a0d043851b0cf797fa54bc02f5e0ded76
wyu <kharma@gmail.com>
2013-12-27 11:42:17 -0800

    63017688: debugging spec

--- spec/lib/weather/weathergetter_spec.rb
+++ spec/lib/weather/weathergetter_spec.rb
@@ -11,19 +11,17 @@ describe WeatherGetter do
   let(:zipcode) { 94530 }
 
   it "gets the weather for 94530" do
-    begin
-      wg = WeatherGetter.new
-      forecast = wg.get_forecast(94530)
+    # begin
+      forecast = wg.get_forecast(zipcode)
       forecast.should include('forecast')
-    rescue
-      pp 'not online or wunderground api call failed'
-    end
+    # rescue
+    #   pp 'not online or wunderground api call failed'
+    # end
   end
 
   it "extracts wunderground's 10 day txt_forecast" do
-    # wg = WeatherGetter.new
     fcd = wg.parse_wunderground_10day(@json)
-    #fcd.should include('date')
+    # fcd.should include('date')
     fcd.should have(10).items
   end
 
