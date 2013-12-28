676d3e7f9336c7a7782e103e8449b030196d5b99
wyu <kharma@gmail.com>
2013-12-27 20:19:11 -0800

    63017688: add perform method

--- app/workers/weather_worker.rb
+++ app/workers/weather_worker.rb
@@ -1,7 +1,10 @@
 class WeatherWorker
   include Sidekiq::Worker
+  sidekiq_options queue: "high"
 
-  def wg
-
+  def perform(weathergetter_id)
+    zipcode = 94530
+    @forecast = WeatherGetter.get_forecast(zipcode)
+    @forecastday = WeatherGetter.parse_wunderground_10day(@forecast)
   end
 end
