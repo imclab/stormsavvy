f1e75ba7f3adb8d38d016c297286fca9b5ba3e1f
wyu <kharma@gmail.com>
2013-12-28 09:05:18 -0800

    63039448: organize ruby script

--- ruby/create_objects.rb
+++ ruby/create_objects.rb
@@ -8,15 +8,17 @@ nf = NOAAForecast.new(zipcode)
 forecast = nf.seven_day_weather(zipcode)
 pp forecast
 
-# Script for generating site and forecast periods in console
 site = Site.new(name: 'ecp', zipcode: 94530)
 site.save
 site.lat
 site.long
 
-site.chance_of_rain.pop
+# Associates site with forecast, then save it
 noaa = NoaaForecastService.new(:site => site)
 noaa.get_forecast
 noaa.save_results
+
+# Logic inside chance_of_rain method
 site.forecast_periods.where('forecast_prediction_time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).map(&:pop)
 ForecastPeriod.where('pop = ?', 66)
+site.chance_of_rain.pop
