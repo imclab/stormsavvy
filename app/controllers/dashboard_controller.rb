ff01f23b651c415ba51fbc73b32fcb6350122ea3
wyu <kharma@gmail.com>
2013-12-28 09:02:21 -0800

    63039448: update from project to site

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,10 @@ class DashboardController < ApplicationController
   def index
     redirect_to index_path unless user_signed_in?
     if user_signed_in?
-      @projects = current_user.projects.includes(sites: [:reports, :inspection_events])
-      @sites = @projects.each.collect { |p| p.sites }.flatten
+      # @projects = current_user.projects.includes(sites: [:reports, :inspection_events])
+      # @sites = @projects.each.collect { |p| p.sites }.flatten
+      sites = current_user.sites
+      @sites = sites.each.collect { |s| s.sites }.flatten
       @pending_ie = @sites.collect{|s| s.inspection_events.needs_attention }.flatten
       @pending_reports = @sites.collect{|s| s.reports.select{ |r| r.status == 'needs_attention' } }.flatten
     end
