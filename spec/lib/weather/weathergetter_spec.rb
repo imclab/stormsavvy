847c5551483605b9acff89115aedc535744bb6bc
wyu <kharma@gmail.com>
2013-12-27 20:22:12 -0800

    63017688: refactor display_forecast spec

--- spec/lib/weather/weathergetter_spec.rb
+++ spec/lib/weather/weathergetter_spec.rb
@@ -15,38 +15,34 @@ describe WeatherGetter do
   it "gets the weather for 94530" do
     # begin
       forecast = wg.get_forecast(zipcode)
-      pp @forecast
+      pp forecast
 
+      forecast.should include('response')
       forecast.should include('forecast')
+      forecast.should have(2).items
     # rescue
-    #   pp 'no netowrk connection or query failed'
+    #   pp 'no network connection or query failed'
     # end
   end
 
   it "extracts wunderground's 10 day txt_forecast" do
     # begin
-      fcd = wg.parse_wunderground_10day(json)
-      pp fcd
-
-      # fcd.should include('date')
-      fcd.should have(10).items
+      forecast = wg.parse_wunderground_10day(json)
+      forecast.should have(10).items
     # rescue
-    #   pp 'no netowrk connection or query failed'
+    #   pp 'no network connection or query failed'
     # end
   end
 
-  it '#make_request' do
+  it '#display_forecast' do
     # begin
       ww.class.should == WeatherWorker
       ww.should_not be_nil
 
-      # APIKEY = IO.read('./config/wunderground_apikey.txt').chomp
-      url = "http://api.wunderground.com/api/#{APIKEY}/forecast10day/q/#{zipcode}.json"
-      pp url
-      wg.make_request(url)
-      pp @data
+      forecast = wg.display_forecast
+      forecast.should have(24).items
     # rescue
-    #   pp 'not online or wunderground api call failed'
+    #   pp 'no network connection or query failed'
     # end
   end
 end
