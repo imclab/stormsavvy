da457b34e32fd416ccca80f88a2b70e73b828c6f
wyu <kharma@gmail.com>
2013-12-29 18:20:46 -0800

    63039448: update deploy script

--- bash/deploy_s4.sh
+++ bash/deploy_s4.sh
@@ -3,5 +3,5 @@
 git push git@heroku.com:stormsavvy4.git master
 heroku restart worker.1 -a stormsavvy4
 heroku restart -a stormsavvy4
-heroku logs -a stormsavvy4
 heroku open -a stormsavvy4
+heroku logs -a stormsavvy4
