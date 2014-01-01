7f5ad5583e158662c6c5ed9a283599484d89ba81
wyu <kharma@gmail.com>
2014-01-01 13:07:17 -0800

    63039448: remove cruft

--- spec/views/pages/_hero.html.haml_spec.rb
+++ spec/views/pages/_hero.html.haml_spec.rb
@@ -2,23 +2,6 @@ require 'spec_helper'
 
 describe "pages/index" do
 
-=begin
-  def site_attributes
-    { :name => 'Foo bar', :zipcode => '94530' }
-  end
-
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-    @p1 = FactoryGirl.create(:project)
-    @p2 = FactoryGirl.create(:project)
-    @projects = [@p1, @p2]
-    @needs_attention_reports = Report.needs_attention
-    #@s1 = @p1.sites.create(site_attributes)
-    #@s2 = @p2.sites.create(site_attributes)
-    sign_in @user
-  end
-=end
-
   describe 'pages/index' do
     before do
       view.stub(:index).and_return(true)
