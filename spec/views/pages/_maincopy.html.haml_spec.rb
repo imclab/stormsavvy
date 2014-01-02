0830f561d77ad0ce8e2fd329a36f6c5270e441df
wyu <kharma@gmail.com>
2014-01-01 17:36:33 -0800

    63130000: comment out lat/long spec for later

--- spec/views/pages/_maincopy.html.haml_spec.rb
+++ spec/views/pages/_maincopy.html.haml_spec.rb
@@ -8,8 +8,8 @@ describe "pages/_maincopy" do
 
   it 'checks maincopy partial view' do
     render
-    rendered.should =~ /Protect the Environment/
-    rendered.should =~ /More Effectively/
-    rendered.should =~ /Sign up now to start tracking your projects/
+    rendered.should =~ /Protect the Environment,/
+    rendered.should =~ /Together and In Real-Time/
+    rendered.should =~ /Sign up now to start tracking projects, together/
   end
 end
