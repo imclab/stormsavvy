dc968c328972bacfdfc330b3112961e580d9cabe
wyu <kharma@gmail.com>
2014-01-13 22:01:57 -0800

    44591773: remove noaa_alert mailer, views and spec

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -139,7 +139,6 @@ class AlertMailer < ActionMailer::Base
 
       if user.has_site?
         mail(
-          :from     => "alerts@stormsavvy.com",
           :to       => "#{user.firstname} #{user.lastname} <#{user.email}>",
           :subject  => "NOAA Forecast Notification"
           ).deliver
