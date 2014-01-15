e58f6efa817d1b25b6f1433ac32ed1eb93f268b7
wyu <kharma@gmail.com>
2014-01-14 22:21:04 -0800

    63675240: remove check_auth filter

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,15 +1,5 @@
 class SitesController < ApplicationController
   respond_to :json
-  before_filter :check_auth
-
-  def check_auth
-    authenticate_or_request_with_http_basic do |username,password|
-      resource = User.find_by_email(username)
-      if resource.valid_password?(password)
-        sign_in :user, resource
-      end
-    end
-  end
 
   # GET /sites
   # GET /sites.json
