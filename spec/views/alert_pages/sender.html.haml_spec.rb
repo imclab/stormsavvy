7b57c222cd145b821f1427674bacf0c902866946
wyu <kharma@gmail.com>
2014-01-14 18:42:21 -0800

    49799949: remove unused mvc and spec

--- spec/views/alert_pages/sender.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "alert_pages/sender.html.haml" do
-  it "has a send link on the page for sending email" do    
-    render    
-    rendered.should have_selector('a')
-  end
-end
