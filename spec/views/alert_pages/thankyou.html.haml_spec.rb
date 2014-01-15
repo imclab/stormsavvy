cf33ada85932ed61f6d15e58dbe25490759bddf4
wyu <kharma@gmail.com>
2014-01-14 18:42:25 -0800

    49799949: remove unused mvc and spec

--- spec/views/alert_pages/thankyou.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "alert_pages/thankyou.html.haml" do
-  it "thanks the user for sending him or herself email" do
-    render
-    rendered.should =~ /thank you/i
-  end
-end
