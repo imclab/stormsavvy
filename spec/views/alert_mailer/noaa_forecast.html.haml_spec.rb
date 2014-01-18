ac4b6c3f61ea33ff8db94417602b6200d9790228
wyu <kharma@gmail.com>
2014-01-18 00:32:36 -0800

    44591773: sorting out site and pop arrays

--- spec/views/alert_mailer/noaa_forecast.html.haml_spec.rb
+++ spec/views/alert_mailer/noaa_forecast.html.haml_spec.rb
@@ -3,23 +3,31 @@ require 'spec_helper'
 describe "alert_mailer/noaa_forecast" do
 
   let!(:user) { FactoryGirl.build(:user) }
-  let!(:site) { user.sites.build(
+  let!(:ecp) { user.sites.build(
     name: 'ecp',
     zipcode: 94530
     )
   }
-  let!(:nfs) {
-    NoaaForecastService.new(site: site)
+  let!(:ucb) { user.sites.build(
+    name: 'ucb',
+    zipcode: 94709
+    )
   }
+  let!(:sites) { [] << ecp << ucb }
 
   it "renders the noaa forecast alert email view" do
     sign_in user
     @user = user
 
-    site.save
-    @site = site
+    ecp.save
+    ucb.save
+    @site_pop = []
 
-    @site_pop = nfs.site_pop(site)
+    sites.each do |site|
+      nfs = NoaaForecastService.new(site: site)
+      site_pop = nfs.site_pop(site)
+      @site_pop << site_pop
+    end
 
     render
     rendered.should =~ /Here are the daily forecasts for your sites:/
