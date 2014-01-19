68c1ff86b6451e1e6b05fdeb98aa545b326a4710
wyu <kharma@gmail.com>
2014-01-19 13:58:15 -0800

    64115114: update noaa info

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -6,7 +6,8 @@ class AlertMailer < ActionMailer::Base
     @greeting = "Greetings"
     @salutation = "The Storm Savvy Team"
     @support = "Questions? Email us at info@stormsavvy.com!"
-    @noaa_info = "Forecast is based on the NOAA API. Compare with the forecast table:"
+    @noaa_info = "Forecast is based on the NOAA API."
+    @noaa_website = "Compare results with the website:"
     @noaa_url = "http://www.wrh.noaa.gov/forecast/wxtables/index.php?"
   end
 
