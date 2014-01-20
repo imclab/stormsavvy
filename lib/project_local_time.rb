a757919896b898e6ca34dc23278ddc6917baf46e
wyu <kharma@gmail.com>
2014-01-19 19:40:37 -0800

    64115114: add date_only method

--- lib/project_local_time.rb
+++ lib/project_local_time.rb
@@ -6,4 +6,8 @@ class ProjectLocalTime
   def self.format(localtime)
     return localtime.strftime("%A, %e %B %Y %H:%M %Z")
   end
+
+  def self.date_only(localtime)
+    return localtime.strftime("%A, %e %B %Y")
+  end
 end
