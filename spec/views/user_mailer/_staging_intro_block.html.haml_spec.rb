e4182b89d1f882b9ad4d18ca24ae1db9d8681d5e
wyu <kharma@gmail.com>
2014-01-20 10:59:04 -0800

    64115114: move spec between staging mailer partials

--- spec/views/user_mailer/_staging_intro_block.html.haml_spec.rb
+++ spec/views/user_mailer/_staging_intro_block.html.haml_spec.rb
@@ -4,5 +4,6 @@ describe "user_mailer/_staging_intro_block" do
   it "renders the intro partial" do
     render
     rendered.should have_content "Storm Savvy Staging App Email"
+    rendered.should match("Please see current stats of the app below.")
   end
 end
