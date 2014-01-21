6f5242cdf8aea1704a0bf275ae82171f3cf20c6b
wyu <kharma@gmail.com>
2014-01-21 08:24:23 -0800

    64207616: replace double with instance

--- spec/lib/weather/noaa_forecast_spec.rb
+++ spec/lib/weather/noaa_forecast_spec.rb
@@ -175,7 +175,7 @@ describe NOAAForecast do
   describe "#seven_day_weather" do
     it "returns array from seven_day_weather" do
       begin
-        forecast = nf2.seven_day_weather(zipcode)
+        forecast = nf.seven_day_weather(zipcode)
         forecast[0].size.should == fullcount
       rescue
         'Not online or method throwing error'
