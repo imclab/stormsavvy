eb6e8abc7b79e40eaca42e332230c9e88ddb23fd
wyu <kharma@gmail.com>
2014-01-20 21:26:32 -0800

    63244662: remove project spec

--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -246,7 +246,6 @@ describe SitesController do
       expect {
         delete :destroy, {
           :id => site.to_param,
-          # :project_id => @project.id
         }, valid_session
       }.to change(Site, :count).by(-1)
     end
