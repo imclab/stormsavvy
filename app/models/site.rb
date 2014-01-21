9074027fdac701d050741219e8cc181f1ef2ee88
wyu <kharma@gmail.com>
2014-01-20 21:24:52 -0800

    63244662: remove unused variable

--- app/models/site.rb
+++ app/models/site.rb
@@ -48,7 +48,7 @@ class Site < ActiveRecord::Base
 
   def forecast_table
     nfs = NoaaForecastService.new(site: self)
-    forecast = nfs.forecast_table(self)
+    nfs.forecast_table(self)
   end
 
   def forecast
