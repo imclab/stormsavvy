e6017196b9d25d72f2eca1d6ec20488f4d78f5ae
wyu <kharma@gmail.com>
2014-01-21 23:27:24 -0800

    64290058: save loop logic for later

--- lib/weather/noaa_forecast.rb
+++ lib/weather/noaa_forecast.rb
@@ -266,7 +266,6 @@ class NOAAForecast
       { :date => ProjectLocalTime::format(Date.today + 162.hours), :weather => pop[0][27], :rainfall => pop[1][27] },
       { :date => ProjectLocalTime::format(Date.today + 168.hours), :weather => pop[0][28], :rainfall => pop[1][28] }
     ]
-=end
   end
 
   def ping_noaa(latlong, duration, interval)
