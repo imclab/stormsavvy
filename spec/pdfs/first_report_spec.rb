528a96af7e65ac74ba101135158f8fca8b557c53
wyu <kharma@gmail.com>
2014-01-24 11:18:02 -0800

    64431120: add let statements

--- spec/pdfs/first_report_spec.rb
+++ spec/pdfs/first_report_spec.rb
@@ -4,16 +4,17 @@ require 'pathname'
 
 describe FirstReport do
 
+  let(:report) { FactoryGirl.create(:report) }
+  let(:filename) { "#{Prawn::DATADIR}/images/reports/CEM2030-2012_Page_01.png" }
+  let(:img_data) { File.open(@filename, "rb") { |f| f.read } }
+
   before(:each) do
-    @report = FactoryGirl.create(:report)
-    @filename = "#{Prawn::DATADIR}/images/reports/CEM2030-2012_Page_01.png"
-    @img_data = File.open(@filename, "rb") { |f| f.read }
     create_pdf
   end
 
-  it "should only embed an image once, even if it's added multiple times" do
-    @pdf.image @filename, :at => [100,100]
-    @pdf.image @filename, :at => [300,300]
+  it "embeds only image once, even if added multiple times" do
+    @pdf.image filename, :at => [100,100]
+    @pdf.image filename, :at => [300,300]
 
     output = @pdf.render
     images = PDF::Inspector::XObject.analyze(output)
