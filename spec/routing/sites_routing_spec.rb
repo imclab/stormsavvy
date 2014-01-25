726be9feab635701a7be04d2d5299aa6597329e1
wyu <kharma@gmail.com>
2014-01-25 14:09:08 -0800

    64431120: clean up controller spec

--- spec/routing/sites_routing_spec.rb
+++ spec/routing/sites_routing_spec.rb
@@ -4,66 +4,64 @@ describe SitesController do
 
   describe "routing" do
 
+    let(:user) { FactoryGirl.build(:user) }
+    let(:site) { FactoryGirl.create(
+      :site,
+      user: user
+      )
+    }
     before(:each) do
-      attrs = {
-        :name => 'Project',
-        :description => 'My description',
-        :startdate => 5.days.ago,
-        :finishdate => 1.day.ago}
-      @user = FactoryGirl.create(:user)
-      @project = @user.projects.create!(attrs)
-      # Stub this later to speed it up...
-      @site = @project.sites.create!({:name => 'My site', :zipcode => '94530'})
     end
 
     it "routes to #index" do
-      { :get => sites_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "users_sites", :format => @site.to_param)
+      { :get => sites_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "index", :format => site.to_param)
+      # .should route_to(:controller => "sites", :action => "users_sites", :format => @site.to_param)
       # { :get => project_sites_path(@project.to_param) }
       # .should route_to(:controller => "sites", :action => "index", :project_id => @project.to_param)
     end
 
     it "routes to #new" do
-      { :get => new_site_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "new", :format => @site.to_param)
+      { :get => new_site_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "new", :format => site.to_param)
       # { :get => new_project_site_path(@project.to_param) }
       # .should route_to(:controller => "sites", :action => "new", :project_id => @project.to_param)
     end
 
     it "routes to #show" do
-      { :get => site_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "show", :id => @site.to_param)
+      { :get => site_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "show", :id => site.to_param)
       # { :get => project_site_path(@project.to_param, @site.to_param) }
       # .should route_to(:controller => "sites", :action => "show", 
       #                  :project_id => @project.to_param, :id => @site.to_param)
     end
 
     it "routes to #edit" do
-      { :get => edit_site_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "edit", :id => @site.to_param)
+      { :get => edit_site_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "edit", :id => site.to_param)
       # { :get => edit_project_site_path(@project.to_param, @site.to_param) }
       # .should route_to(:controller => "sites", :action => "edit", 
       #                  :project_id => @project.to_param, :id => @site.to_param)
     end
 
     it "routes to #create" do
-      { :post => sites_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "create", :format => @site.to_param)
+      { :post => sites_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "create", :format => site.to_param)
       # { :post => project_sites_path(@project.to_param) }
       # .should route_to(:controller => "sites", :action => "create", :project_id => @project.to_param)
     end
 
     it "routes to #update" do
-      { :put => site_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "update", :id => @site.to_param)
+      { :put => site_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "update", :id => site.to_param)
       # { :put => project_site_path(@project.to_param, @site.to_param) }
       # .should route_to(:controller => "sites", :action => "update", 
       #                  :project_id => @project.to_param, :id => @site.to_param)
     end
 
     it "routes to #destroy" do
-      { :delete => site_path(@site.to_param) }
-      .should route_to(:controller => "sites", :action => "destroy", :id => @site.to_param)
+      { :delete => site_path(site.to_param) }
+      .should route_to(:controller => "sites", :action => "destroy", :id => site.to_param)
       # { :delete => project_site_path(@project.to_param, @site.to_param) }
       # .should route_to(:controller => "sites", :action => "destroy", 
       #                  :project_id => @project.to_param, :id => @site.to_param)
