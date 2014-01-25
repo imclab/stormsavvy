c71c09ee60806fc1bebc8710487a54ac290ec2ed
wyu <kharma@gmail.com>
2014-01-25 12:32:14 -0800

    64431120: spec out image background

--- spec/pdfs/reports_pdf_spec.rb
+++ spec/pdfs/reports_pdf_spec.rb
@@ -8,9 +8,15 @@ describe ReportsPdf do
 
   let(:report) { ReportsPdf.new }
   # let(:report) { double(ReportsPdf) }
-  let(:filename) { "#{Prawn::DATADIR}/images/reports/CEM2031-2012_Page_01.png" }
+  let(:filename) { "#{Prawn::DATADIR}/images/pigs.jpg" }
+  let(:cem) { "#{Prawn::DATADIR}/images/reports/CEM2031-2012_Page_01.png" }
+
+  before(:each) do
+    create_pdf
+  end
 
   xit 'renders pdf correctly' do
+    # pdf-reader gem for spec
     reader = PDF::Reader.new(StringIO.new(pdf.build))
     page = Capybara.string(reader.pages.collect(&:text).join("\n"))
     page.should have_content('text')
@@ -20,6 +26,19 @@ describe ReportsPdf do
     it 'places background image' do
       report.should respond_to(:page_background)
       pp report.page_background(filename)
+    end
+
+    it "returns default image info object" do
+      info =  @pdf.image(filename)
+      info.should be_a_kind_of(Prawn::Images::JPG)
+      info.height.should == 453
+    end
+
+    it "returns default image info object" do
+      info =  @pdf.image(cem)
+      pp info
+      info.should be_a_kind_of(Prawn::Images::PNG)
+      # info.height.should == 453
     end
   end
 end
