01de402a781af2415207c03a2ab18d0705d7dbf6
wyu <kharma@gmail.com>
2014-01-26 09:59:13 -0800

    63437680: correct load order

--- spec/javascripts/karma.conf.js
+++ spec/javascripts/karma.conf.js
@@ -2,26 +2,45 @@ module.exports = function(config) {
   config.set({
     basePath: '../..',
 
-    frameworks: ['jasmine'],
-
-    autoWatch: true,
-
     preprocessors: {
       '**/*.coffee': 'coffee'
     },
 
+    frameworks: ['jasmine', 'ng-scenario'],
+
+    autoWatch: true,
+
+    colors: true,
+
+    port: 9876,
+
+    logLevel: config.LOG_DEBUG,
+
+    browsers: ['Safari'],
+
+    loggers: [{type: 'console'}],
+
+    singlerun: false,
+
+    plugins : [
+      'karma-jasmine',
+      'karma-ng-scenario',
+      'karma-safari-launcher',
+      'karma-coffee-preprocessor',
+      'karma-junit-reporter'
+    ],
+
+    exclude: [
+      'app/assets/javascripts/angular-loader.js',
+    ],
+
     files: [
       'app/assets/javascripts/angular.js',
-      'app/assets/javascripts/angular-*.js',
       'app/assets/javascripts/angular-mocks.js',
       'app/assets/javascripts/main.js.coffee',
-      'app/assets/javascripts/application.js',
-      'app/assets/javascripts/controllers/*',
-      'app/assets/javascripts/controllers/main/*',
-      'app/assets/javascripts/directives/*',
-      'app/assets/javascripts/filters/*',
-      'app/assets/javascripts/services/*',
-      'spec/javascripts/*_spec.js.coffee'
+      'app/assets/javascripts/controllers/main/mainSiteCtrl.js.coffee',
+      'app/assets/javascripts/controllers/main/mainIndexCtrl.js.coffee',
+      'spec/javascripts/controllers_spec.js.coffee'
     ]
   });
 };
