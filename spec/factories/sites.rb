1c69f3aeb43b6fa922a305d62455a118e33188db
wyu <kharma@gmail.com>
2014-01-27 09:34:34 -0800

    49044959: add site attributes

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -15,5 +15,45 @@ FactoryGirl.define do
 
     lat 38.2473117
     long 122.5712101
+
+    project_ea '3A23U4'
+    wdid_number '004001005'
+    construction_phase 'active'
+
+    contractor_name 'gcc'
+    contractor_address_1 'gcc hq'
+    contractor_address_2 '246 gcc ave'
+    contractor_city 'santa rosa'
+    contractor_state 'CA'
+    contractor_zipcode '95407'
+    contract_number '154009'
+
+    wpcm_name 'yoda'
+    wpcm_company 'gcc'
+    wpcm_phone '707-555-9999'
+    wpcm_emergency_phone '707-555-9999'
+    qsp_name 'obi'
+    qsp_company 'gcc'
+    qsp_phone '707-555-9999'
+    qsp_emergency_phone '707-555-9999'
+
+    total_area 50.00
+    total_dsa 30.00
+    current_dsa 20.00
+    inactive_dsa 10.00
+
+    time_since_last_storm '99 days'
+    precipitation_received '0.50 inches'
+
+    permits '401/1601'
+    resident_engineer_name 'yogi'
+    structures_representative_name 'barney'
+    last_bmp_status 'Mon, 27 Jan 2014'
+    last_inspection 'Mon, 27 Jan 2014'
+    last_corrective_action 'Mon, 27 Jan 2014'
+    last_reap 'Mon, 27 Jan 2014'
+    last_training 'Mon, 27 Jan 2014'
+    last_weather_forecast 'Mon, 27 Jan 2014'
+    last_sampling 'Mon, 27 Jan 2014'
   end
 end
