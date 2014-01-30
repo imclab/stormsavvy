c39d9ca73c622738f6cba5a2383cad8ae1a565ce
wyu <kharma@gmail.com>
2014-01-29 23:21:35 -0800

    64756666: debugging capybara field matchers

--- spec/features/sites_spec.rb
+++ spec/features/sites_spec.rb
@@ -11,29 +11,26 @@ describe "Sites" do
     { "warden.user.user.key" => session["warden.user.user.key"] }
   end
 
+  let(:user) { FactoryGirl.build(:user) }
+
   before :each do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, :scope => :user)
-    visit new_project_path
   end
 
   describe "GET /sites" do
-
-    it "saves the new project" do
+    it "saves the new site" do
       visit root_path
-      click_link 'New Site'
+      login_as(user, :scope => :user)
 
       # visit new_site_path
-      current_path.should == new_site_path
       page.should have_content('New Site')
-      fill_in 'Name', :with => 'ECP Improvements', :match => :prefer_exact
-      fill_in 'Description', :with => 'Jungle Gym, etc.', :match => :prefer_exact
+      click_link 'New Site'
+      current_path.should == new_site_path
+      fill_in 'Name', :with => 'ecp', :match => :prefer_exact
       fill_in 'Zipcode', :with => 94530
       click_button 'Save'
 
       page.should have_content('Site was successfully created.')
       current_path.should == '/sites/1'
     end
-
   end
 end
