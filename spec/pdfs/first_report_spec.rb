370b5484a6ff09091937869abf49b6446f692b45
wyu <kharma@gmail.com>
2014-01-31 18:58:44 -0800

    64431120: generate pdf to correct path

--- spec/pdfs/first_report_spec.rb
+++ spec/pdfs/first_report_spec.rb
@@ -68,7 +68,7 @@ describe FirstReport do
       result = PDF::Inspector::Page.analyze(pdf.render)
       result.pages.size.should === 1
 
-      pdf.render_file 'stormsavvy.pdf'
+      pdf.render_file "#{Rails.root}/app/assets/pdfs/text_analysis.pdf"
     end
   end
 
