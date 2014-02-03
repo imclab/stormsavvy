3600da458c9eddc3790b069cbbc068734ccb4f3d
wyu <kharma@gmail.com>
2014-02-02 18:32:21 -0800

    64979194: pass failing spec

--- spec/features/sites_spec.rb
+++ spec/features/sites_spec.rb
@@ -11,20 +11,32 @@ describe "Sites" do
     { "warden.user.user.key" => session["warden.user.user.key"] }
   end
 
-  let(:user) { FactoryGirl.build(:user) }
+  let(:user) { FactoryGirl.build(
+    :user,
+    email: 'name@stormsavvy.com',
+    password: 'stormpopwho?',
+    password_confirmation: 'stormpopwho?'
+    )
+  }
 
   before :each do
+    user.save!
+    login_as(user, :scope => :user)
+    visit root_path
   end
 
   describe "GET /sites" do
     it "saves the new site" do
-      visit root_path
-      login_as(user, :scope => :user)
+      # fill_in 'Email', with: 'name@stormsavvy.com'#, :match => :prefer_exact
+      # fill_in 'Password', with: 'stormpopwho?'#, :match => :prefer_exact
+      # click_button 'Sign in'
 
-      # visit new_site_path
-      page.should have_content('New Site')
+      page.should have_content('Active Sites')
+      page.should have_content('Weather Report')
       click_link 'New Site'
       current_path.should == new_site_path
+      pp current_path
+
       fill_in 'Name', :with => 'ecp', :match => :prefer_exact
       fill_in 'Zipcode', :with => 94530
       click_button 'Save'
