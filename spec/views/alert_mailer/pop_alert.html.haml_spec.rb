51861164285a495ef4ac9fbf55573fd940a53bd0
wyu <kharma@gmail.com>
2014-02-05 07:54:44 -0800

    65170106: remove unused factory

--- spec/views/alert_mailer/pop_alert.html.haml_spec.rb
+++ spec/views/alert_mailer/pop_alert.html.haml_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "alert_mailer/pop_alert.html.haml" do
 
   it "renders POP alert" do
-    # @user = FactoryGirl.build(:user)
     render
     rendered.should =~ /NOAA POP Alert/
   end
