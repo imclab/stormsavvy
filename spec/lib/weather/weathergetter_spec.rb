e41de09f2f10efc81075862baa9d836f1d806e2e
wyu <kharma@gmail.com>
2014-02-08 14:56:27 -0800

    45436017: remove used begin/rescue block

--- spec/lib/weather/weathergetter_spec.rb
+++ spec/lib/weather/weathergetter_spec.rb
@@ -7,42 +7,37 @@ include Typhoeus
 
 describe WeatherGetter do
 
-  let!(:json) { JSON.parse(IO.read('./spec/fixtures/wunderground_10day.json')) }
+  let(:json) { JSON.parse(IO.read('./spec/fixtures/wunderground_10day.json')) }
   let(:wg) { WeatherGetter.new }
   let(:ww) { WeatherWorker.new }
   let(:zipcode) { 94530 }
+  let(:forecast) { wg.get_forecast(zipcode) }
+  let(:forecastday) { wg.parse_wunderground_10day(json) }
+  let(:site) { FactoryGirl.build(:site) }
 
   it "gets the weather for 94530" do
-    # begin
-      forecast = wg.get_forecast(zipcode)
-      pp forecast
-
-      forecast.should include('response')
-      forecast.should include('forecast')
-      forecast.should have(2).items
-    # rescue
-    #   pp 'no network connection or query failed'
-    # end
+    wg.should respond_to(:get_forecast)
+    forecast.should include('response')
+    forecast.should include('forecast')
+    forecast.should have(2).items
   end
 
   it "extracts wunderground's 10 day txt_forecast" do
-    # begin
-      forecast = wg.parse_wunderground_10day(json)
-      forecast.should have(10).items
-    # rescue
-    #   pp 'no network connection or query failed'
-    # end
+    wg.should respond_to(:parse_wunderground_10day)
+    forecastday.should have(10).items
+  end
+
+  it 'extracts forecast using site zipcode' do
+    forecast = wg.get_forecast(site.zipcode)
+    forecastday = wg.parse_wunderground_10day(forecast)
+    forecastday.should have(10).items
   end
 
   it '#display_forecast' do
-    # begin
-      ww.class.should == WeatherWorker
-      ww.should_not be_nil
+    ww.class.should == WeatherWorker
+    ww.should_not be_nil
 
-      forecast = wg.display_forecast
-      forecast.should have(24).items
-    # rescue
-    #   pp 'no network connection or query failed'
-    # end
+    forecast = wg.display_forecast
+    forecast.should have(10).items
   end
 end
