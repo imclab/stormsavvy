59d01dd443b6467687bfc3862cb6efcd60f59d9e
wyu <kharma@gmail.com>
2014-02-08 14:59:29 -0800

    45436017 rename daily mailer spec

---
+++ spec/views/alert_mailer/daily_mailer.html.haml_spec.rb
@@ -0,0 +1,91 @@
+require 'spec_helper'
+
+describe "alert_mailer/daily_mailer" do
+
+  let!(:user) { FactoryGirl.build(:user) }
+  let!(:ecp) { user.sites.build(
+    name: 'ecp',
+    zipcode: 94530
+    )
+  }
+  let!(:ucb) { user.sites.build(
+    name: 'ucb',
+    zipcode: 94709
+    )
+  }
+  let!(:sites) { [ecp, ucb] }
+
+  it "renders the noaa forecast alert email view" do
+    sign_in user
+    @user = user
+
+    ecp.save
+    ucb.save
+    @forecast_table = []
+
+    sites.each do |site|
+      nfs = NoaaForecastService.new(site: site)
+      forecast_table = nfs.forecast_table(site)
+      @forecast_table << forecast_table
+    end
+
+    render
+    rendered.should match(/Project/)
+    rendered.should match(/Project EA/)
+    rendered.should match(/Project Costcode/)
+    rendered.should match(/Site ID/)
+    rendered.should match(/Site Name/)
+    rendered.should match(/Description/)
+    rendered.should match(/Address 1/)
+    rendered.should match(/Address 2/)
+    rendered.should match(/City/)
+    rendered.should match(/State/)
+    rendered.should match(/Zipcode/)
+    rendered.should match(/Lat/)
+    rendered.should match(/Long/)
+    rendered.should match(/Risk Level/)
+    rendered.should match(/WDID Number/)
+    rendered.should match(/Construction Phase/)
+    rendered.should match(/Total Area/)
+    rendered.should match(/Total DSA/)
+    rendered.should match(/Current DSA/)
+    rendered.should match(/Inactive DSA/)
+    rendered.should match(/Time Since Last Storm/)
+    rendered.should match(/Precipitation Received During Last Storm/)
+    rendered.should match(/Inactive DSA/)
+    rendered.should match(/Contractor Name/)
+    rendered.should match(/Contractor Address 1/)
+    rendered.should match(/Contractor Address 2/)
+    rendered.should match(/Contractor City/)
+    rendered.should match(/Contractor State/)
+    rendered.should match(/Contractor Zipcode/)
+    rendered.should match(/WPCM Name/)
+    rendered.should match(/WPCM Company/)
+    rendered.should match(/WPCM Phone Number/)
+    rendered.should match(/WPCM Emergency Phone Number/)
+    rendered.should match(/QSP Name/)
+    rendered.should match(/QSP Company/)
+    rendered.should match(/QSP Phone Number/)
+    rendered.should match(/QSP Emergency Phone Number/)
+    rendered.should match(/Permits/)
+    rendered.should match(/Resident Engineer Name/)
+    rendered.should match(/Structures Representative Name/)
+    rendered.should match(/Last BMP Status Report Date/)
+    rendered.should match(/Last Weekly Inspection Date/)
+    rendered.should match(/Last Corrective Action Report Date/)
+    rendered.should match(/Last REAP Report Date/)
+    rendered.should match(/Last Training Report Date/)
+    rendered.should match(/Last Weather Forecast Date/)
+    rendered.should match(/Last Rain Event Sampling Date/)
+
+    rendered.should match(/NOAA Forecast Table/)
+    rendered.should match(/7-Day Forecast/)
+    rendered.should match(/Date/)
+    rendered.should match(/Chance of Rain/)
+    rendered.should match(/Forecast Rain/)
+
+    rendered.should match(/UTC/)
+    rendered.should match(/%/)
+    rendered.should match(/"/)
+  end
+end
