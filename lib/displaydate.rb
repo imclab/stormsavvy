7646d5057906154b4f9f2f4a45f0d10ec4abdd34
wyu <kharma@gmail.com>
2014-02-08 22:21:24 -0800

    45436017 return time without tags

--- lib/displaydate.rb
+++ lib/displaydate.rb
@@ -6,19 +6,23 @@ class DisplayDate
   end
 
   def get_day(htmlclass)
-    "<span class='#{htmlclass}'>#{@epoch.strftime("%A")}</span>"
+    "#{@epoch.strftime("%A")}"
+    # "<span class='#{htmlclass}'>#{@epoch.strftime("%A")}</span>"
   end
 
   def get_date(htmlclass)
-    "<span class='#{htmlclass}'>#{@epoch.strftime("%d")}</span>"
+    "#{@epoch.strftime("%d")}"
+    # "<span class='#{htmlclass}'>#{@epoch.strftime("%d")}</span>"
   end
 
   def get_month(htmlclass)
-    "<span class='#{htmlclass}'>#{@epoch.strftime("%b")}</span>"
+    "#{@epoch.strftime("%b")}"
+    # "<span class='#{htmlclass}'>#{@epoch.strftime("%b")}</span>"
   end
 
   def get_year(htmlclass)
-    "<span class='#{htmlclass}'>#{@epoch.strftime("%Y")}</span>"
+    "#{@epoch.strftime("%Y")}"
+    # "<span class='#{htmlclass}'>#{@epoch.strftime("%Y")}</span>"
   end
 
 end
