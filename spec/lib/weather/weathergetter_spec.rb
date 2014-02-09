c6c087e5e0a1a421a13fcba8a292ffab79fe355c
wyu <kharma@gmail.com>
2014-02-08 22:22:13 -0800

    45436017 add wg api attributes

--- spec/lib/weather/weathergetter_spec.rb
+++ spec/lib/weather/weathergetter_spec.rb
@@ -15,29 +15,52 @@ describe WeatherGetter do
   let(:forecastday) { wg.parse_wunderground_10day(json) }
   let(:site) { FactoryGirl.build(:site) }
 
-  it "gets the weather for 94530" do
-    wg.should respond_to(:get_forecast)
-    forecast.should include('response')
-    forecast.should include('forecast')
-    forecast.should have(2).items
+  describe '#get_forecast' do
+    it "gets the weather for 94530" do
+      wg.should respond_to(:get_forecast)
+      forecast.should include('response')
+      forecast.should include('forecast')
+      forecast.should have(2).items
+    end
+
+    it 'extracts forecast using site zipcode' do
+      forecast = wg.get_forecast(site.zipcode)
+      forecastday = wg.parse_wunderground_10day(forecast)
+      forecastday.should have(10).items
+    end
   end
 
-  it "extracts wunderground's 10 day txt_forecast" do
-    wg.should respond_to(:parse_wunderground_10day)
-    forecastday.should have(10).items
+  describe '#parse_wunderground_10day' do
+    it "extracts wunderground's 10 day txt_forecast" do
+      wg.should respond_to(:parse_wunderground_10day)
+      forecastday.should have(10).items
+    end
   end
 
-  it 'extracts forecast using site zipcode' do
-    forecast = wg.get_forecast(site.zipcode)
-    forecastday = wg.parse_wunderground_10day(forecast)
-    forecastday.should have(10).items
+  describe '#display_forecast' do
+    it 'fetches forecast using background worker' do
+      ww.class.should == WeatherWorker
+      forecast = wg.display_forecast
+      forecast.should have(10).items
+    end
   end
 
-  it '#display_forecast' do
-    ww.class.should == WeatherWorker
-    ww.should_not be_nil
-
-    forecast = wg.display_forecast
-    forecast.should have(10).items
+  describe '#forecast_table' do
+    it 'returns forecast for given site' do
+      wg.should respond_to(:forecast_table)
+      forecastday = wg.forecast_table(site)
+      forecastday.each do |f|
+        f['pop'].should be_between(0,100)
+        f['qpf_allday'].count.should == 2
+        f['qpf_allday']['in'].should be_between(0,100)
+        # f['date']['weekday'].count.should == 1
+        f['date']['day'].should be_between(0,31)
+        # f['date']['monthname'].count.should == 1
+        # f['date']['year'].count.should == 1
+        f['date']['hour'].should == 22
+        # f['date']['min'].should == 00
+        f['date']['tz_short'].should == 'PST'
+      end
+    end
   end
 end
