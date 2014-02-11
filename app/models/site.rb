85fde626bab53efed735810c6f7345d1f5281883
wyu <kharma@gmail.com>
2014-02-10 22:21:43 -0800

    65533816 debugging site forecast

--- app/models/site.rb
+++ app/models/site.rb
@@ -90,16 +90,18 @@ class Site < ActiveRecord::Base
 
   def noaa_table
     nfs = NoaaForecastService.new(site: self)
-    forecast = nfs.forecast_table(self)
-    self.noaa_forecast = forecast
-    self.save
+    nfs.forecast_table(self)
+    # forecast = nfs.forecast_table(self)
+    # self.noaa_forecast = forecast
+    # self.save
   end
 
   def wg_table
     wg = WeatherGetter.new
-    forecast = wg.forecast_table(self)
-    self.wg_forecast = forecast
-    self.save
+    wg.forecast_table(self)
+    # forecast = wg.forecast_table(self)
+    # self.wg_forecast = forecast
+    # self.save
   end
 
   def forecast
