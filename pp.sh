1d6969ca5073aef1569912528c3db22c76e064ab
wyu <kharma@gmail.com>
2014-02-15 15:42:54 -0800

    64290146 add bash script

---
+++ pp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# git pull bitbucket master
+# git pull github master
+
+git pull --rebase bitbucket master
+# git pull --rebase github master
+
+git push bitbucket master
+# git push github master
