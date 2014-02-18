74a30629c5e7e7eb36c53a50194d38ee74089ae0
wyu <kharma@gmail.com>
2014-02-17 23:47:33 -0800

    65533816 update syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,9 +90,10 @@ Stormsavvy::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+  config.wunderground_apikey = ENV['WUNDERGROUND_APIKEY']
 end
