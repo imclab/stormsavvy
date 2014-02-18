f609ea8b86d864d268fee516a17a95986d75ce73
wyu <kharma@gmail.com>
2014-02-17 23:48:13 -0800

    65533816 group queries into batches

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -77,11 +77,14 @@ describe User do
   describe '#wg_forecast' do
     it 'saves wg forecast for all sites' do
       user.should respond_to(:wg_forecast)
-      user.wg_forecast
-      user.sites.each(batch_size: 5) do |site|
-        site.wg_forecast.each do |f|
-          f['pop'].should be_between(0,100)
-          f['qpf_allday']['in'].should be_between(0,100)
+      user.sites.in_groups_of(4) do |group|
+        group.each do |site|
+          sleep(30)
+          site.save_wg
+          site.wg_forecast.each do |f|
+            f['pop'].should be_between(0,100)
+            f['qpf_allday']['in'].should be_between(0,100)
+          end
         end
       end
     end
