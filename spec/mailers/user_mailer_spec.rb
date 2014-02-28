00e5d383d1aa648d78b39341efbb3785daacd238
wyu <kharma@gmail.com>
2014-02-27 22:41:00 -0800

    64290058 update mailer delivery count

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -136,7 +136,7 @@ describe UserMailer do
     let!(:mailer) { UserMailer.mailout(email).deliver }
 
     it "delivers mailer" do
-      ActionMailer::Base.deliveries.count.should == 1
+      ActionMailer::Base.deliveries.count.should == 2
       ActionMailer::Base.deliveries.should_not be_empty
     end
 
