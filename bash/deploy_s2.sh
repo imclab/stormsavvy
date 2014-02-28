b7112177725f2fe1fcae04d0a29716b8c64e4b1b
wyu <kharma@gmail.com>
2014-02-27 22:51:36 -0800

    64290058 rename script

--- bash/deploy_s2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git push git@heroku.com:stormsavvy2.git master
-heroku restart worker.1 -a stormsavvy2
-heroku restart -a stormsavvy2
-heroku logs -a stormsavvy2
-heroku open -a stormsavvy2
