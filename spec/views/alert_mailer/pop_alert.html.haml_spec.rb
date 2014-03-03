30a3ee9ffd1f290bc20d9a5ce3e41a56510b76cf
wyu <kharma@gmail.com>
2014-03-03 15:02:16 -0800

    66819324 update pop_alert spec

--- spec/views/alert_mailer/pop_alert.html.haml_spec.rb
+++ spec/views/alert_mailer/pop_alert.html.haml_spec.rb
@@ -1,9 +1,42 @@
 require 'spec_helper'
 
 describe "alert_mailer/pop_alert.html.haml" do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:site) {
+    FactoryGirl.create(
+      :site,
+      user: user
+    )
+  }
+  let!(:forecast) { site.forecast }
+  let!(:fe) { ForecastExaminer.new(site, forecast) }
+
+  before {
+    @forecast_table = []
+    nfs = NoaaForecastService.new(site: site)
+    noaa_table = nfs.forecast_table(site)
+    @forecast_table << noaa_table
+  }
+
+  before(:each) do
+    sign_in user
+    @user = user
+    @site = site
+    render
+  end
 
   it "renders POP alert" do
-    render
-    rendered.should =~ /NOAA POP Alert/
+    rendered.should match(/NOAA POP Alert/)
+    rendered.should match(/NOAA 7-Day Forecast/)
+    rendered.should match(/Wunderground 7-Day Forecast/)
+  end
+
+  it 'renders forecast table' do
+    rendered.should match(/Date/)
+    rendered.should match(/POP/)
+    rendered.should match(/QPF/)
+    rendered.should match(/UTC/)
+    rendered.should match(/%/)
+    rendered.should match(/inch/)
   end
 end
