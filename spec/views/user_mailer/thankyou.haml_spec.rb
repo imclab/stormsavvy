4a85ac16484056de2fa8a6c7da9863035ffb1871
wyu <kharma@gmail.com>
2014-03-03 15:53:00 -0800

    66822536 update spec

--- spec/views/user_mailer/thankyou.haml_spec.rb
+++ spec/views/user_mailer/thankyou.haml_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe "user_mailer/thankyou" do
-
   it "thanks user for signing up" do
     render
     rendered.should have_selector 'table'
     rendered.should have_selector 'h1', :text => "Welcome to Storm Savvy!"
-    rendered.should =~ /Thank you/
   end
-
 end
