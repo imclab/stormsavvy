e7483972552b1b2a3a34bdcb77cc1d327b4eb9fa
wyu <kharma@gmail.com>
2014-03-04 10:50:56 -0800

    64431120 catch geocoder response error

--- spec/services/geocoder_service_spec.rb
+++ spec/services/geocoder_service_spec.rb
@@ -45,7 +45,7 @@ describe GeocoderService do
         query_results = GeonamesGeocodingService.search(0)
         query_results.body["postalCodes"][0].should == nil
       rescue
-        'not online or api query error'
+        'Geocoding API connection cannot be established'
       end
     end
   end
