15dbe32d61f31127f88a452acfb6fa487bcaedd6
wyu <kharma@gmail.com>
2014-03-10 23:23:05 -0700

    65430250 add ww spec

--- spec/workers/weatherworker_spec.rb
+++ spec/workers/weatherworker_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
+require 'redis'
+require 'sidekiq'
 require 'weather/weathergetter'
-require 'typhoeus'
-require 'json'
-
-include Typhoeus
 
 describe WeatherWorker do
-  it '#perform' do
 
+  include Sidekiq::Worker
+
+  let(:ww) { WeatherWorker.new }
+  let(:zipcode) { 94612 }
+
+  context 'when passed zipcode' do
+    it '#perform' do
+      ww.should respond_to(:perform)
+      data = ww.perform(zipcode)
+      pp data
+    end
+
+    it 'gets forecast using worker' do
+      data = WeatherWorker.perform_async(zipcode)
+      pp data
+    end
   end
 end
