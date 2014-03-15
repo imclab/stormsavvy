be85d414165dcdb19843b3e5d0fb6c450541c5ba
wyu <kharma@gmail.com>
2014-03-15 11:48:36 -0700

    67604632 update syntax

--- spec/controllers/inspection_events_controller_spec.rb
+++ spec/controllers/inspection_events_controller_spec.rb
@@ -12,12 +12,12 @@ describe InspectionEventsController do
 
   def valid_attributes
     {
-      :site_id => 1,
-      :inspection_type => "weekly",
-      :inspection_description => "ec jungle gym inspection",
-      :inspection_date => "2013-01-26 23:13:55",
-      :submitted_by => "wyu",
-      :inspected_by => "wyu"
+      site_id: 1,
+      inspection_type: "weekly",
+      inspection_description: "ec jungle gym inspection",
+      inspection_date: "2013-01-26 23:13:55",
+      submitted_by: "wyu",
+      inspected_by: "wyu"
     }
   end
 
