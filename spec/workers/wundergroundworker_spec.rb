1c1bf1851de023afd4125d65b7c01476e31a5ebd
wyu <kharma@gmail.com>
2014-03-15 23:35:39 -0700

    64547646 add wunderground worker spec

---
+++ spec/workers/wundergroundworker_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'redis'
+require 'sidekiq'
+require 'weather/weathergetter'
+
+describe WundergroundWorker do
+
+  include Sidekiq::Worker
+
+  let(:ww) { WundergroundWorker.new }
+  let(:site) { FactoryGirl.create(:site) }
+  let(:zipcode) { 94612 }
+
+  context 'when passed site' do
+    it '#perform' do
+      ww.should respond_to(:perform)
+      data = ww.perform(site.id)
+
+      pp data
+      data.count.should have(8).items
+      data.each do |f|
+        f['pop'].should be_between(0,100)
+        f['qpf_allday'].count.should == 2
+        f['qpf_allday']['in'].should be_between(0,100)
+        f['date']['day'].should be_between(0,31)
+      end
+    end
+
+    it '#perform_async' do
+      data = WundergroundWorker.perform_async(site.id)
+      data.class.should == String
+    end
+  end
+end
