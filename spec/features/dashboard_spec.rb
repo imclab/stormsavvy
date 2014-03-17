94e51aa8d6408f9d9e782a91e2969d79094b1d52
wyu <kharma@gmail.com>
2014-03-17 16:53:56 -0700

    67189134 add pp statement

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -268,6 +268,7 @@ describe "Dashboard" do
           current_pending_site.chance_of_rain.pop.should_not be_nil
         rescue => e
           # pp 'not online or pop method error'
+          pp e
         end
       end
 
