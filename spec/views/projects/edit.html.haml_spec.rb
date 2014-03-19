c1959486fddf1c16a25b3df54a8ca6abc51408df
wyu <kharma@gmail.com>
2014-03-18 20:47:37 -0700

    TBD add project edit spec

--- spec/views/projects/edit.html.haml_spec.rb
+++ spec/views/projects/edit.html.haml_spec.rb
@@ -4,20 +4,33 @@ describe "projects/edit" do
 
   before(:each) do
     @user = FactoryGirl.create(:user)
-    @project = @user.projects.create(
-      {
-        :name        => "MyString",
-        :description => "MyText",
-        :startdate   => 5.days.ago,
-        :finishdate  => 1.day.ago,
-        :active      => false
-      }
+    @project = FactoryGirl.create(
+    # @project = @user.projects.create(
+      :project,
+      user: user,
+      name: "ecp",
+      description: "el plaza",
+      startdate: 5.days.ago,
+      finishdate: 1.day.ago,
+      active: false
     )
   end
 
-  xit "renders the edit project form" do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:project) { FactoryGirl.create(
+    :project,
+    user:user,
+    name: "ecp",
+    description: "el plaza",
+    startdate: 5.days.ago,
+    finishdate: 1.day.ago,
+    active: false,
+    )
+  }
+
+  it "renders edit project form" do
     render
-    assert_select "form", :action => projects_path(@project), :method => "post" do
+    assert_select "form", :action => projects_path(project), :method => "post" do
       assert_select "input#project_name", :name => "project[name]"
       assert_select "input#project_active", :name => "project[active]"
       assert_select "textarea#project_description", :name => "project[description]"
