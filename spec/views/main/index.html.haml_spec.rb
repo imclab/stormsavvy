9641fbe3eb65c1230331a92f11396d4baaeabdeb
wyu <kharma@gmail.com>
2014-03-18 20:47:22 -0700

    TBD add main index view spec

--- spec/views/main/index.html.haml_spec.rb
+++ spec/views/main/index.html.haml_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe "main/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "main/index.html.erb" do
+
+  before do
+    view.stub(:index).and_return(true)
+  end
+
+  it 'checks maincopy partial view' do
+    render
+    response.code.should == 200
+  end
 end
