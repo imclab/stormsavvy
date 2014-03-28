4ed61433a412e70f93bf021f20171bf714cea441
wyu <kharma@gmail.com>
2014-03-27 20:05:28 -0700

    64520038 update spec to pending

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe MainController do
 
   describe "GET 'index'" do
-    xit "returns http success" do
-      # debug with angular js routes
+    it "returns http success" do
+      pending 'debug with angular js routes'
       get 'index'
       response.should be_success
     end
